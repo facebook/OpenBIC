8d28bb1fde138e2406d29f4cf49d48bef1ba2bd2
Lora Lin <Lora_Lin@wiwynn.com>
2021-12-15 20:21:28 -0500

    fby3.5: cl:Fixed PCH reading function
    Summary:
    - Added the judgment condition of completion code to confirm return value is valid.
    - Added PCH retry read feature while getting invalid response case.
    
    Test plan:
    - Build code: Pass
    - Test 600 times reset: Pass

--- meta-facebook/yv35-cl/src/sensor/dev/pch.c
+++ meta-facebook/yv35-cl/src/sensor/dev/pch.c
@@ -4,30 +4,80 @@
 #include "sensor_def.h"
 #include "ipmi.h"
 
+ipmb_error pch_ipmb_read(ipmi_msg *bridge_msg) {
+  bridge_msg->seq_source = 0xff;
+  bridge_msg->netfn = NETFN_SENSOR_REQ;
+  bridge_msg->cmd = CMD_SENSOR_GET_SENSOR_READING;
+  bridge_msg->InF_source = Self_IFs;
+  bridge_msg->InF_target = ME_IPMB_IFs;
+  bridge_msg->data_len = 1;
+  bridge_msg->data[0] = 0x08;
+  return ipmb_read(bridge_msg, IPMB_inf_index_map[bridge_msg->InF_target]);
+}
+
 bool pal_pch_read(uint8_t sensor_num, int *reading) {
 
   ipmb_error status;
   ipmi_msg *bridge_msg;
   bridge_msg = (ipmi_msg*)malloc(sizeof(ipmi_msg));
+  if (bridge_msg == NULL) {
+    printk("PCH bridge message alloc fail\n");
+  }
 
-  bridge_msg->data_len = 1;
-  bridge_msg->seq_source = 0xff;
-  bridge_msg->InF_source = Self_IFs;
-  bridge_msg->InF_target = ME_IPMB_IFs;
-  bridge_msg->netfn = NETFN_SENSOR_REQ;
-  bridge_msg->cmd = CMD_SENSOR_GET_SENSOR_READING;
-  bridge_msg->data[0] = 0x08;
-
-  status = ipmb_read(bridge_msg, IPMB_inf_index_map[bridge_msg->InF_target]);
+  status = pch_ipmb_read(bridge_msg);
   if (status != ipmb_error_success) {
     sensor_config[SnrNum_SnrCfg_map[sensor_num]].cache_status = SNR_FAIL_TO_ACCESS;
-    printf("ipmb read fail status: %x", status);
-    free(bridge_msg);
+    printk("ipmb read fail status: %x\n", status);
+    if (bridge_msg != NULL) {
+      free(bridge_msg);
+    }
     return false;
   }
-  *reading = (cal_MBR(sensor_num, bridge_msg->data[0])) & 0xff;
-  sensor_config[SnrNum_SnrCfg_map[sensor_num]].cache = *reading;
-  sensor_config[SnrNum_SnrCfg_map[sensor_num]].cache_status = SNR_READ_SUCCESS;
-  free(bridge_msg);
-  return true;
+  if (bridge_msg->completion_code == CC_SUCCESS) {
+    *reading = (cal_MBR(sensor_num, bridge_msg->data[0])) & 0xff;
+    sensor_config[SnrNum_SnrCfg_map[sensor_num]].cache = *reading;
+    sensor_config[SnrNum_SnrCfg_map[sensor_num]].cache_status = SNR_READ_SUCCESS;
+    if (bridge_msg != NULL) {
+      free(bridge_msg);
+    }
+    return true;
+  } else if (bridge_msg->completion_code == CC_NODE_BUSY) {
+    uint8_t pch_retry_num;
+    for (pch_retry_num = 0 ; pch_retry_num < 3 ; pch_retry_num ++) {
+       ipmb_error pch_retry_result = pch_ipmb_read(bridge_msg);
+      if (pch_retry_result != ipmb_error_success) {
+        printk("ipmb read fail status: %x\n", status);
+        if (bridge_msg != NULL) {
+          free(bridge_msg);
+        }
+        return false;
+      }
+      if (bridge_msg->completion_code == CC_SUCCESS) { 
+        sensor_config[SnrNum_SnrCfg_map[sensor_num]].cache = cal_MBR(sensor_num, bridge_msg->data[0]) & 0xff;
+        sensor_config[SnrNum_SnrCfg_map[sensor_num]].cache_status = SNR_READ_SUCCESS;
+        if (bridge_msg != NULL) {
+          free(bridge_msg);
+        }
+        return true;
+      } else if (bridge_msg->completion_code != CC_NODE_BUSY) {
+        if (bridge_msg != NULL) {
+          free(bridge_msg);
+        }
+        sensor_config[SnrNum_SnrCfg_map[sensor_num]].cache_status = SNR_UNSPECIFIED_ERROR;
+        return false;
+      } 
+    }
+    if (bridge_msg != NULL) {
+      free(bridge_msg);
+    }
+    printk("PCH retry read fail\n");
+    sensor_config[SnrNum_SnrCfg_map[sensor_num]].cache_status = SNR_UNSPECIFIED_ERROR;
+    return false;
+  } else {
+    if (bridge_msg != NULL) {
+      free(bridge_msg);
+    }
+    sensor_config[SnrNum_SnrCfg_map[sensor_num]].cache_status = SNR_UNSPECIFIED_ERROR;
+    return false;
+  }
 }
