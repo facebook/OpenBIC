68b47ab09337702db19bf7e86a8ecbf1a31f0099
rainlinWW <rain_lin@wiwynn.com>
2022-01-25 19:51:30 -0800

    fby3.5: cl: Supported BB BIC IPMB and fixed IPMB interface (#135)
    Summary:
    - Added class2 check and set IPMB config for BB BIC.
    - Modified IPMB bridge interface to align with Yv3.
    - Fixed IPMB interface and index mapping.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/135
    
    Test Plan:
    - Build code: Pass
    - Bridge command: Pass
    
    Log:
    Test bridge command on class1 system with 1ou and 2ou expansion BIC:
    root@bmc-oob:~# bic-util slot1 0x18 0x1
    00 80 11 04 02 BF 9C 9C 00 00 00 00 00 00 00
    root@bmc-oob:~# bic-util slot1 0xe0 0x2 0x9c 0x9c 0x0 0x1 0x18 0x1
    9C 9C 00 01 07 01 00 50 01 06 02 02 21 57 01 00
    18 0B 06 02 40 01
    root@bmc-oob:~# bic-util slot1 0xe0 0x2 0x9c 0x9c 0x0 0x2 0x18 0x1
    9C 9C 00 02 07 01 00 20 81 15 50 02 BF 15 A0 00
    46 31 01 00 00 00
    root@bmc-oob:~# bic-util slot1 0xe0 0x2 0x9c 0x9c 0x0 0x5 0x18 0x1
    9C 9C 00 05 07 01 00 25 80 25 01 02 BF 4C 1C 00
    50 57 00 00 00 00
    root@bmc-oob:~# bic-util slot1 0xe0 0x2 0x9c 0x9c 0x0 0x15 0x18 0x1
    9C 9C 00 15 07 01 00 25 80 27 04 02 BF 4C 1C 00
    50 57 00 00 00 00
    
    Test bridge command on class2 system with 2ou expansion BIC:
    root@bmc-oob:~# bic-util slot1 0xe0 0x2 0x9c 0x9c 0x0 0x01 0x18 0x01
    9C 9C 00 01 07 01 00 50 01 86 01 02 20 57 01 00
    18 0B 00 02 70 00
    root@bmc-oob:~# bic-util slot1 0xe0 0x2 0x9c 0x9c 0x0 0x02 0x18 0x01
    9C 9C 00 02 07 01 00 20 81 01 03 02 BF 15 A0 00
    46 31 00 00 00 00
    root@bmc-oob:~# bic-util slot1 0xe0 0x2 0x9c 0x9c 0x0 0x05 0x18 0x01
    BIC no response!
    root@bmc-oob:~# bic-util slot1 0xe0 0x2 0x9c 0x9c 0x0 0x10 0x18 0x01
    9C 9C 00 10 07 01 00 00 80 01 01 02 BF 9C 9C 00
    00 00 00 00 00 00
    root@bmc-oob:~# bic-util slot1 0xe0 0x2 0x9c 0x9c 0x0 0x15 0x18 0x01
    9C 9C 00 15 07 01 00 25 80 47 04 02 BF 4C 1C 00
    50 57 00 00 00 00
    
    Reviewed By: garnermic
    
    Differential Revision: D33693225
    
    Pulled By: GoldenBug
    
    fbshipit-source-id: 195ace9

--- meta-facebook/yv35-cl/src/ipmi/include/plat_ipmb.h
+++ meta-facebook/yv35-cl/src/ipmi/include/plat_ipmb.h
@@ -3,23 +3,26 @@
 
 #include "plat_i2c.h"
 
-#define IPMB_BMC_BUS  i2c_bus7
-#define IPMB_ME_BUS   i2c_bus3
-#define IPMB_EXP1_BUS i2c_bus8
-#define IPMB_EXP2_BUS i2c_bus9
-#define Reserve_BUS 0xff
+#define IPMB_BMC_BUS    i2c_bus7
+#define IPMB_ME_BUS     i2c_bus3
+#define IPMB_EXP1_BUS   i2c_bus8
+#define IPMB_EXP2_BUS   i2c_bus9
+#define IPMB_BB_BIC_BUS i2c_bus8
+#define Reserve_BUS     0xff
 
-#define BMC_I2C_ADDRESS  0x10
-#define ME_I2C_ADDRESS   0x16
-#define Self_I2C_ADDRESS 0x20
-#define BIC1_I2C_ADDRESS 0x20
-#define BIC2_I2C_ADDRESS 0x20
-#define Reserve_ADDRESS  0xff
+#define BMC_I2C_ADDRESS    0x10
+#define ME_I2C_ADDRESS     0x16
+#define Self_I2C_ADDRESS   0x20
+#define BIC1_I2C_ADDRESS   0x20
+#define BIC2_I2C_ADDRESS   0x20
+#define BB_BIC_I2C_ADDRESS 0x20
+#define Reserve_ADDRESS    0xff
 
 enum {
 	BMC_IPMB_IDX,
 	ME_IPMB_IDX,
 	EXP1_IPMB_IDX,
+	BB_IPMB_IDX = EXP1_IPMB_IDX,
 	EXP2_IPMB_IDX,
 	RESERVE_IPMB_IDX,
 };
