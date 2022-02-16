fef13be3734850b8090d4500983b2b2e55291e39
Peter Fetros <goldenbug@fb.com>
2022-02-15 17:56:01 -0800

    Clang Format common/util Files
    Summary: Apply Clang Format to the util files under the common directory.
    
    Reviewed By: garnermic
    
    Differential Revision: D34256805
    
    fbshipit-source-id: 782af55

--- common/util/hal_peci.c
+++ common/util/hal_peci.c
@@ -2,206 +2,221 @@
 #include <drivers/peci.h>
 #include "hal_peci.h"
 
-
 const struct device *dev;
 
-int peci_init() {
-  dev = device_get_binding("PECI");
-  int ret;
-  uint32_t bitrate = 1000;
-  if (!dev) {
-    printf("peci device not found");
-    return false;
-  }
-  ret = peci_config(dev, bitrate);
-  if (ret) {
-    printf("set bitrate %dKbps failed %d\n", bitrate, ret);
-    return ret;
-  }
-  ret = peci_enable(dev);
-  if (ret) {
-    printf("peci enable failed %d\n", ret);
-    return ret;
-  }
+int peci_init()
+{
+	dev = device_get_binding("PECI");
+	int ret;
+	uint32_t bitrate = 1000;
+	if (!dev) {
+		printf("peci device not found");
+		return false;
+	}
+	ret = peci_config(dev, bitrate);
+	if (ret) {
+		printf("set bitrate %dKbps failed %d\n", bitrate, ret);
+		return ret;
+	}
+	ret = peci_enable(dev);
+	if (ret) {
+		printf("peci enable failed %d\n", ret);
+		return ret;
+	}
 
-  return ret;
+	return ret;
 }
 
-int peci_ping(uint8_t address) {
-  struct peci_msg pkgcfg;
-  int ret;
+int peci_ping(uint8_t address)
+{
+	struct peci_msg pkgcfg;
+	int ret;
 
-  pkgcfg.addr = address;
-  pkgcfg.cmd_code = PECI_CMD_PING;
-  pkgcfg.tx_buffer.buf = NULL;
-  pkgcfg.tx_buffer.len = 0x0;
-  pkgcfg.rx_buffer.len = 0x0;
-  
-  ret = peci_transfer(dev, &pkgcfg);
-  if (ret) {
-    printf("ping failed %d\n", ret);
-    free(pkgcfg.tx_buffer.buf);
-    return ret;
-  }
-  
-  free(pkgcfg.tx_buffer.buf);
-  return ret;
+	pkgcfg.addr = address;
+	pkgcfg.cmd_code = PECI_CMD_PING;
+	pkgcfg.tx_buffer.buf = NULL;
+	pkgcfg.tx_buffer.len = 0x0;
+	pkgcfg.rx_buffer.len = 0x0;
+
+	ret = peci_transfer(dev, &pkgcfg);
+	if (ret) {
+		printf("ping failed %d\n", ret);
+		free(pkgcfg.tx_buffer.buf);
+		return ret;
+	}
+
+	free(pkgcfg.tx_buffer.buf);
+	return ret;
 }
 
-int peci_read(uint8_t cmd, uint8_t address, uint8_t u8Index, uint16_t u16Param, uint8_t u8ReadLen, uint8_t *readBuf) {
-  struct peci_msg rdpkgcfg;
-  int ret;
-  rdpkgcfg.cmd_code = cmd;
-  rdpkgcfg.addr = address;
-  rdpkgcfg.tx_buffer.len = 0x05;
-  rdpkgcfg.rx_buffer.len = u8ReadLen;
-  rdpkgcfg.tx_buffer.buf = malloc(rdpkgcfg.tx_buffer.len * sizeof(uint8_t));
-  rdpkgcfg.rx_buffer.buf = readBuf;
-  rdpkgcfg.tx_buffer.buf[0] = 0x00;
-  rdpkgcfg.tx_buffer.buf[1] = u8Index;
-  rdpkgcfg.tx_buffer.buf[2] = u16Param & 0xff;
-  rdpkgcfg.tx_buffer.buf[3] = u16Param >> 8;  
-  ret = peci_transfer(dev, &rdpkgcfg);
-  
-  if (DEBUG_PECI) {
-    uint8_t index;
-    for (index = 0; index < 5; index++)
-      printf("%02x ", readBuf[index]);
-    printf("\n");
-  }
+int peci_read(uint8_t cmd, uint8_t address, uint8_t u8Index, uint16_t u16Param, uint8_t u8ReadLen,
+	      uint8_t *readBuf)
+{
+	struct peci_msg rdpkgcfg;
+	int ret;
+	rdpkgcfg.cmd_code = cmd;
+	rdpkgcfg.addr = address;
+	rdpkgcfg.tx_buffer.len = 0x05;
+	rdpkgcfg.rx_buffer.len = u8ReadLen;
+	rdpkgcfg.tx_buffer.buf = malloc(rdpkgcfg.tx_buffer.len * sizeof(uint8_t));
+	rdpkgcfg.rx_buffer.buf = readBuf;
+	rdpkgcfg.tx_buffer.buf[0] = 0x00;
+	rdpkgcfg.tx_buffer.buf[1] = u8Index;
+	rdpkgcfg.tx_buffer.buf[2] = u16Param & 0xff;
+	rdpkgcfg.tx_buffer.buf[3] = u16Param >> 8;
+	ret = peci_transfer(dev, &rdpkgcfg);
 
-  if (ret) {
-    printf("peci read failed %d\n", ret);
-    free(rdpkgcfg.tx_buffer.buf);
-    return ret;
-  }
+	if (DEBUG_PECI) {
+		uint8_t index;
+		for (index = 0; index < 5; index++)
+			printf("%02x ", readBuf[index]);
+		printf("\n");
+	}
 
-  free(rdpkgcfg.tx_buffer.buf);
-  return ret;  
+	if (ret) {
+		printf("peci read failed %d\n", ret);
+		free(rdpkgcfg.tx_buffer.buf);
+		return ret;
+	}
+
+	free(rdpkgcfg.tx_buffer.buf);
+	return ret;
 }
 
-int peci_write(uint8_t cmd, uint8_t address, uint8_t u8ReadLen, uint8_t *readBuf, uint8_t u8WriteLen, uint8_t *writeBuf) {
-  struct peci_msg wrpkgcfg;
-  int ret;
+int peci_write(uint8_t cmd, uint8_t address, uint8_t u8ReadLen, uint8_t *readBuf,
+	       uint8_t u8WriteLen, uint8_t *writeBuf)
+{
+	struct peci_msg wrpkgcfg;
+	int ret;
 
-  wrpkgcfg.addr = address;
-  wrpkgcfg.cmd_code = cmd;
-  wrpkgcfg.tx_buffer.len = u8WriteLen;
-  wrpkgcfg.rx_buffer.len = u8ReadLen;
-  wrpkgcfg.rx_buffer.buf = readBuf;
-  wrpkgcfg.tx_buffer.buf = writeBuf;
-  
-  ret = peci_transfer(dev, &wrpkgcfg);
-  if (ret) {
-    printk("peci write failed %d\n", ret);
-    return ret;
-  }
-  
-  return ret;
+	wrpkgcfg.addr = address;
+	wrpkgcfg.cmd_code = cmd;
+	wrpkgcfg.tx_buffer.len = u8WriteLen;
+	wrpkgcfg.rx_buffer.len = u8ReadLen;
+	wrpkgcfg.rx_buffer.buf = readBuf;
+	wrpkgcfg.tx_buffer.buf = writeBuf;
+
+	ret = peci_transfer(dev, &wrpkgcfg);
+	if (ret) {
+		printk("peci write failed %d\n", ret);
+		return ret;
+	}
+
+	return ret;
 }
 
-bool peci_retry_read(uint8_t cmd, uint8_t address, uint8_t u8Index, uint16_t u16Param, uint8_t u8ReadLen, uint8_t *readBuf) {
-  uint8_t i, ret, retry = 5;
-  
-  if(readBuf == NULL)
-    return false;
+bool peci_retry_read(uint8_t cmd, uint8_t address, uint8_t u8Index, uint16_t u16Param,
+		     uint8_t u8ReadLen, uint8_t *readBuf)
+{
+	uint8_t i, ret, retry = 5;
 
-  for(i = 0; i < retry; ++i) {
-    k_msleep(10);
-    memcpy(&readBuf[0], 0, u8ReadLen * sizeof(uint8_t));
-    ret = peci_read(cmd, address, u8Index, u16Param, u8ReadLen, readBuf);
-    if (!ret) {
-      if (readBuf[0] == PECI_CC_RSP_SUCCESS) {
-        return true;
-      }
-    }
-  }
-  return false;
+	if (readBuf == NULL)
+		return false;
+
+	for (i = 0; i < retry; ++i) {
+		k_msleep(10);
+		memcpy(&readBuf[0], 0, u8ReadLen * sizeof(uint8_t));
+		ret = peci_read(cmd, address, u8Index, u16Param, u8ReadLen, readBuf);
+		if (!ret) {
+			if (readBuf[0] == PECI_CC_RSP_SUCCESS) {
+				return true;
+			}
+		}
+	}
+	return false;
 }
 
-bool peci_getPwr(uint8_t sensor_num, int *reading) {
-  uint8_t rdpkgcfgCmd = PECI_RD_PKG_CFG0_CMD;
-  uint8_t PECIaddr = 0x30;
-  uint8_t readlen = 0x05;
-  uint8_t *readbuf = malloc( 2 * readlen * sizeof(uint8_t) );
-  uint8_t u8index[2] = { 0x03 , 0x1F };
-  uint16_t u16Param[2] = { 0x00FF , 0x0000 };
-  uint8_t ret, complete_code;
-  bool is_retry_success = false;
-  uint32_t pkg_energy, run_time, diff_energy, diff_time;
-  static uint32_t last_pkg_energy = 0, last_run_time = 0;
+bool peci_getPwr(uint8_t sensor_num, int *reading)
+{
+	uint8_t rdpkgcfgCmd = PECI_RD_PKG_CFG0_CMD;
+	uint8_t PECIaddr = 0x30;
+	uint8_t readlen = 0x05;
+	uint8_t *readbuf = malloc(2 * readlen * sizeof(uint8_t));
+	uint8_t u8index[2] = { 0x03, 0x1F };
+	uint16_t u16Param[2] = { 0x00FF, 0x0000 };
+	uint8_t ret, complete_code;
+	bool is_retry_success = false;
+	uint32_t pkg_energy, run_time, diff_energy, diff_time;
+	static uint32_t last_pkg_energy = 0, last_run_time = 0;
 
-  peci_read( rdpkgcfgCmd , PECIaddr , u8index[0] , u16Param[0] , readlen , readbuf );
-  complete_code = readbuf[0];
-  if ( complete_code == PECI_CC_RSP_TIMEOUT || complete_code == PECI_CC_OUT_OF_RESOURCES_TIMEOUT ) {
-    is_retry_success = peci_retry_read( rdpkgcfgCmd , PECIaddr , u8index[0] , u16Param[0] , readlen , readbuf );
-    if ( !is_retry_success ) {
-      printf("PECI sensor [%x] response timeout. Reach Max retry.\n", sensor_num );
-      free(readbuf);
-      return false;
-    }
-  }
-  ret = peci_read( rdpkgcfgCmd , PECIaddr , u8index[1] , u16Param[1] , readlen , &readbuf[5] );
-  complete_code = readbuf[5];
-  if ( complete_code == PECI_CC_RSP_TIMEOUT || complete_code == PECI_CC_OUT_OF_RESOURCES_TIMEOUT ) {
-    is_retry_success = peci_retry_read( rdpkgcfgCmd , PECIaddr , u8index[1] , u16Param[1] , readlen , &readbuf[5] );
-    if ( !is_retry_success ) {
-      printf("PECI sensor [%x] response timeout. Reach Max retry.\n", sensor_num );
-      free(readbuf);
-      return false;
-    }
-  }
-  if ( ret ) {
-    free( readbuf );
-    return false;
-  }
-  if ( readbuf[0] != PECI_CC_RSP_SUCCESS || readbuf[5] != PECI_CC_RSP_SUCCESS ) {
-    if ( readbuf[0] == PECI_CC_ILLEGAL_REQUEST || readbuf[5] == PECI_CC_ILLEGAL_REQUEST ) {
-      printf("Unknown request\n");
-    } else {
-      printf("PECI control hardware, firmware or associated logic error\n");
-    }
-    free( readbuf );
-    return false;
-  }
+	peci_read(rdpkgcfgCmd, PECIaddr, u8index[0], u16Param[0], readlen, readbuf);
+	complete_code = readbuf[0];
+	if (complete_code == PECI_CC_RSP_TIMEOUT ||
+	    complete_code == PECI_CC_OUT_OF_RESOURCES_TIMEOUT) {
+		is_retry_success = peci_retry_read(rdpkgcfgCmd, PECIaddr, u8index[0], u16Param[0],
+						   readlen, readbuf);
+		if (!is_retry_success) {
+			printf("PECI sensor [%x] response timeout. Reach Max retry.\n", sensor_num);
+			free(readbuf);
+			return false;
+		}
+	}
+	ret = peci_read(rdpkgcfgCmd, PECIaddr, u8index[1], u16Param[1], readlen, &readbuf[5]);
+	complete_code = readbuf[5];
+	if (complete_code == PECI_CC_RSP_TIMEOUT ||
+	    complete_code == PECI_CC_OUT_OF_RESOURCES_TIMEOUT) {
+		is_retry_success = peci_retry_read(rdpkgcfgCmd, PECIaddr, u8index[1], u16Param[1],
+						   readlen, &readbuf[5]);
+		if (!is_retry_success) {
+			printf("PECI sensor [%x] response timeout. Reach Max retry.\n", sensor_num);
+			free(readbuf);
+			return false;
+		}
+	}
+	if (ret) {
+		free(readbuf);
+		return false;
+	}
+	if (readbuf[0] != PECI_CC_RSP_SUCCESS || readbuf[5] != PECI_CC_RSP_SUCCESS) {
+		if (readbuf[0] == PECI_CC_ILLEGAL_REQUEST ||
+		    readbuf[5] == PECI_CC_ILLEGAL_REQUEST) {
+			printf("Unknown request\n");
+		} else {
+			printf("PECI control hardware, firmware or associated logic error\n");
+		}
+		free(readbuf);
+		return false;
+	}
 
-  pkg_energy = readbuf[4];
-  pkg_energy = ( pkg_energy << 8 ) | readbuf[3];
-  pkg_energy = ( pkg_energy << 8 ) | readbuf[2];
-  pkg_energy = ( pkg_energy << 8 ) | readbuf[1];
+	pkg_energy = readbuf[4];
+	pkg_energy = (pkg_energy << 8) | readbuf[3];
+	pkg_energy = (pkg_energy << 8) | readbuf[2];
+	pkg_energy = (pkg_energy << 8) | readbuf[1];
 
-  run_time = readbuf[9];
-  run_time = ( run_time << 8 ) | readbuf[8];
-  run_time = ( run_time << 8 ) | readbuf[7];
-  run_time = ( run_time << 8 ) | readbuf[6];
+	run_time = readbuf[9];
+	run_time = (run_time << 8) | readbuf[8];
+	run_time = (run_time << 8) | readbuf[7];
+	run_time = (run_time << 8) | readbuf[6];
 
-  if ( last_pkg_energy == 0 && last_run_time == 0 ) { // first read, need second data to calculate
-    last_pkg_energy = pkg_energy;
-    last_run_time = run_time;
-    free( readbuf );
-    return false;
-  }
+	if (last_pkg_energy == 0 &&
+	    last_run_time == 0) { // first read, need second data to calculate
+		last_pkg_energy = pkg_energy;
+		last_run_time = run_time;
+		free(readbuf);
+		return false;
+	}
 
-  if( pkg_energy >= last_pkg_energy ) {
-    diff_energy = pkg_energy - last_pkg_energy;
-  } else {
-    diff_energy = pkg_energy + ( 0xffffffff - last_pkg_energy + 1 );
-  }
-  last_pkg_energy = pkg_energy;
+	if (pkg_energy >= last_pkg_energy) {
+		diff_energy = pkg_energy - last_pkg_energy;
+	} else {
+		diff_energy = pkg_energy + (0xffffffff - last_pkg_energy + 1);
+	}
+	last_pkg_energy = pkg_energy;
 
-  if( run_time >= last_run_time ) {
-    diff_time = run_time - last_run_time;
-  } else {
-    diff_time = run_time + ( 0xffffffff - last_run_time + 1 );
-  }
-  last_run_time = run_time;
+	if (run_time >= last_run_time) {
+		diff_time = run_time - last_run_time;
+	} else {
+		diff_time = run_time + (0xffffffff - last_run_time + 1);
+	}
+	last_run_time = run_time;
 
-  if( diff_time == 0 ) {
-    free(readbuf);
-    return false;
-  } else {
-    free(readbuf);
-    *reading = ((float)diff_energy / (float)diff_time * 0.06103515625); // energy / unit time
-    return true;
-  }
+	if (diff_time == 0) {
+		free(readbuf);
+		return false;
+	} else {
+		free(readbuf);
+		*reading = ((float)diff_energy / (float)diff_time *
+			    0.06103515625); // energy / unit time
+		return true;
+	}
 }
--- common/util/util_sys.c
+++ common/util/util_sys.c
@@ -11,45 +11,51 @@
 
 static bool is_boot_ACon = 0;
 
-void set_boot_source() {
-  uint32_t sys_rst_evt;
+void set_boot_source()
+{
+	uint32_t sys_rst_evt;
 
-  sys_rst_evt = sys_read32(SYS_RST_EVT_LOG_REG);
-  is_boot_ACon = sys_rst_evt & 0x1;
-  sys_write32(SRST_POWER_ON_SET, SYS_RST_EVT_LOG_REG);
+	sys_rst_evt = sys_read32(SYS_RST_EVT_LOG_REG);
+	is_boot_ACon = sys_rst_evt & 0x1;
+	sys_write32(SRST_POWER_ON_SET, SYS_RST_EVT_LOG_REG);
 }
 
-bool get_boot_source_ACon() {
-  return is_boot_ACon;
+bool get_boot_source_ACon()
+{
+	return is_boot_ACon;
 }
 /* get bic boot source through from SRST */
 
 /* bic warm reset work */
 #define bic_warm_reset_delay 100
 
-void bic_warm_reset() {
-  pal_warm_reset_prepare();
-  k_msleep(bic_warm_reset_delay);
-  sys_reboot(SYS_REBOOT_WARM);
+void bic_warm_reset()
+{
+	pal_warm_reset_prepare();
+	k_msleep(bic_warm_reset_delay);
+	sys_reboot(SYS_REBOOT_WARM);
 }
 
 K_WORK_DEFINE(bic_warm_reset_work, bic_warm_reset);
-void submit_bic_warm_reset() {
-  k_work_submit(&bic_warm_reset_work);
+void submit_bic_warm_reset()
+{
+	k_work_submit(&bic_warm_reset_work);
 }
 /* bic warm reset work */
 
 /* bic cold reset work */
 #define bic_cold_reset_delay 100
 
-void bic_cold_reset() {
-  pal_cold_reset_prepare();
-  k_msleep(bic_cold_reset_delay);
-  sys_reboot(SYS_REBOOT_COLD);
+void bic_cold_reset()
+{
+	pal_cold_reset_prepare();
+	k_msleep(bic_cold_reset_delay);
+	sys_reboot(SYS_REBOOT_COLD);
 }
 
 K_WORK_DEFINE(bic_cold_reset_work, bic_cold_reset);
-void submit_bic_cold_reset() {
-  k_work_submit(&bic_cold_reset_work);
+void submit_bic_cold_reset()
+{
+	k_work_submit(&bic_cold_reset_work);
 }
 /* bic cold reset work */
