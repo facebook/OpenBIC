2b1c86e37911720aa13d99e35af8e054b1f880f5
Peter Fetros <goldenbug@fb.com>
2022-02-15 17:14:52 -0800

    Clang Format Yv3.5 BB ipmi Files
    Summary: Apply clang-format to all ipmi files in Yv3.5 BB
    
    Reviewed By: garnermic
    
    Differential Revision: D34256110
    
    fbshipit-source-id: 1939675

--- meta-facebook/yv35-bb/src/ipmi/include/ipmi_def.h
+++ meta-facebook/yv35-bb/src/ipmi/include/ipmi_def.h
@@ -1,27 +1,26 @@
 #ifndef IPMI_DEF_H
 #define IPMI_DEF_H
 
-#define DEVICE_ID                  0x00
-#define DEVICE_REVISION            0x80
-#define FIRMWARE_REVISION_1        0x01
-#define FIRMWARE_REVISION_2        0x01
-#define IPMI_VERSION               0x02
-#define ADDITIONAL_DEVICE_SUPPORT  0xBF
-#define PRODUCT_ID                 0x0000
-#define AUXILIARY_FW_REVISION      0x00000000
+#define DEVICE_ID 0x00
+#define DEVICE_REVISION 0x80
+#define FIRMWARE_REVISION_1 0x01
+#define FIRMWARE_REVISION_2 0x01
+#define IPMI_VERSION 0x02
+#define ADDITIONAL_DEVICE_SUPPORT 0xBF
+#define PRODUCT_ID 0x0000
+#define AUXILIARY_FW_REVISION 0x00000000
 
-#define BIC_FW_YEAR_MSB   0x20
-#define BIC_FW_YEAR_LSB   0x22
-#define BIC_FW_WEEK       0x01
-#define BIC_FW_VER        0x01
+#define BIC_FW_YEAR_MSB 0x20
+#define BIC_FW_YEAR_LSB 0x22
+#define BIC_FW_WEEK 0x01
+#define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x62 // char: b
 #define BIC_FW_platform_1 0x62 // char: b
 #define BIC_FW_platform_2 0x00 // char: '\0'
 
 // firmware update interface
-#define BIC_UPDATE  0x02
-#define UPDATE_EN   0x80
+#define BIC_UPDATE 0x02
+#define UPDATE_EN 0x80
 #define GET_TEST_RESULT 0
-
 
 #endif
