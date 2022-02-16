063657d755c946b262fb47d08634245baa84a62b
Peter Fetros <goldenbug@fb.com>
2022-02-15 17:55:59 -0800

    Clang Format Yv3.5 CL main File
    Summary: Apply Clang Format to the yv5-cl main file.
    
    Reviewed By: garnermic
    
    Differential Revision: D34256507
    
    fbshipit-source-id: f862dee

--- meta-facebook/yv35-cl/src/main.c
+++ meta-facebook/yv35-cl/src/main.c
@@ -16,58 +16,56 @@
 #include "kcs.h"
 #include "plat_func.h"
 
-void device_init() {
-  adc_init();
-  peci_init();
-  hsc_init();
+void device_init()
+{
+	adc_init();
+	peci_init();
+	hsc_init();
 }
 
-void switch_spi_mux() {
-  gpio_set(FM_SPI_PCH_MASTER_SEL_R, GPIO_LOW);
+void switch_spi_mux()
+{
+	gpio_set(FM_SPI_PCH_MASTER_SEL_R, GPIO_LOW);
 }
 
-void set_sys_status() {
-  gpio_set(BIC_READY, GPIO_HIGH);
-  set_DC_status();
-  set_DC_on_5s_status();
-  set_DC_off_10s_status();
-  set_post_status();
-  set_post_thread();
-  set_SCU_setting();
+void set_sys_status()
+{
+	gpio_set(BIC_READY, GPIO_HIGH);
+	set_DC_status();
+	set_DC_on_5s_status();
+	set_DC_off_10s_status();
+	set_post_status();
+	set_post_thread();
+	set_SCU_setting();
 }
 
 void main(void)
 {
-  uint8_t proj_stage = (FIRMWARE_REVISION_1 & 0xf0) >> 4;
-  printk("Hello, wellcome to yv35 craterlake %x%x.%x.%x\n", BIC_FW_YEAR_MSB, BIC_FW_YEAR_LSB, BIC_FW_WEEK, BIC_FW_VER);
+	uint8_t proj_stage = (FIRMWARE_REVISION_1 & 0xf0) >> 4;
+	printk("Hello, wellcome to yv35 craterlake %x%x.%x.%x\n", BIC_FW_YEAR_MSB, BIC_FW_YEAR_LSB,
+	       BIC_FW_WEEK, BIC_FW_VER);
 
-  util_init_timer();
-  util_init_I2C();
+	util_init_timer();
+	util_init_I2C();
 
-  set_sys_config();
-  disable_PRDY_interrupt();
-  sensor_init();
-  FRU_init();
-  ipmi_init();
-  kcs_init();
-  usb_dev_init();
-  device_init();
-  set_sys_status();
-  set_ME_restore();
+	set_sys_config();
+	disable_PRDY_interrupt();
+	sensor_init();
+	FRU_init();
+	ipmi_init();
+	kcs_init();
+	usb_dev_init();
+	device_init();
+	set_sys_status();
+	set_ME_restore();
 }
 
 #define DEF_PROJ_GPIO_PRIORITY 78
 
-DEVICE_DEFINE(PRE_DEF_PROJ_GPIO, "PRE_DEF_PROJ_GPIO_NAME",
-        &gpio_init, NULL,
-        NULL, NULL,
-        POST_KERNEL, DEF_PROJ_GPIO_PRIORITY,
-        NULL);
+DEVICE_DEFINE(PRE_DEF_PROJ_GPIO, "PRE_DEF_PROJ_GPIO_NAME", &gpio_init, NULL, NULL, NULL,
+	      POST_KERNEL, DEF_PROJ_GPIO_PRIORITY, NULL);
 
 #define SWITCH_SPI_MUX_PRIORITY 81 // right after spi driver init
 
-DEVICE_DEFINE(PRE_SWITCH_SPI_MUX, "PRE_SWITCH_SPI_MUX_NAME",
-        &switch_spi_mux, NULL,
-        NULL, NULL,
-        POST_KERNEL, SWITCH_SPI_MUX_PRIORITY,
-        NULL);
+DEVICE_DEFINE(PRE_SWITCH_SPI_MUX, "PRE_SWITCH_SPI_MUX_NAME", &switch_spi_mux, NULL, NULL, NULL,
+	      POST_KERNEL, SWITCH_SPI_MUX_PRIORITY, NULL);
