fe2b74c58dae67d8076e7ce6b0c89e3ae67d3482
Lora Lin <Lora_Lin@wiwynn.com>
2022-02-23 20:45:55 -0500

    fby3.5: cl:Add KCS kernel config check before calling KCS APIs Summary: - Add to check the KCS kernel configuration is enabled or not before we call KCS API in common layer code.
    Test Plan:
    - Build code: Pass
    - Test send ipmi command from host: Pass
    
    Log:
    [root@Stream8_211112 ~]# ipmitool raw 0x06 0x01
     20 81 16 07 02 bf 15 a0 00 46 31 01 00 00 00

--- common/ipmi/ipmi.c
+++ common/ipmi/ipmi.c
@@ -196,7 +196,6 @@ ipmi_error IPMI_handler(void *arug0, void *arug1, void *arug2)
 {
 	uint8_t i;
 	ipmi_msg_cfg msg_cfg;
-	uint8_t *kcs_buff;
 
 	while (1) {
 		k_msgq_get(&ipmi_msgq, &msg_cfg, K_FOREVER);
@@ -285,8 +284,9 @@ ipmi_error IPMI_handler(void *arug0, void *arug1, void *arug2)
 
 			if (msg_cfg.buffer.InF_source == BMC_USB_IFs) {
 				USB_write(&msg_cfg.buffer);
-			} else if (msg_cfg.buffer.InF_source == HOST_KCS_IFs) {
 #ifdef CONFIG_IPMI_KCS_ASPEED
+			} else if (msg_cfg.buffer.InF_source == HOST_KCS_IFs) {
+				uint8_t *kcs_buff;
 				kcs_buff = malloc(KCS_buff_size * sizeof(uint8_t));
 				if (kcs_buff == NULL) { // allocate fail, retry allocate
 					k_msleep(10);
@@ -320,7 +320,6 @@ ipmi_error IPMI_handler(void *arug0, void *arug1, void *arug2)
 				if (kcs_buff != NULL)
 					free(kcs_buff);
 #endif
-
 			} else {
 				status = ipmb_send_response(
 					&msg_cfg.buffer,
