d8d3271686f1af3fb1544fe9368b60bc18148bcb
SaraLin-wiwynn <Sara_SY_Lin@wiwynn.com>
2022-03-15 05:21:02 -0400

    fby3.5: bb: Version commit for oby35-bb-2022.11.01
    Summary:
    - Version commit for Yv3.5 Baseboard BIC oby35-bb-2022.11.01.
    - Remove unused parameters.
    - Modify baseboard BIC revision.
    
    Test plan:
    - Build code: Pass
    
    Log:
    1. Get firmware revision.
    root@bmc-oob:~# bic-util slot1 0xE0 0x02 0x9C 0x9C 0x00 0x10 0x18 0x01
    9C 9C 00 10 07 01 00 00 80 12 01 02 BF 9C 9C 00
    00 00 00 00 00 00
    
    2. Get firmware version
    root@bmc-oob:~# fw-util slot1 --version bb_bic
    BB Bridge-IC Version: oby35-bb-v2022.11.01
    
    root@bmc-oob:~# bic-util slot1 0xE0 0x02 0x9C 0x9C 0x00 0x10 0xE0 0x0B 0x9C 0x9C 0x00 0x02
    9C 9C 00 10 39 0B 00 9C 9C 00 20 22 11 01 62 62
    00

--- meta-facebook/yv35-bb/src/ipmi/include/ipmi_def.h
+++ meta-facebook/yv35-bb/src/ipmi/include/ipmi_def.h
@@ -3,7 +3,9 @@
 
 #define DEVICE_ID 0x00
 #define DEVICE_REVISION 0x80
-#define FIRMWARE_REVISION_1 0x01
+// Bit 0 boade: 01h CL  02h BB
+// Bit 1 stage: 00h POC 01h EVT
+#define FIRMWARE_REVISION_1 0x12
 #define FIRMWARE_REVISION_2 0x01
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
@@ -12,7 +14,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x22
-#define BIC_FW_WEEK 0x01
+#define BIC_FW_WEEK 0x11
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x62 // char: b
 #define BIC_FW_platform_1 0x62 // char: b
