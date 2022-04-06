5fa1338152809a586494bafb4cb845ecae90c5a8
Lora Lin <Lora_Lin@wiwynn.com>
2022-04-06 06:49:26 -0400

    fby3.5: cl:Add read and write BIC register command
    Summary:
    - Add OEM command to read and write BIC register
    
    Test Plan:
    - Build Code: Pass
    - Command Test: Pass
    
    Log:
    read register 0x7e7b0300
    [BIC console]
    uart:~$ md 0x7e7b0300
    
    [7e7b0300] 00000001
    [BMC console]
    root@bmc-oob:~# bic-util slot1 0xe0 0x68 0x9c 0x9c 0x00 0x00 0x03 0x7b 0x7e 0x4
    9C 9C 00 01 00 00 00
    
    write 2 bytes 0x2211 to register 0x7e7b0300
    [BMC console]
    root@bmc-oob:~# bic-util slot1 0xe0 0x69 0x9c 0x9c 0x00 0x00 0x03 0x7b 0x7e 0x2 0x11 0x22
    9C 9C 00
    
    read register 0x7e7b0300
    [BIC console]
    uart:~$ md 0x7e7b0300
    
    [7e7b0300] 00002211
    [BMC console]
    root@bmc-oob:~# bic-util slot1 0xe0 0x68 0x9c 0x9c 0x00 0x00 0x03 0x7b 0x7e 0x4
    9C 9C 00 11 22 00 00

--- common/ipmi/oem_1s_handler.c
+++ common/ipmi/oem_1s_handler.c
@@ -799,6 +799,71 @@ __weak void OEM_1S_12V_CYCLE_SLOT(ipmi_msg *msg)
 	return;
 }
 
+__weak void OEM_1S_READ_BIC_REGISTER(ipmi_msg *msg)
+{
+	/*********************************
+	* data 0~3: start of register address to read, LSB first
+	* data 4  : bytes to read
+	***********************************/
+	if (!msg) {
+		printf("pal_OEM_1S_READ_BIC_REGISTER: parameter msg is NULL\n");
+		msg->completion_code = CC_UNSPECIFIED_ERROR;
+		return;
+	}
+	if (msg->data_len != 5) {
+		msg->completion_code = CC_INVALID_LENGTH;
+		return;
+	}
+	uint32_t *addr = (uint32_t *)(msg->data[0] | (msg->data[1] << 8) | (msg->data[2] << 16) |
+				      (msg->data[3] << 24));
+	uint8_t read_len = msg->data[4];
+	memcpy(&msg->data[0], addr, read_len);
+
+	msg->data_len = read_len;
+	msg->completion_code = CC_SUCCESS;
+	return;
+}
+
+__weak void OEM_1S_WRITE_BIC_REGISTER(ipmi_msg *msg)
+{
+	/*********************************
+ 	* data 0~3: start of register address to write, LSB first
+ 	* data 4  : bytes to write, <= 4 bytes
+	* data 5~N: date to write, LSB first
+	*
+	* NOTE: The register address must be a multiple of 4
+	***********************************/
+	if (!msg) {
+		printf("pal_OEM_1S_WRITE_BIC_REGISTER: parameter msg is NULL\n");
+		msg->completion_code = CC_UNSPECIFIED_ERROR;
+		return;
+	}
+	if (msg->data[4] < 1 || msg->data[4] > 4 || (msg->data_len != 5 + msg->data[4])) {
+		msg->completion_code = CC_INVALID_LENGTH;
+		return;
+	}
+
+	uint32_t addr =
+		msg->data[0] | (msg->data[1] << 8) | (msg->data[2] << 16) | (msg->data[3] << 24);
+	if (addr % 4) {
+		msg->completion_code = CC_INVALID_DATA_FIELD;
+		return;
+	}
+
+	uint8_t write_len = msg->data[4];
+	uint32_t reg_data = *(uint32_t *)addr;
+	/* replace write_len bytes */
+	reg_data &= ~BIT_MASK(write_len * 8);
+	for (uint8_t i = 0; i < write_len; i++) {
+		reg_data |= msg->data[i + 5] << (i * 8);
+	}
+	*(uint32_t *)addr = reg_data;
+
+	msg->data_len = 0;
+	msg->completion_code = CC_SUCCESS;
+	return;
+}
+
 void IPMI_OEM_1S_handler(ipmi_msg *msg)
 {
 	if (msg == NULL) {
@@ -845,6 +910,12 @@ void IPMI_OEM_1S_handler(ipmi_msg *msg)
 		break;
 	case CMD_OEM_1S_12V_CYCLE_SLOT:
 		OEM_1S_12V_CYCLE_SLOT(msg);
+		break;
+	case CMD_OEM_1S_READ_BIC_REGISTER:
+		OEM_1S_READ_BIC_REGISTER(msg);
+		break;
+	case CMD_OEM_1S_WRITE_BIC_REGISTER:
+		OEM_1S_WRITE_BIC_REGISTER(msg);
 		break;
 #ifdef CONFIG_IPMI_KCS_ASPEED
 	case CMD_OEM_1S_GET_POST_CODE:
