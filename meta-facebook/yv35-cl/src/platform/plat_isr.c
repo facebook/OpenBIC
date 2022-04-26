9579dfaa3f066a13332a3c621c53c3758eaced44
Ren Chen <ren_chen@wiwynn.com>
2022-04-26 13:06:15 -0700

    fby3.5: cl:[sensors] Add power/ME status check and clear cache when invalid (#250)
    Summary:
    fby3.5: cl:[sensors] Add power/ME status check and clear cache when invalid
    - Add power status check for power on sensors before/after sensor reading to avoid BMC accidentally reading unexpectedly cache data.
    - Block BIC reading PCH temperature through ME during BIC firmware update and reset.
    
    Dependency: https://github.com/facebook/OpenBIC/issues/249
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/250
    
    Test Plan:
    - Build code: Pass
    - AC/DC cycle: Pass
    - BIC firmware update/reset: Pass
    
    Log:
    Before block BIC reading PCH temperature during BIC firmware update.
    
    Hello, wellcome to yv35 craterlake 2022.6.51
    peed:BIC class type(class-1), 1ou present status(0), 2ou present status(0), board revision(0x2)
    [00:00:00.057,000] <inf> spi_aspeed: Flash data is monotonous, skip calibration.
    [00:00:00.232,000] <inf> usb_cdc_acm: Device suspended
    uart:~$ I2C 2 master write retry reach max
    [00:00:00.478,000] <inf> usb_cdc_acm: Device resumed
    [00:00:00.478,000] <inf> usb_cdc_acm: from suspend
    [00:00:00.782,000] <inf> usb_cdc_acm: Device configured
    [00:00:00.478,000] <inf> usb_cdc_acm: Device resumed
    [00:00:00.478,000] <inf> usb_cdc_acm: from suspend
    [00:00:00.782,000] <inf> usb_cdc_acm: Device configured
    uart:~$ I2C 2 master write retry reach max
    pch_read ipmb read fail, ret 8
    I2C 2 master write retry reach max
    I2C 2 master write retry reach max
    I2C 2 master write retry reach max
    I2C 2 master write retry reach max
    [IPMB_TXTask] Failed to send command
    [IPMB_TXTask] Failed to send a request message, from(0) to(1) netfn(0x4) cmd(0x2d)
    I2C 2 master write retry reach max
    I2C 2 master write retry reach max
    pch_read ipmb read fail, ret 8
    I2C 2 master write retry reach max
    I2C 2 master write retry reach max
    
    uart:~$
    
    After block BIC reading PCH temperature during BIC firmware update.
    
    Hello, wellcome to yv35 craterlake 2022.6.51
    peed:BIC class type(class-1), 1ou present status(0), 2ou present status(0), board revision(0x2)
    [00:00:00.058,000] <inf> spi_aspeed: Flash data is monotonous, skip calibration.
    [00:00:00.233,000] <inf> usb_cdc_acm: Device suspended
    uart:~$ [00:00:00.394,000] <inf> usb_cdc_acm: Device resumed
    [00:00:00.394,000] <inf> usb_cdc_acm: from suspend
    [00:00:00.696,000] <inf> usb_cdc_acm: Device configured
    [00:00:00.394,000] <inf> usb_cdc_acm: Device resumed
    [00:00:00.394,000] <inf> usb_cdc_acm: from suspend
    [00:00:00.696,000] <inf> usb_cdc_acm: Device configured
    uart:~$
    uart:~$
    
    Reviewed By: garnermic
    
    Differential Revision: D35941590
    
    Pulled By: GoldenBug
    
    fbshipit-source-id: 85823716d1ded1aecdfb34c924deccce989790d7

--- meta-facebook/yv35-cl/src/platform/plat_isr.c
+++ meta-facebook/yv35-cl/src/platform/plat_isr.c
@@ -90,7 +90,6 @@ void ISR_DC_ON()
 		set_DC_off_delayed_status();
 	} else {
 		set_DC_on_delayed_status();
-		clear_unaccessible_sensor_cache();
 		k_work_schedule(&set_DC_off_10s_work, K_SECONDS(DC_OFF_10_SECOND));
 
 		if ((gpio_get(FM_SLPS3_PLD_N) == GPIO_HIGH) &&
