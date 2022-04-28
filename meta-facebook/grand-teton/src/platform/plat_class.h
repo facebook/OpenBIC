ac8b6c895374b7aa13d5bc3db770c9a7b62de5b7
Yang.Chen <Yang.Chen@quantatw.com>
2022-04-28 16:27:29 +0800

    GT: Remove unused function in this project
    Summary:
    - Remove some function that to check 1ou/2ou status use in yv35-cl
    - Remove pal_fix_full_sdr_table()

--- meta-facebook/grand-teton/src/platform/plat_class.h
+++ meta-facebook/grand-teton/src/platform/plat_class.h
@@ -19,20 +19,4 @@ enum BIC_BOARD_REVISION {
 	SYS_BOARD_MP_EFUSE,
 };
 
-enum BIC_CLASS_TYPE {
-	TYPE_2OU_EXP = 0x1,
-	TYPE_2OU_SPE = 0x2,
-	TYPE_2OU_DPV2 = 0x77,
-	TYPE_2OU_DPV2_8 = 0x7,
-	TYPE_2OU_DPV2_16 = 0x70,
-	TYPE_UNKNOWN = 0xFF,
-};
-
-uint8_t get_system_class();
-bool get_1ou_status();
-bool get_2ou_status();
-uint8_t get_board_revision();
-uint8_t get_2ou_cardtype();
-float get_hsc_type_adc_voltage();
-
 #endif
