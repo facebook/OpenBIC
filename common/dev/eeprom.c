316c8b3f6806ecefc989601a98e19bdf5e9b6d25
Yang.Chen <Yang.Chen@quantatw.com>
2022-05-05 10:14:06 +0800

    Modify eeprom read/write function
    Summary:
    - Add member in structure EEPROM_CFG check whether need to change mux before access EEPROM
    - Add check mux function check whether need to access i2c mux first before access EEPROM

--- common/dev/eeprom.c
+++ common/dev/eeprom.c
@@ -3,27 +3,51 @@
 #include "hal_i2c.h"
 #include <string.h>
 
+bool eeprom_mux_check(EEPROM_ENTRY *entry)
+{
+	if (entry == NULL) {
+		return false;
+	}
+	I2C_MSG msg;
+	uint8_t retry = 5;
+	if (entry->config.have_mux) {
+		msg.bus = entry->config.port;
+		msg.target_addr = entry->config.mux_addr;
+		msg.tx_len = 1;
+		msg.data[0] = (1 << (entry->config.mux_channel));
+		return ((i2c_master_write(&msg, retry) == 0) ? true : false);
+	} else {
+		return true;
+	}
+}
+
 bool eeprom_write(EEPROM_ENTRY *entry)
 {
 	if (entry == NULL) {
 		return false;
 	}
-
 	I2C_MSG msg;
 	uint8_t retry = 5;
+	uint8_t count = 0;
 
-	msg.bus = entry->config.port;
-	msg.target_addr = entry->config.target_addr;
-	msg.tx_len = entry->data_len + 2; // write 2 byte offset to EEPROM
-	msg.data[0] = ((entry->config.start_offset + entry->offset) >> 8) & 0xFF; // offset msb
-	msg.data[1] = (entry->config.start_offset + entry->offset) & 0xFF; // offset lsb
-	memcpy(&msg.data[2], &entry->data[0], entry->data_len);
+	do {
+		/* Check if there have a MUX before EEPROM and access it to change channel first */
+		if (eeprom_mux_check(entry) == false)
+			continue;
 
-	if (i2c_master_write(&msg, retry)) {
-		return false;
-	}
+		msg.bus = entry->config.port;
+		msg.target_addr = entry->config.target_addr;
+		msg.tx_len = entry->data_len + 2; // write 2 byte offset to EEPROM
+		msg.data[0] =
+			((entry->config.start_offset + entry->offset) >> 8) & 0xFF; // offset msb
+		msg.data[1] = (entry->config.start_offset + entry->offset) & 0xFF; // offset lsb
+		memcpy(&msg.data[2], &entry->data[0], entry->data_len);
 
-	return true;
+		if (i2c_master_write(&msg, retry) == 0)
+			break;
+	} while (count++ < retry);
+
+	return ((count == retry) ? false : true);
 }
 
 bool eeprom_read(EEPROM_ENTRY *entry)
@@ -34,20 +58,27 @@ bool eeprom_read(EEPROM_ENTRY *entry)
 
 	I2C_MSG msg;
 	uint8_t retry = 5;
+	uint8_t count = 0;
 
-	msg.bus = entry->config.port;
-	msg.target_addr = entry->config.target_addr;
-	msg.tx_len = 2; // write 2 byte offset to EEPROM
-	msg.rx_len = entry->data_len;
-	msg.data[0] = ((entry->config.start_offset + entry->offset) >> 8) & 0xFF; // offset msb
-	msg.data[1] = (entry->config.start_offset + entry->offset) & 0xFF; // offset lsb
+	do {
+		/* Check if there have a MUX before EEPROM and access it to change channel first */
+		if (eeprom_mux_check(entry) == false)
+			continue;
 
-	if (i2c_master_read(&msg, retry)) {
-		return false;
-	}
+		msg.bus = entry->config.port;
+		msg.target_addr = entry->config.target_addr;
+		msg.tx_len = 2; // write 2 byte offset to EEPROM
+		msg.rx_len = entry->data_len;
+		msg.data[0] =
+			((entry->config.start_offset + entry->offset) >> 8) & 0xFF; // offset msb
+		msg.data[1] = (entry->config.start_offset + entry->offset) & 0xFF; // offset lsb
 
-	entry->data_len = msg.rx_len;
-	memcpy(&entry->data, &msg.data, msg.rx_len);
+		if (i2c_master_read(&msg, retry) == 0) {
+			entry->data_len = msg.rx_len;
+			memcpy(&entry->data, &msg.data, msg.rx_len);
+			break;
+		}
+	} while (count++ < retry);
 
-	return true;
+	return ((count == retry) ? false : true);
 }
