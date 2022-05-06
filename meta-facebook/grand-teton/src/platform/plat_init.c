1610d032d089a6e65c137288d8e8afe7e79713fc
Yang.Chen <Yang.Chen@quantatw.com>
2022-05-06 21:41:03 +0800

    GT: Add scu config to disable gpio internal pull-up
    Summary:
    - Disable GPIOK0-GPIOK7 internal pull-up for NIC4-NIC7 i2c with level shift
    - Solve the issue with the level shift can't recognize when SDA pulls to low

--- meta-facebook/grand-teton/src/platform/plat_init.c
+++ meta-facebook/grand-teton/src/platform/plat_init.c
@@ -8,25 +8,29 @@
 
 SCU_CFG scu_cfg[] = {
 	//register    value
+	{ 0x7e6e2618, 0x00FF0000 },
 };
 
 void pal_pre_init()
 {
-  /* init i2c slave */
+	/* init i2c slave */
 	for (int index = 0; index < MAX_SLAVE_NUM; index++) {
 		if (I2C_SLAVE_ENABLE_TABLE[index])
-			i2c_slave_control(index, (struct _i2c_slave_config *)&I2C_SLAVE_CONFIG_TABLE[index], 1);
+			i2c_slave_control(
+				index, (struct _i2c_slave_config *)&I2C_SLAVE_CONFIG_TABLE[index],
+				1);
 	}
+	scu_init(scu_cfg, sizeof(scu_cfg) / sizeof(SCU_CFG));
 }
 
 void pal_post_init()
 {
-  plat_mctp_init();
+	plat_mctp_init();
 }
 
 void pal_set_sys_status()
 {
-  gpio_set(BIC_SYS_READY_N, GPIO_LOW);
+	gpio_set(BIC_SYS_READY_N, GPIO_LOW);
 }
 
 #define DEF_PROJ_GPIO_PRIORITY 78
