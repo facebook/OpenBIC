5d811bd96cdd49139a84ed67364b8221bc6ce8db
SaraLin-wiwynn <Sara_SY_Lin@wiwynn.com>
2022-05-24 22:27:58 -0400

    fby3.5: rf: Initialize rainbow fall BIC
    Summary:
    - Initialize Rainbow Falls BIC.
    - Create project config, dts files, file header, and CMake file.
    - Add project code in meta-facebook/yv35-rf/.
    
    Test plan:
    - Build code: Pass
    - Can access BIC console: Pass
    
    Log:
    1. Can build pass.
    openbic$ west build -p auto -b ast1030_evb meta-facebook/yv35-rf
    -- west build: generating a build system
    ...
    [244/244] Linking C executable zephyr/Y35BRF.elf
    Memory region         Used Size  Region Size  %age Used
             SRAM_NC:         12 KB       320 KB      3.75%
               FLASH:          0 GB         0 GB
                SRAM:      343752 B       448 KB     74.93%
            IDT_LIST:          0 GB         2 KB      0.00%
    248284+0 records in
    248284+0 records out
    248284 bytes (248 kB, 242 KiB) copied, 0.759969 s, 327 kB/s
    0+0 records in
    0+0 records out
    0 bytes copied, 5.4698e-05 s, 0.0 kB/s
    
    2. Can access BIC console.
    *** Booting Zephyr OS build v00.01.04-5-g70b0b6ecc605  ***
    Hello, welcome to yv35 Rainbow Falls 2022.20.e3
    Function pal_fix_full_sdr_table is not implemented
    The SDR number is equal to 0
    ipmi_init

---
+++ meta-facebook/yv35-rf/src/platform/plat_ipmb.h
@@ -0,0 +1,8 @@
+#ifndef PLAT_IPMB_H
+#define PLAT_IPMB_H
+
+#define SELF_I2C_ADDRESS 0x20
+
+#define MAX_IPMB_IDX 2
+
+#endif
---
+++ meta-facebook/yv35-rf/src/platform/plat_sdr_table.c
@@ -0,0 +1,6 @@
+#include "plat_sdr_table.h"
+
+uint8_t load_sdr_table(void)
+{
+	return 0;
+};
