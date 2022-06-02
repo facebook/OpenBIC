2bc40c5143bbb1c3bf29d4eef21bd7f52d7696bd
Lora Lin <Lora_Lin@wiwynn.com>
2022-06-01 20:13:50 -0400

    fby3.5: cl: Version commit for oby35-cl-2022.22.01
    Summary:
    - Version commit for Yv3.5 Craterlake BIC oby35-cl-2022.22.01.
    
    Test Plan:
    - Build code: Pass
    - Check BIC version is changed: Pass
    
    Log:
    root@bmc-oob:~# fw-util slot1 --version bic
    SB Bridge-IC Version: oby35-cl-v2022.22.01

--- meta-facebook/yv35-cl/src/platform/plat_version.h
+++ meta-facebook/yv35-cl/src/platform/plat_version.h
@@ -7,7 +7,7 @@
 #define DEVICE_ID 0x00
 #define DEVICE_REVISION 0x80
 #define FIRMWARE_REVISION_1 0x11
-#define FIRMWARE_REVISION_2 0x12
+#define FIRMWARE_REVISION_2 0x13
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
 #define PRODUCT_ID 0x0000
@@ -15,7 +15,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x22
-#define BIC_FW_WEEK 0x19
+#define BIC_FW_WEEK 0x22
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x63 // char: c
 #define BIC_FW_platform_1 0x6c // char: l
