22828399f3414dda358154e85b94f1fdc043aa69
marshallZhan <Marshall_Zhan@wiwynn.com>
2022-06-08 05:50:38 -0400

    fby3: dl: Setting the configuration of server board BIC Summary: - Add project code in meta-facebook/yv3-dl/. - Setting yv3 basic configuration.
    Test Plan:
    - Build code : This will be tested after a few commit.

---
+++ meta-facebook/yv3-dl/src/platform/plat_version.h
@@ -0,0 +1,24 @@
+#ifndef PLAT_VERSION_H
+#define PLAT_VERSION_H
+
+#define PLATFORM_NAME "yv3"
+#define PROJECT_NAME "deltalake"
+#define IANA_ID 0x009c9c
+#define DEVICE_ID 0x00
+#define DEVICE_REVISION 0x80
+#define FIRMWARE_REVISION_1 0x01
+#define FIRMWARE_REVISION_2 0x01
+#define IPMI_VERSION 0x02
+#define ADDITIONAL_DEVICE_SUPPORT 0xBF
+#define PRODUCT_ID 0x0000
+#define AUXILIARY_FW_REVISION 0x00000000
+
+#define BIC_FW_YEAR_MSB 0x20
+#define BIC_FW_YEAR_LSB 0x22
+#define BIC_FW_WEEK 0x22
+#define BIC_FW_VER 0x01
+#define BIC_FW_platform_0 0x64 // char: d
+#define BIC_FW_platform_1 0x6c // char: l
+#define BIC_FW_platform_2 0x00 // char: '\0'
+
+#endif
