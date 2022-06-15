dbb990c8bf1bea0f37ead59f66e781ae0760d3cb
matt <matt.lin@quantatw.com>
2022-06-15 14:55:32 +0800

    Remove Improperly Configured KCONFIG Settings

--- meta-facebook/yv35-wf/src/platform/plat_version.h
+++ meta-facebook/yv35-wf/src/platform/plat_version.h
@@ -6,9 +6,9 @@
 #define IANA_ID 0x009c9c
 #define DEVICE_ID 0x00
 #define DEVICE_REVISION 0x80
-// BIT 0:3  1: CraterLake 2: Baseboard 3: Rainbow falls
+// BIT 0:3  1: CraterLake 2: Baseboard 3: Rainbow falls 4: Waimano falls
 // BIT 4:7  0: POC 1: EVT 2: DVT
-#define FIRMWARE_REVISION_1 0x03
+#define FIRMWARE_REVISION_1 0x04
 #define FIRMWARE_REVISION_2 0x01
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
@@ -19,7 +19,7 @@
 #define BIC_FW_YEAR_LSB 0x22
 #define BIC_FW_WEEK 0x22
 #define BIC_FW_VER 0x00 
-#define BIC_FW_platform_0 0x77 // char: r
+#define BIC_FW_platform_0 0x77 // char: w
 #define BIC_FW_platform_1 0x66 // char: f
 #define BIC_FW_platform_2 0x00 // char: '\0'
 
