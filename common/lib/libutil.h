3e380d44f7b9d02a42a34329a9b643a777802e16
Jerry-wiwynn <jerry_c_chen@wiwynn.com>
2022-06-16 05:46:35 -0400

    common: ltc4282: support ein reading Summary: Support ltc4282 ein reading.
    Test plan:
    Build and test pass on fby35
    
    1. Bind ein reading on sensor number: 0xD8 and check reading is correct
    root@bmc-oob:~# sensor-util slot1 | grep 0xD8
    BB_MEDUSA_PWR                (0xD8) :   62.67 Watts | (ok)

--- common/lib/libutil.h
+++ common/lib/libutil.h
@@ -11,6 +11,10 @@
 		p = NULL;                                                                          \
 	}
 
+#define SETBIT(x, y) (x | (1ULL << y))
+#define GETBIT(x, y) ((x & (1ULL << y)) > y)
+#define CLEARBIT(x, y) (x & (~(1ULL << y)))
+
 ipmi_msg construct_ipmi_message(uint8_t seq_source, uint8_t netFn, uint8_t command,
 				uint8_t source_inft, uint8_t target_inft, uint16_t data_len,
 				uint8_t *data);
