b0abb55ded0d79c91c8d7c96747c80d9b172c9b5
Peter Fetros <goldenbug@fb.com>
2022-06-22 16:41:31 -0700

    Fix Warning for Incorrect printf Argument Type.
    Summary:
    Fix a warning about the improper type for a print statement containing an
    unsigned int instead of an unsigned long.
    
    Reviewed By: garnermic
    
    Differential Revision: D37364407
    
    fbshipit-source-id: c81ab2d40030a36652f86e1951699b7634334e72

--- common/lib/util_spi.c
+++ common/lib/util_spi.c
@@ -76,8 +76,8 @@ static int do_update(const struct device *flash_device, off_t offset, uint8_t *b
 	bool update_it = false;
 
 	if (flash_sz < flash_offset + len) {
-		printf("ERROR: update boundary exceeds flash size. (%u, %u, %lu)\n", flash_sz,
-		       flash_offset, len);
+		printf("ERROR: update boundary exceeds flash size. (%u, %u, %u)\n", flash_sz,
+		       flash_offset, (unsigned int)len);
 		ret = -EINVAL;
 		goto end;
 	}
