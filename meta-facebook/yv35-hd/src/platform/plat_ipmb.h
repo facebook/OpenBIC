0179db3b33dd8bc5b3e73968785a0b5b0352ebd4
Eason Chen <EasonChen1@quantatw.com>
2022-07-06 16:58:09 +0800

    fby3.5: hd: Support IPMB
    Summary:
    - Support IPMB handler.
    
    Test plan:
    - Build code: Pass
    - Check IPMB is workable: Pass
    
    LOG:
    ```
    root@bmc-oob:~# bic-util slot1 0x18 0x1
    00 80 FF FF 02 BF 15 A0 00 00 00 00 00 00 00
    ```

--- meta-facebook/yv35-hd/src/platform/plat_ipmb.h
+++ meta-facebook/yv35-hd/src/platform/plat_ipmb.h
@@ -4,8 +4,20 @@
 #include "plat_i2c.h"
 #include "ipmb.h"
 
+#define IPMB_BMC_BUS I2C_BUS7
+#define IPMB_EXP1_BUS I2C_BUS8
+#define IPMB_EXP2_BUS I2C_BUS9
+
+#define BMC_I2C_ADDRESS 0x10
 #define SELF_I2C_ADDRESS 0x20
-#define MAX_IPMB_IDX 2
+#define BIC1_I2C_ADDRESS 0x20
+#define BIC2_I2C_ADDRESS 0x20
+#define MAX_IPMB_IDX 4
+
+enum { BMC_IPMB_IDX,
+       EXP1_IPMB_IDX,
+       EXP2_IPMB_IDX,
+};
 
 extern IPMB_config pal_IPMB_config_table[];
 #endif
