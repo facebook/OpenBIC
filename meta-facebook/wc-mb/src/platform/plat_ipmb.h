5dc6aa78661551eeb16b045b1095fb2f651b65de
MouchenHung <Mouchen.Hung@quantatw.com>
2022-07-06 17:31:41 +0800

    wc: mb: Support ipmb
    [Features]
    - Add IPMB table to enable ipmb funciton, support BIC<->BMC/ME
    
    [BugFix]
    - Modify some codes for build pass(follow with current OpenBIC main
    branch).
    
    [Note]
    - none

--- meta-facebook/wc-mb/src/platform/plat_ipmb.h
+++ meta-facebook/wc-mb/src/platform/plat_ipmb.h
@@ -1,6 +1,23 @@
 #ifndef PLAT_IPMB_H
 #define PLAT_IPMB_H
 
-#define SELF_I2C_ADDRESS 0x20
+#include "plat_i2c.h"
+#include "ipmb.h"
 
+#define IPMB_BMC_BUS I2C_BUS7
+#define IPMB_PEER_BMC_BUS I2C_BUS8
+#define IPMB_ME_BUS I2C_BUS3
+
+#define BMC_I2C_ADDRESS 0x10
+#define PEER_BMC_I2C_ADDRESS 0x10
+#define ME_I2C_ADDRESS 0x16
+#define SELF_I2C_ADDRESS 0x20
+#define MAX_IPMB_IDX 3
+
+enum { BMC_IPMB_IDX,
+       ME_IPMB_IDX,
+       PEER_BMC_IPMB_IDX,
+};
+
+extern IPMB_config pal_IPMB_config_table[];
 #endif
--- meta-facebook/wc-mb/src/platform/plat_sensor_table.c
+++ meta-facebook/wc-mb/src/platform/plat_sensor_table.c
@@ -4,7 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 
-uint8_t load_sensor_config(void)
+uint8_t plat_get_config_size()
 {
 	return 0;
+}
+
+void load_sensor_config(void)
+{
 }
