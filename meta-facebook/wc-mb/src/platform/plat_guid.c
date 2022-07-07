20dad823dcf231861ba97f4ad08323c445b7eab8
MouchenHung <Mouchen.Hung@quantatw.com>
2022-07-07 11:11:48 +0800

    wc: mb: Support GUID
    [Features]
    - Support GUID.
    
    [BugFix]
    - none
    
    [Note]
    - To enable GUID read command, "CONFIG_ESPI" need to turn on, then
    sensor number "SENSOR_NUM_PWR_HSCIN" need to be defined for build
    success.

---
+++ meta-facebook/wc-mb/src/platform/plat_guid.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include "guid.h"
+#include "plat_guid.h"
+#include "fru.h"
+
+#define MB_GUID_PORT 0x01
+#define MB_GUID_ADDR 0x50
+
+const EEPROM_CFG guid_config[] = {
+	{
+		NV_ATMEL_24C128,
+		MB_SYS_GUID_ID,
+		MB_GUID_PORT,
+		MB_GUID_ADDR,
+		GUID_ACCESS_BYTE,
+		GUID_START,
+		GUID_SIZE,
+	},
+};
+
+uint8_t get_system_guid(uint16_t *data_len, uint8_t *data)
+{
+	EEPROM_ENTRY guid_entry;
+
+	guid_entry.offset = 0;
+	guid_entry.data_len = 16;
+	guid_entry.config.dev_id = MB_SYS_GUID_ID;
+
+	uint8_t status = GUID_read(&guid_entry);
+	if (status == GUID_READ_SUCCESS) {
+		*data_len = guid_entry.data_len;
+		memcpy(data, &guid_entry.data, guid_entry.data_len);
+	}
+
+	return status;
+}
+
+uint8_t set_system_guid(uint16_t *data_len, uint8_t *data)
+{
+	EEPROM_ENTRY guid_entry;
+
+	guid_entry.offset = 0;
+	guid_entry.data_len = *data_len;
+	guid_entry.config.dev_id = MB_SYS_GUID_ID;
+	memcpy(&guid_entry.data[0], data, guid_entry.data_len);
+	return GUID_write(&guid_entry);
+}
