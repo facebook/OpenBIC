1ebaa7ae31b2195c716dac7a448c0eb72ba7c14f
MouchenHung <Mouchen.Hung@quantatw.com>
2022-07-07 10:43:50 +0800

    wc: mb: Support FRU

--- meta-facebook/wc-mb/src/platform/plat_fru.h
+++ meta-facebook/wc-mb/src/platform/plat_fru.h
@@ -1,6 +1,12 @@
 #ifndef PLAT_FRU_H
 #define PLAT_FRU_H
 
+#define MB_FRU_PORT 0x01
+#define MB_FRU_ADDR 0x50
+
+#define IOM_FRU_PORT 0x07
+#define IOM_FRU_ADDR 0x50
+
 enum { MB_FRU_ID,
        IOM_FRU_ID,
        // OTHER_FRU_ID,
