d89ee8df850b0686533d794118e265e949fba902
Eason Chen <EasonChen1@quantatw.com>
2022-07-08 14:25:45 -0700

    fby3.5: hd: Support IPMB (#378)
    Summary:
    - Support IPMB handler.
    
    Dependency: https://github.com/facebook/OpenBIC/issues/377
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/378
    
    Test Plan:
    - Build code: Pass
    - Check IPMB is workable: Pass
    
    LOG:
    ```
    root@bmc-oob:~# bic-util slot1 0x18 0x1
    00 80 FF FF 02 BF 15 A0 00 00 00 00 00 00 00
    ```
    
    Reviewed By: garnermic
    
    Differential Revision: D37679073
    
    Pulled By: GoldenBug
    
    fbshipit-source-id: 1733c51fca36362abe08074b60f91f9d915b6645

---
+++ meta-facebook/yv35-hd/src/platform/plat_ipmb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "plat_ipmb.h"
+
+IPMB_config pal_IPMB_config_table[] = {
+	// index, interface, channel, bus, channel_target_address, enable_status, self_address,
+	// rx_thread_name, tx_thread_name
+	{ BMC_IPMB_IDX, I2C_IF, BMC_IPMB, IPMB_I2C_BMC, BMC_I2C_ADDRESS, ENABLE, SELF_I2C_ADDRESS,
+	  "RX_BMC_IPMB_TASK", "TX_BMC_IPMB_TASK" },
+	{ EXP1_IPMB_IDX, I2C_IF, EXP1_IPMB, IPMB_EXP1_BUS, BIC1_I2C_ADDRESS, DISABLE,
+	  SELF_I2C_ADDRESS, "RX_EPX1_IPMB_TASK", "TX_EXP1_IPMB_TASK" },
+	{ EXP2_IPMB_IDX, I2C_IF, EXP2_IPMB, IPMB_EXP2_BUS, BIC2_I2C_ADDRESS, DISABLE,
+	  SELF_I2C_ADDRESS, "RX_EPX2_IPMB_TASK", "TX_EXP2_IPMB_TASK" },
+	{ RESERVED_IDX, RESERVED_IF, RESERVED, RESERVED_BUS, RESERVED_ADDRESS, DISABLE,
+	  RESERVED_ADDRESS, "RESERVED_ATTR", "RESERVED_ATTR" },
+};
+
+bool pal_load_ipmb_config(void)
+{
+	memcpy(IPMB_config_table, pal_IPMB_config_table, sizeof(pal_IPMB_config_table));
+	return true;
+}
