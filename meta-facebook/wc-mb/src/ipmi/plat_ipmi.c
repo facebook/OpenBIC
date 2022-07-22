32688203b84e68337c09765be2762714d1e6a17e
MouchenHung <Mouchen.Hung@quantatw.com>
2022-07-22 11:25:03 +0800

    wc: mb: Modify oem command get set gpio (NetFn:0x38 Cmd:0x41)

--- meta-facebook/wc-mb/src/ipmi/plat_ipmi.c
+++ meta-facebook/wc-mb/src/ipmi/plat_ipmi.c
@@ -7,6 +7,21 @@
 #include "libutil.h"
 #include "ipmi.h"
 #include "plat_ipmb.h"
+#include "hal_gpio.h"
+
+#define GET_BIT_VAL_1(val, n) ((val & BIT(n)) >> (n))
+#define REG_GPIO_BASE 0x7e780000
+
+enum { CMD_GPIO_GET, CMD_GPIO_SET };
+
+static uint32_t GPIO_GROUP_REG_ACCESS[GPIO_GROUP_NUM] = {
+	REG_GPIO_BASE + 0x00, /* GPIO_A/B/C/D Data Value Register */
+	REG_GPIO_BASE + 0x20, /* GPIO_E/F/G/H Data Value Register */
+	REG_GPIO_BASE + 0x70, /* GPIO_I/J/K/L Data Value Register */
+	REG_GPIO_BASE + 0x78, /* GPIO_M/N/O/P Data Value Register */
+	REG_GPIO_BASE + 0x80, /* GPIO_Q/R/S/T Data Value Register */
+	REG_GPIO_BASE + 0x88, /* GPIO_U Data Value Register */
+};
 
 bool add_sel_evt_record(addsel_msg_t *sel_msg)
 {
@@ -70,4 +85,103 @@ bool add_sel_evt_record(addsel_msg_t *sel_msg)
 	default:
 		return true;
 	}
+}
+
+/*
+  - Name: OEM_1S_GET_SET_GPIO
+  - Description: OEM command NetFn:0x38 Cmd:0x41
+  - Request:
+		Byte 1:3 - MFG ID
+		Byte 4 - option
+			0x00 - get gpio status
+			0x01 - set gpio status
+		Byte 5 - gpio number
+		Byte 6 - (if Byte4 = 0x01)set value
+  - Response:
+		Byte 1 - Completion Code
+		Byte 2:4 - MFG ID
+		Byte 5 - gpio number
+		Byte 6 - gpio direction
+		Byte 7 - gpio value
+*/
+void OEM_1S_GET_SET_GPIO(ipmi_msg *msg)
+{
+	if (msg == NULL) {
+		return;
+	}
+
+	uint8_t completion_code = CC_INVALID_LENGTH;
+
+	if (msg->data_len < 2 || msg->data_len > 3) {
+		msg->completion_code = completion_code;
+		return;
+	}
+
+	if (msg->data[0] == CMD_GPIO_GET) {
+		if (msg->data_len != 2) {
+			msg->completion_code = CC_INVALID_LENGTH;
+			return;
+		}
+	} else if (msg->data[0] == CMD_GPIO_SET) {
+		if (msg->data_len != 3) {
+			msg->completion_code = CC_INVALID_LENGTH;
+			return;
+		}
+	}
+
+	uint8_t gpio_idx = msg->data[1];
+
+	if (gpio_idx >= GPIO_CFG_SIZE || gpio_idx < 0) {
+		completion_code = CC_PARAM_OUT_OF_RANGE;
+		goto exit;
+	}
+
+	if (gpio_cfg[gpio_idx].is_init == DISABLE) {
+		completion_code = CC_INVALID_DATA_FIELD;
+		goto exit;
+	}
+
+	uint8_t val = 0xFF;
+	uint8_t dir = 0xFF;
+	uint32_t g_val = sys_read32(GPIO_GROUP_REG_ACCESS[gpio_idx / 32]);
+	uint32_t g_dir = sys_read32(GPIO_GROUP_REG_ACCESS[gpio_idx / 32] + 0x4);
+
+	val = GET_BIT_VAL_1(g_val, gpio_idx % 32);
+	if (g_dir & BIT(gpio_idx % 32))
+		dir = 0x01;
+	else
+		dir = 0x00;
+
+	switch (msg->data[0]) {
+	case CMD_GPIO_GET:
+		if (msg->data_len == 2) {
+			msg->data[0] = gpio_idx;
+			msg->data[1] = dir;
+			msg->data[2] = val;
+			completion_code = CC_SUCCESS;
+		}
+		break;
+	case CMD_GPIO_SET:
+		if (msg->data_len == 3) {
+			msg->data[0] = gpio_idx;
+			gpio_set(gpio_idx, msg->data[2]);
+			g_dir = sys_read32(GPIO_GROUP_REG_ACCESS[gpio_idx / 32] + 0x4);
+			msg->data[1] = (g_dir & BIT(gpio_idx % 32)) >> (gpio_idx % 32);
+			msg->data[2] = gpio_get(gpio_idx);
+			completion_code = CC_SUCCESS;
+		}
+		break;
+	default:
+		printf("[%s] Unknown options(0x%x)", __func__, msg->data[0]);
+		return;
+	}
+
+exit:
+	if (completion_code != CC_SUCCESS) {
+		msg->data_len = 0;
+	} else {
+		msg->data_len = 3;
+	}
+	msg->completion_code = completion_code;
+	return;
 }
