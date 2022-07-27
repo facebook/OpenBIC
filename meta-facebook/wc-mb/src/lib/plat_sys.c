7c96fad3fbffbf2442d2dd4676f9b2ae2ca1eab0
MouchenHung <Mouchen.Hung@quantatw.com>
2022-07-26 23:04:38 -0700

    wc: mb: Support bmc reset (#430)
    Summary:
    - Support OEM reset BMC command.
    
    Dependency: https://github.com/facebook/OpenBIC/issues/426
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/430
    
    Test Plan:
    - Build code: PASS
    - Test reset BMC command from HOST: Pass
    
    Log:
    ```
    [root@localhost ~]# ipmitool raw 0x38 0x16 0x15 0xa0 0x00
     15 a0 00
    ```
    
    Reviewed By: garnermic
    
    Differential Revision: D38128130
    
    Pulled By: GoldenBug
    
    fbshipit-source-id: f23dc6822d6fb5858594b6600ff5e64eeb04aa21

---
+++ meta-facebook/wc-mb/src/lib/plat_sys.c
@@ -0,0 +1,21 @@
+#include "plat_sys.h"
+
+#include "util_sys.h"
+#include "hal_gpio.h"
+#include "plat_gpio.h"
+
+/* BMC reset */
+void BMC_reset_handler()
+{
+	gpio_set(RST_BMC_R_N, GPIO_LOW);
+	k_msleep(10);
+	gpio_set(RST_BMC_R_N, GPIO_HIGH);
+}
+
+K_WORK_DELAYABLE_DEFINE(BMC_reset_work, BMC_reset_handler);
+int pal_submit_bmc_cold_reset()
+{
+	k_work_schedule(&BMC_reset_work, K_MSEC(1000));
+	return 0;
+}
+/* BMC reset */
