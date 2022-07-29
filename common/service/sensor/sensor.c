8b2b5c22387c58682d796ce64219d4d88a43e1b7
MouchenHung <Mouchen.Hung@quantatw.com>
2022-07-29 16:02:59 +0800

    Fix clang-format problem

--- common/service/sensor/sensor.c
+++ common/service/sensor/sensor.c
@@ -38,9 +38,9 @@ static bool sensor_poll_enable_flag = true;
 
 static bool is_sensor_ready_flag = false;
 
-const int negative_ten_power[16] = { 1,     1,		1,	 1,	1,       1,
-				     1,     1000000000, 100000000, 10000000, 1000000, 100000,
-				     10000, 1000,       100,       10 };
+const int negative_ten_power[16] = { 1,	    1,		1,	   1,	     1,	      1,
+				     1,	    1000000000, 100000000, 10000000, 1000000, 100000,
+				     10000, 1000,	100,	   10 };
 
 sensor_cfg *sensor_config;
 uint8_t sensor_config_count;
