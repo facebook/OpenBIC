43c9e71b8a8dbe82060f5848236031c334847f0b
HungYi <Hung-Yi.Li@quantatw.com>
2022-08-11 14:17:52 -0700

    fby3: vf: modify plat_class & plat_gpio (#448)
    Summary:
    - Modify plat_class
    - Modify plat_gpio
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/448
    
    Test Plan:
    - Build code : This will be tested after a few commit.
    
    Dependency: https://github.com/facebook/OpenBIC/issues/447
    
    Reviewed By: peterdelevoryas
    
    Differential Revision: D38376965
    
    Pulled By: GoldenBug
    
    fbshipit-source-id: 0b23db9c2bdec1991d082627f1a860ce6b393efc

--- meta-facebook/yv3-vf/src/platform/plat_class.c
+++ meta-facebook/yv3-vf/src/platform/plat_class.c
@@ -2,8 +2,41 @@
 #include "hal_gpio.h"
 #include "plat_class.h"
 
+static uint8_t system_board_id = 0;
 static uint8_t e1s_hsc_config = 0;
 static uint8_t e1s_adc_config = 0;
+
+#define VERNAL_FALLS_BOARD_TYPE 0x07
+
+void init_sys_board_id(uint8_t board_id)
+{
+	switch (board_id) {
+	case RAINBOW_FALLS:
+		system_board_id = RAINBOW_FALLS;
+		break;
+	case VERNAL_FALLS:
+		system_board_id = VERNAL_FALLS_BOARD_TYPE;
+		break;
+	default:
+		printf("[%s] input board id not support: 0x%x\n", __func__, board_id);
+		system_board_id = UNKNOWN_BOARD;
+	}
+}
+
+void init_platform_config()
+{
+	uint8_t board_id = 0;
+	board_id = ((gpio_get(BOARD_ID_BIT_3) << 3) | (gpio_get(BOARD_ID_BIT_2) << 2) |
+		    (gpio_get(BOARD_ID_BIT_1) << 1) | (gpio_get(BOARD_ID_BIT_0) << 0));
+
+	init_sys_board_id(board_id);
+	printf("[%s] board id 0x%x\n", __func__, system_board_id);
+}
+
+uint8_t get_board_id()
+{
+	return system_board_id;
+}
 
 void init_e1s_config()
 {
