2d020e9eb3917955fc5bc32c9bc5cfe09ab49025
Eason Chen <EasonChen1@quantatw.com>
2022-08-15 14:36:08 -0700

    fby3.5: hd: Add ISR (#488)
    Summary:
    - Remove HSC OCP setting and use default setting(600W).
      Because 320W CPU is used currently, if we set HSC OCP to 350W it will trigger HSC CSOUT during power on.
    - Add ISR to record the following SEL to BMC.
    
    | sensor type | sensor number      | event data                  | condition                                                                                          |
    |-------------|--------------------|-----------------------------|----------------------------------------------------------------------------------------------------|
    | C3h         | 56h(POWER_ERROR)   | 01FFFFh(SYS_PWROK_FAIL)     | asserted when PWRGD_CPU_LVC3 is low and FM_CPU_BIC_SLP_S3_N is high.                               |
    | C9h         | 10h(SYSTEM_STATUS) | 00FFFFh(SYS_THERMAL_TRIP)   | asserted when FM_CPU_BIC_THERMTRIP_N falls                                                         |
    | C9h         | 10h(SYSTEM_STATUS) | 02FFFFh(SYS_THROTTLE)       | asserted/deassert when FM_CPU_BIC_PROCHOT_LVT3_N falls/rises                                       |
    | C9h         | 10h(SYSTEM_STATUS) | 05FFFFh(SYS_PMBUSALERT)     | asserted/deassert when IRQ_HSC_ALERT1_N falls/rises                                                |
    | C9h         | 10h(SYSTEM_STATUS) | 06FFFFh(SYS_HSCTIMER)       | asserted/deassert when FM_HSC_TIMER rises/falls                                                    |
    | C9h         | 10h(SYSTEM_STATUS) | 07FFFFh(SYS_FIRMWAREASSERT) | asserted/deassert when FAST_PROCHOT_N falls/rises                                                  |
    | C9h         | 10h(SYSTEM_STATUS) | 0AFFFFh(SYS_VRWATCHDOG)     | asserted when PWRGD_CPU_LVC3 is low after FM_CPU_BIC_SLP_S3_N is high of 10 second.                |
    | CBh         | 71h(VR_OCP)        |        byte 1: VR number<br>0 - PVDDCR_CPU0<br> 1 - PVDDCR_CPU1<br>2 - PVDD11_S3 <br>byte 2: FFh<br>byte 3: FFh       | asserted/deassert when PVDDCR_CPU0_BIC_OCP_N, PVDDCR_CPU1_BIC_OCP_N, PVDD11_S3_BIC_OCP_N fall/rise |
    | CBh         | 72h(VR_ALERT)      |        byte 1: VR number and page<br>bit [7:1] - VR number<br>bit [0] - page<br>byte 2: VR STATUS_WORD byte 0<br>byte 3: VR STATUS_WORD byte 1        | asserted/deassert when PVDDCR_CPU0_PMALERT_N, PVDDCR_CPU1_PMALERT_N, PVDD11_S3_PMALERT_N fall/rise |
    | CCh         | BDh(HDT_PRESENT)   | FFFFFFh | asserted/deassert when FM_DBP_PRESENT_N falls/rises                                                |
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/488
    
    Test Plan: Build code: Pass
    
    Reviewed By: williamspatrick
    
    Differential Revision: D38708679
    
    Pulled By: GoldenBug
    
    fbshipit-source-id: 20950ba8ab263422df16165f7ee73fd04196db14

--- meta-facebook/yv35-hd/src/platform/plat_init.c
+++ meta-facebook/yv35-hd/src/platform/plat_init.c
@@ -25,6 +25,7 @@ void pal_set_sys_status()
 {
 	set_DC_status(PWRGD_CPU_LVC3);
 	set_DC_on_delayed_status();
+	set_DC_off_delayed_status();
 	set_post_status(FM_BIOS_POST_CMPLT_BIC_N);
 	set_CPU_power_status(PWRGD_CPU_LVC3);
 	set_sys_ready_pin(BIC_READY);
--- meta-facebook/yv35-hd/src/platform/plat_isr.c
+++ meta-facebook/yv35-hd/src/platform/plat_isr.c
@@ -1,9 +1,13 @@
+#include <stdlib.h>
 #include "plat_isr.h"
 #include "plat_gpio.h"
 #include "plat_sensor_table.h"
 #include "libipmi.h"
 #include "power_status.h"
 #include "ipmi.h"
+#include "plat_class.h"
+#include "plat_i2c.h"
+#include "pmbus.h"
 
 void ISR_POST_COMPLETE()
 {
@@ -11,13 +15,335 @@ void ISR_POST_COMPLETE()
 }
 
 K_WORK_DELAYABLE_DEFINE(set_DC_on_5s_work, set_DC_on_delayed_status);
+K_WORK_DELAYABLE_DEFINE(set_DC_off_10s_work, set_DC_off_delayed_status);
 #define DC_ON_5_SECOND 5
+#define DC_OFF_10_SECOND 10
 void ISR_DC_ON()
 {
 	set_DC_status(PWRGD_CPU_LVC3);
 	if (get_DC_status() == true) {
 		k_work_schedule(&set_DC_on_5s_work, K_SECONDS(DC_ON_5_SECOND));
+		if (k_work_cancel_delayable(&set_DC_off_10s_work) != 0) {
+			printf("[%s] Failed to cancel set dc off delay work.\n", __func__);
+		}
+		set_DC_off_delayed_status();
 	} else {
+		k_work_schedule(&set_DC_off_10s_work, K_SECONDS(DC_OFF_10_SECOND));
+		if (k_work_cancel_delayable(&set_DC_on_5s_work) != 0) {
+			printf("[%s] Failed to cancel set dc on delay work.\n", __func__);
+		}
 		set_DC_on_delayed_status();
+
+		if ((gpio_get(FM_CPU_BIC_SLP_S3_N) == GPIO_HIGH) &&
+		    (gpio_get(RST_RSMRST_BMC_N) == GPIO_HIGH)) {
+			common_addsel_msg_t sel_msg;
+			sel_msg.InF_target = BMC_IPMB;
+			sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_OEM_C3;
+			sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+			sel_msg.sensor_number = SENSOR_NUM_POWER_ERROR;
+			sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_PWROK_FAIL;
+			sel_msg.event_data2 = 0xFF;
+			sel_msg.event_data3 = 0xFF;
+			if (!common_add_sel_evt_record(&sel_msg)) {
+				printf("[%s] Failed to add system PWROK failure sel.\n", __func__);
+			}
+		}
+	}
+}
+
+static void SLP3_handler()
+{
+	common_addsel_msg_t sel_msg;
+	if ((gpio_get(FM_CPU_BIC_SLP_S3_N) == GPIO_HIGH) &&
+	    (gpio_get(PWRGD_CPU_LVC3) == GPIO_LOW)) {
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+		sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+		sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_VRWATCHDOG;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			printf("[%s] Failed to add VR watchdog timeout sel.\n", __func__);
+		}
+	}
+}
+
+K_WORK_DELAYABLE_DEFINE(SLP3_work, SLP3_handler);
+void ISR_SLP3()
+{
+	if (gpio_get(FM_CPU_BIC_SLP_S3_N) == GPIO_HIGH) {
+		printf("slp3\n");
+		k_work_schedule(&SLP3_work, K_MSEC(10000));
+		return;
+	} else {
+		if (k_work_cancel_delayable(&SLP3_work) != 0) {
+			printf("[%s] Failed to cancel delayable work.\n", __func__);
+		}
+	}
+}
+
+void ISR_DBP_PRSNT()
+{
+	common_addsel_msg_t sel_msg;
+	if ((gpio_get(FM_DBP_PRESENT_N) == GPIO_HIGH)) {
+		sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSART;
+	} else {
+		sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+	}
+	sel_msg.InF_target = BMC_IPMB;
+	sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_HDT;
+	sel_msg.sensor_number = SENSOR_NUM_HDT_PRESENT;
+	sel_msg.event_data1 = 0xFF;
+	sel_msg.event_data2 = 0xFF;
+	sel_msg.event_data3 = 0xFF;
+	if (!common_add_sel_evt_record(&sel_msg)) {
+		printf("[%s] Failed to add HDT present sel.\n", __func__);
+	}
+}
+
+void ISR_HSC_THROTTLE()
+{
+	common_addsel_msg_t sel_msg;
+	static bool is_hsc_throttle_assert = false; // Flag for filt out fake alert
+	if (gpio_get(RST_RSMRST_BMC_N) == GPIO_HIGH) {
+		if ((gpio_get(PWRGD_CPU_LVC3) == GPIO_LOW) &&
+		    (get_DC_off_delayed_status() == false)) {
+			return;
+		} else {
+			if ((gpio_get(IRQ_HSC_ALERT1_N) == GPIO_HIGH) &&
+			    (is_hsc_throttle_assert == true)) {
+				sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSART;
+				is_hsc_throttle_assert = false;
+			} else if ((gpio_get(IRQ_HSC_ALERT1_N) == GPIO_LOW) &&
+				   (is_hsc_throttle_assert == false)) {
+				sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+				is_hsc_throttle_assert = true;
+			} else { // Fake alert
+				return;
+			}
+
+			sel_msg.InF_target = BMC_IPMB;
+			sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+			sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+			sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_PMBUSALERT;
+			sel_msg.event_data2 = 0xFF;
+			sel_msg.event_data3 = 0xFF;
+			if (!common_add_sel_evt_record(&sel_msg)) {
+				printf("[%s] Failed to add HSC Throttle sel.\n", __func__);
+			}
+		}
+	}
+}
+
+void ISR_MB_THROTTLE()
+{
+	common_addsel_msg_t sel_msg;
+	if (gpio_get(RST_RSMRST_BMC_N) == GPIO_HIGH) {
+		if (gpio_get(FAST_PROCHOT_N) == GPIO_HIGH) {
+			sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSART;
+		} else {
+			sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		}
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+		sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+		sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_FIRMWAREASSERT;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			printf("[%s] Failed to add MB Throttle sel.\n", __func__);
+		}
+	}
+}
+
+void ISR_SOC_THMALTRIP()
+{
+	common_addsel_msg_t sel_msg;
+	if (gpio_get(RST_PLTRST_BIC_N) == GPIO_HIGH) {
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+		sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+		sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_THERMAL_TRIP;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			printf("[%s] Failed to add SOC Thermal trip sel.\n", __func__);
+		}
+	}
+}
+
+void ISR_SYS_THROTTLE()
+{
+	common_addsel_msg_t sel_msg;
+	if ((gpio_get(RST_PLTRST_BIC_N) == GPIO_HIGH) && (gpio_get(PWRGD_CPU_LVC3) == GPIO_HIGH)) {
+		if (gpio_get(FM_CPU_BIC_PROCHOT_LVT3_N) == GPIO_HIGH) {
+			sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSART;
+		} else {
+			sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		}
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+		sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+		sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_THROTTLE;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			printf("[%s] Failed to add System Throttle sel.\n", __func__);
+		}
+	}
+}
+
+void ISR_HSC_OC()
+{
+	common_addsel_msg_t sel_msg;
+	if (gpio_get(RST_RSMRST_BMC_N) == GPIO_HIGH) {
+		if (gpio_get(FM_HSC_TIMER) == GPIO_LOW) {
+			sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSART;
+		} else {
+			sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		}
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_SYS_STA;
+		sel_msg.sensor_number = SENSOR_NUM_SYSTEM_STATUS;
+		sel_msg.event_data1 = IPMI_OEM_EVENT_OFFSET_SYS_HSCTIMER;
+		sel_msg.event_data2 = 0xFF;
+		sel_msg.event_data3 = 0xFF;
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			printf("[%s] Failed to add HSC OC sel.\n", __func__);
+		}
+	}
+}
+
+static void add_vr_ocp_sel(uint8_t gpio_num, uint8_t vr_num)
+{
+	common_addsel_msg_t sel_msg;
+	if (gpio_get(gpio_num) == GPIO_HIGH) {
+		sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSART;
+	} else {
+		sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+	}
+	sel_msg.InF_target = BMC_IPMB;
+	sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_VR;
+	sel_msg.sensor_number = SENSOR_NUM_VR_OCP;
+	sel_msg.event_data1 = vr_num;
+	sel_msg.event_data2 = 0xFF;
+	sel_msg.event_data3 = 0xFF;
+	if (!common_add_sel_evt_record(&sel_msg)) {
+		printf("[%s] Failed to add VR OCP sel.\n", __func__);
+	}
+}
+
+void ISR_PVDDCR_CPU0_OCP()
+{
+	if (get_DC_status() == true) {
+		add_vr_ocp_sel(PVDDCR_CPU0_BIC_OCP_N, 0);
+	}
+}
+
+void ISR_PVDDCR_CPU1_OCP()
+{
+	if (get_DC_status() == true) {
+		add_vr_ocp_sel(PVDDCR_CPU1_BIC_OCP_N, 1);
+	}
+}
+
+void ISR_PVDD11_S3_OCP()
+{
+	if (get_DC_status() == true) {
+		add_vr_ocp_sel(PVDD11_S3_BIC_OCP_N, 2);
+	}
+}
+
+static void add_vr_pmalert_sel(uint8_t gpio_num, uint8_t vr_addr, uint8_t vr_num)
+{
+	uint8_t retry = 5;
+	I2C_MSG *msg = (I2C_MSG *)malloc(sizeof(I2C_MSG));
+	if (msg == NULL) {
+		printf("[%s] Failed to allocate I2C_MSG.\n", __func__);
+		return;
+	}
+
+	for (int page = 0; page < 2; page++) {
+		msg->bus = I2C_BUS5;
+		msg->target_addr = vr_addr;
+		msg->tx_len = 2;
+		msg->data[0] = PMBUS_PAGE;
+		msg->data[1] = page;
+
+		if (i2c_master_write(msg, retry)) {
+			printf("[%s] Failed to write page.\n", __func__);
+			return;
+		}
+
+		msg->bus = I2C_BUS5;
+		msg->target_addr = vr_addr;
+		msg->tx_len = 1;
+		msg->rx_len = 2;
+		msg->data[0] = PMBUS_STATUS_WORD;
+
+		if (i2c_master_read(msg, retry)) {
+			printf("[%s] Failed to read PMBUS_STATUS_WORD.\n", __func__);
+			return;
+		}
+
+		common_addsel_msg_t sel_msg;
+		if (gpio_get(gpio_num) == GPIO_HIGH) {
+			sel_msg.event_type = IPMI_OEM_EVENT_TYPE_DEASSART;
+		} else {
+			sel_msg.event_type = IPMI_EVENT_TYPE_SENSOR_SPECIFIC;
+		}
+		sel_msg.InF_target = BMC_IPMB;
+		sel_msg.sensor_type = IPMI_OEM_SENSOR_TYPE_VR;
+		sel_msg.sensor_number = SENSOR_NUM_VR_ALERT;
+		sel_msg.event_data1 = (vr_num << 1) | (page & 0x01);
+		sel_msg.event_data2 = msg->data[0];
+		sel_msg.event_data3 = msg->data[1];
+		if (!common_add_sel_evt_record(&sel_msg)) {
+			printf("[%s] Failed to add VR PMALERT sel.\n", __func__);
+		}
+	}
+}
+
+void ISR_PVDDCR_CPU0_PMALERT()
+{
+	if (get_DC_status() == true) {
+		uint8_t board_rev = get_board_revision();
+		if (board_rev == SYS_BOARD_EVT_BOM2) {
+			add_vr_pmalert_sel(PVDDCR_CPU0_PMALERT_N, XDPE19283B_PVDDCR_CPU0_ADDR, 0);
+		} else if (board_rev == SYS_BOARD_EVT_BOM3) {
+			add_vr_pmalert_sel(PVDDCR_CPU0_PMALERT_N, MP2856GUT_PVDDCR_CPU0_ADDR, 0);
+		} else {
+			add_vr_pmalert_sel(PVDDCR_CPU0_PMALERT_N, RAA229621_PVDDCR_CPU0_ADDR, 0);
+		}
+	}
+}
+
+void ISR_PVDDCR_CPU1_PMALERT()
+{
+	if (get_DC_status() == true) {
+		uint8_t board_rev = get_board_revision();
+		if (board_rev == SYS_BOARD_EVT_BOM2) {
+			add_vr_pmalert_sel(PVDDCR_CPU1_PMALERT_N, XDPE19283B_PVDDCR_CPU1_ADDR, 1);
+		} else if (board_rev == SYS_BOARD_EVT_BOM3) {
+			add_vr_pmalert_sel(PVDDCR_CPU1_PMALERT_N, MP2856GUT_PVDDCR_CPU1_ADDR, 1);
+		} else {
+			add_vr_pmalert_sel(PVDDCR_CPU1_PMALERT_N, RAA229621_PVDDCR_CPU1_ADDR, 1);
+		}
+	}
+}
+
+void ISR_PVDD11_S3_PMALERT()
+{
+	if (get_DC_status() == true) {
+		uint8_t board_rev = get_board_revision();
+		if (board_rev == SYS_BOARD_EVT_BOM2) {
+			add_vr_pmalert_sel(PVDD11_S3_PMALERT_N, XDPE19283B_PVDD11_S3_ADDR, 2);
+		} else if (board_rev == SYS_BOARD_EVT_BOM3) {
+			add_vr_pmalert_sel(PVDD11_S3_PMALERT_N, MP2856GUT_PVDD11_S3_ADDR, 2);
+		} else {
+			add_vr_pmalert_sel(PVDD11_S3_PMALERT_N, RAA229621_PVDD11_S3_ADDR, 2);
+		}
 	}
 }
