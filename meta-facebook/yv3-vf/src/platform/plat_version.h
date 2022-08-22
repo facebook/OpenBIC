2009510f9fb5e917aaf518c58e054c9f2e82ee41
HungYi <Hung-Yi.Li@quantatw.com>
2022-08-22 10:41:55 +0800

    fby3: vf: Version commit for oby3-vf-2022.32.01
    Summary:
    - Version commit for Yv3 Vernalfall BIC oby3-vf-2022.32.01
    
    Test Plan:
    - Build code: Pass
    - Check BIC version: Pass

--- meta-facebook/yv3-vf/src/platform/plat_version.h
+++ meta-facebook/yv3-vf/src/platform/plat_version.h
@@ -9,28 +9,27 @@
 /*
  *  FIRMWARE REVISION_1 
  *  [bit 0-3] 
- *    1: CraterLake 
- *    2: Baseboard 
- *    3: Rainbow falls
- *    4. Vernal falls
+ *    7: Vernal falls
  *  [bit 4-7]
  *    0: POC 
  *    1: EVT 
  *    2: DVT
+ *    3: PVT
+ *    4: MP
  *  FIRMWARE_REVISION_2
  *    Count of release firmware at each stage.
  */
-#define FIRMWARE_REVISION_1 0xFF
-#define FIRMWARE_REVISION_2 0xFF
+#define FIRMWARE_REVISION_1 0x47
+#define FIRMWARE_REVISION_2 0x03
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
 #define PRODUCT_ID 0x0000
 #define AUXILIARY_FW_REVISION 0x00000000
 
-#define BIC_FW_YEAR_MSB 0xFF
-#define BIC_FW_YEAR_LSB 0xFF
-#define BIC_FW_WEEK 0xFF
-#define BIC_FW_VER 0xFF
+#define BIC_FW_YEAR_MSB 0x20
+#define BIC_FW_YEAR_LSB 0x22
+#define BIC_FW_WEEK 0x32
+#define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x76 // char: v
 #define BIC_FW_platform_1 0x66 // char: f
 #define BIC_FW_platform_2 0x00 // char: '\0'
