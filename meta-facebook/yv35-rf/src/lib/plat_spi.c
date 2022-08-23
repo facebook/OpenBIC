5d03ded237e91fdbb1f348dda0653b63a18a3d78
Scron Chang <Scron.Chang@quantatw.com>
2022-08-23 18:54:58 +0800

    fby3.5: rf: Impl CXL FW update
    Implement CXL conttroller FW update to RF BIC.
    The image to update is 32MB. The RF BIC checks the standby power,
    P1V8_ASIC, and the direction of the SPI switch and then writes data.
    
    Tested:
    Write different images for 20 times, and they are all successful.
    
    Signed-off-by: Scron Chang <Scron.Chang@quantatw.com>

--- meta-facebook/yv35-rf/src/lib/plat_spi.c
+++ meta-facebook/yv35-rf/src/lib/plat_spi.c
@@ -1,24 +1,91 @@
+#include <stdio.h>
+#include <plat_power_seq.h>
+
 #include "hal_gpio.h"
 #include "util_spi.h"
+#include "util_sys.h"
 #include "plat_gpio.h"
 
-bool pal_switch_cxl_spi_mux()
+#define CXL_FLASH_TO_BIC 1
+#define CXL_FLASH_TO_CXL 0
+
+#define CXL_UPDATE_MAX_OFFSET 0x2000000
+
+static bool switch_cxl_spi_mux(int gpio_status)
 {
-	// Switch CXL MUX selection pin to BIC
-	int ret = gpio_set(SPI_MASTER_SEL, GPIO_HIGH);
-	if (ret != 0) {
+	if (gpio_status != CXL_FLASH_TO_BIC && gpio_status != CXL_FLASH_TO_CXL) {
+		printf("[%s] Invalid argument\n", __func__);
 		return false;
 	}
 
-	// Enable CXL MUX
-	ret = gpio_set(FM_SPI_MUX_OE_CTL_N, GPIO_LOW);
-	if (ret != 0) {
+	if (gpio_set(SPI_MASTER_SEL, gpio_status)) {
+		printf("Fail to switch the flash to %s\n",
+		       (gpio_status == CXL_FLASH_TO_BIC) ? "BIC" : "PIONEER");
 		return false;
 	}
+
 	return true;
 }
 
-int pal_get_cxl_flash_position()
+static bool control_flash_power(int power_state)
 {
-	return DEVSPI_SPI1_CS0;
+	int control_mode = 0;
+
+	switch (power_state) {
+	case POWER_OFF:
+		control_mode = DISABLE_POWER_MODE;
+		break;
+	case POWER_ON:
+		control_mode = ENABLE_POWER_MODE;
+		break;
+	default:
+		return false;
+	}
+
+	for (int retry = 3;; retry--) {
+		if (gpio_get(P1V8_ASIC_PG_R) == power_state) {
+			return true;
+		}
+
+		if (!retry) {
+			break;
+		}
+
+		control_power_stage(control_mode, P1V8_ASIC_EN_R);
+		k_msleep(CHKPWR_DELAY_MSEC);
+	}
+
+	printf("Fail to %s the ASIC_1V8\n", (power_state == POWER_OFF) ? "disable" : "enable");
+
+	return false;
+}
+
+uint8_t fw_update_cxl(uint32_t offset, uint16_t msg_len, uint8_t *msg_buf, bool sector_end)
+{
+	uint8_t ret = FWUPDATE_UPDATE_FAIL;
+	bool last = ((offset + msg_len) == CXL_UPDATE_MAX_OFFSET);
+
+	if (offset > CXL_UPDATE_MAX_OFFSET) {
+		return FWUPDATE_OVER_LENGTH;
+	}
+
+	// Enable the P1V8_ASCI to power the flash
+	if (control_flash_power(POWER_ON) == false) {
+		return FWUPDATE_UPDATE_FAIL;
+	}
+
+	// Set high to choose the BIC as the host
+	if (switch_cxl_spi_mux(CXL_FLASH_TO_BIC) == false) {
+		printf("Fail to switch PIONEER flash to BIC\n");
+		return FWUPDATE_UPDATE_FAIL;
+	}
+
+	ret = fw_update(offset, msg_len, msg_buf, sector_end, DEVSPI_SPI1_CS0);
+
+	if (last || ret != FWUPDATE_SUCCESS) {
+		control_flash_power(POWER_OFF);
+		switch_cxl_spi_mux(CXL_FLASH_TO_CXL);
+	}
+
+	return ret;
 }
