82e3557708a65bee94dd397a897f10b2f464ed7f
MouchenHung <Mouchen.Hung@quantatw.com>
2022-09-06 16:48:33 -0700

    wc: mb: Add isr and power status log (#537)
    Summary:
    - Add debug log LOG_* to power_status.c
    - Add ISR debug log for Waimea Canyon
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/537
    
    Test Plan: - Build Code: PASS
    
    Reviewed By: zhdaniel12
    
    Differential Revision: D39284755
    
    Pulled By: GoldenBug
    
    fbshipit-source-id: f6163be585decffa7e61607e8a58317a6ef58bac

--- common/lib/power_status.c
+++ common/lib/power_status.c
@@ -1,9 +1,12 @@
 #include "power_status.h"
 
 #include <stdio.h>
+#include <logging/log.h>
 
 #include "hal_gpio.h"
 #include "snoop.h"
+
+LOG_MODULE_REGISTER(power_status);
 
 static bool is_DC_on = false;
 static bool is_DC_on_delayed = false;
@@ -15,7 +18,7 @@ static bool vr_monitor_status = true;
 void set_DC_status(uint8_t gpio_num)
 {
 	is_DC_on = (gpio_get(gpio_num) == 1) ? true : false;
-	printf("[%s] gpio number(%d) status(%d)\n", __func__, gpio_num, is_DC_on);
+	LOG_WRN("DC_STATUS: %s", (is_DC_on) ? "on" : "off");
 }
 
 bool get_DC_status()
@@ -46,7 +49,7 @@ bool get_DC_off_delayed_status()
 void set_post_status(uint8_t gpio_num)
 {
 	is_post_complete = (gpio_get(gpio_num) == 1) ? false : true;
-	printf("[%s] gpio number(%d) status(%d)\n", __func__, gpio_num, is_post_complete);
+	LOG_WRN("POST_COMPLETE: %s", (is_post_complete) ? "yes" : "no");
 }
 
 bool get_post_status()
@@ -57,6 +60,7 @@ bool get_post_status()
 void set_CPU_power_status(uint8_t gpio_num)
 {
 	is_CPU_power_good = gpio_get(gpio_num);
+	LOG_WRN("CPU_PWR_GOOD: %s", (is_CPU_power_good) ? "yes" : "no");
 }
 
 bool CPU_power_good()
--- meta-facebook/wc-mb/src/platform/plat_isr.c
+++ meta-facebook/wc-mb/src/platform/plat_isr.c
@@ -1,5 +1,6 @@
 #include "plat_isr.h"
 
+#include <logging/log.h>
 #include "libipmi.h"
 #include "kcs.h"
 #include "power_status.h"
@@ -12,6 +13,32 @@
 #include "hal_gpio.h"
 #include "util_sys.h"
 
+LOG_MODULE_REGISTER(plat_isr);
+
+static void isr_dbg_print(uint8_t gpio_num)
+{
+	switch (gpio_cfg[gpio_num].int_type) {
+	case GPIO_INT_EDGE_FALLING:
+		LOG_INF("gpio[%-3d] isr type[fall] trigger 1 -> 0", gpio_num);
+		break;
+
+	case GPIO_INT_EDGE_RISING:
+		LOG_INF("gpio[%-3d] isr type[rise] trigger 0 -> 1", gpio_num);
+		break;
+
+	case GPIO_INT_EDGE_BOTH:
+		if (gpio_get(gpio_num))
+			LOG_INF("gpio[%-3d] isr type[both] trigger 0 -> 1", gpio_num);
+		else
+			LOG_INF("gpio[%-3d] isr type[both] trigger 1 -> 0", gpio_num);
+		break;
+
+	default:
+		LOG_WRN("gpio[%-3d] isr trigger unexpected", gpio_num);
+		break;
+	}
+}
+
 void send_gpio_interrupt(uint8_t gpio_num)
 {
 	ipmb_error status;
@@ -19,7 +46,7 @@ void send_gpio_interrupt(uint8_t gpio_num)
 	uint8_t gpio_val;
 
 	gpio_val = gpio_get(gpio_num);
-	printf("Send gpio interrupt to BMC, gpio number(%d) status(%d)\n", gpio_num, gpio_val);
+	LOG_INF("Send gpio interrupt to BMC, gpio number(%d) status(%d)", gpio_num, gpio_val);
 
 	msg.data_len = 5;
 	msg.InF_source = SELF;
@@ -35,8 +62,8 @@ void send_gpio_interrupt(uint8_t gpio_num)
 
 	status = ipmb_read(&msg, IPMB_inf_index_map[msg.InF_target]);
 	if (status != IPMB_ERROR_SUCCESS) {
-		printf("Failed to send GPIO interrupt event to BMC, gpio number(%d) status(%d)\n",
-		       gpio_num, status);
+		LOG_ERR("Failed to send GPIO interrupt event to BMC, gpio number(%d) status(%d)",
+			gpio_num, status);
 	}
 }
 
@@ -52,7 +79,7 @@ static void SLP3_handler()
 		sel_msg.event_data2 = 0xFF;
 		sel_msg.event_data3 = 0xFF;
 		if (!common_add_sel_evt_record(&sel_msg)) {
-			printf("VR watchdog timeout addsel fail\n");
+			LOG_ERR("VR watchdog timeout addsel fail\n");
 		}
 	}
 }
@@ -60,18 +87,22 @@ static void SLP3_handler()
 K_WORK_DELAYABLE_DEFINE(SLP3_work, SLP3_handler);
 void ISR_SLP3()
 {
+	isr_dbg_print(FM_SLPS3_PLD_N);
+
 	if (gpio_get(FM_SLPS3_PLD_N) == GPIO_HIGH) {
-		printf("slp3\n");
+		LOG_INF("slp3");
 		k_work_schedule(&SLP3_work, K_MSEC(10000));
 		return;
 	}
 	if (k_work_cancel_delayable(&SLP3_work) != 0) {
-		printf("[%s] Failed to cancel delayable work\n", __func__);
+		LOG_ERR("[%s] Failed to cancel delayable work", __func__);
 	}
 }
 
 void ISR_POST_COMPLETE()
 {
+	isr_dbg_print(FM_BIOS_POST_CMPLT_BIC_N);
+
 	if (gpio_get(FM_BIOS_POST_CMPLT_BIC_N) == GPIO_LOW) { // Post complete
 		if (get_me_mode() == ME_INIT_MODE) {
 			init_me_firmware();
@@ -87,6 +118,7 @@ K_WORK_DELAYABLE_DEFINE(set_DC_off_10s_work, set_DC_off_delayed_status);
 #define DC_OFF_10_SECOND 10
 void ISR_DC_ON()
 {
+	isr_dbg_print(PWRGD_SYS_PWROK);
 	set_DC_status(PWRGD_SYS_PWROK);
 
 	if (get_DC_status() == true) {
@@ -94,7 +126,7 @@ void ISR_DC_ON()
 		k_work_schedule(&set_DC_on_5s_work, K_SECONDS(DC_ON_5_SECOND));
 
 		if (k_work_cancel_delayable(&set_DC_off_10s_work) != 0) {
-			printf("Cancel set dc off delay work fail\n");
+			LOG_ERR("Cancel set dc off delay work fail");
 		}
 		set_DC_off_delayed_status();
 	} else {
@@ -113,7 +145,7 @@ void ISR_DC_ON()
 			sel_msg.event_data2 = 0xFF;
 			sel_msg.event_data3 = 0xFF;
 			if (!common_add_sel_evt_record(&sel_msg)) {
-				printf("System PWROK failure addsel fail\n");
+				LOG_ERR("System PWROK failure addsel fail");
 			}
 		}
 	}
@@ -121,6 +153,7 @@ void ISR_DC_ON()
 
 void ISR_BMC_PRDY()
 {
+	isr_dbg_print(JTAG_DBP_BMC_PRDY_N);
 	send_gpio_interrupt(JTAG_DBP_BMC_PRDY_N);
 }
 
@@ -141,7 +174,7 @@ static void PROC_FAIL_handler(struct k_work *work)
 		sel_msg.event_data3 = 0xFF;
 		ret = common_add_sel_evt_record(&sel_msg);
 		if (!ret) {
-			printf("Fail to assert FRE3 event log.\n");
+			LOG_ERR("Fail to assert FRE3 event log");
 		}
 	}
 }
@@ -150,7 +183,9 @@ K_WORK_DELAYABLE_DEFINE(PROC_FAIL_work, PROC_FAIL_handler);
 #define PROC_FAIL_START_DELAY_SECOND 10
 void ISR_PWRGD_CPU()
 {
+	isr_dbg_print(PWRGD_CPU_LVC3);
 	set_CPU_power_status(PWRGD_CPU_LVC3);
+
 	if (gpio_get(PWRGD_CPU_LVC3) == GPIO_HIGH) {
 		init_snoop_thread();
 		init_send_postcode_thread();
@@ -161,7 +196,7 @@ void ISR_PWRGD_CPU()
 		abort_snoop_thread();
 
 		if (k_work_cancel_delayable(&PROC_FAIL_work) != 0) {
-			printf("Cancel proc_fail delay work fail\n");
+			LOG_ERR("Cancel proc_fail delay work fail");
 		}
 		reset_kcs_ok();
 		reset_postcode_ok();
@@ -190,7 +225,7 @@ static void CAT_ERR_handler(struct k_work *work)
 		sel_msg.event_data3 = 0xFF;
 		ret = common_add_sel_evt_record(&sel_msg);
 		if (!ret) {
-			printf("Fail to assert CatErr event log.\n");
+			LOG_ERR("Fail to assert CatErr event log");
 		}
 	}
 }
@@ -199,9 +234,11 @@ K_WORK_DELAYABLE_DEFINE(CAT_ERR_work, CAT_ERR_handler);
 #define CATERR_START_DELAY_SECOND 2
 void ISR_CATERR()
 {
+	isr_dbg_print(FM_CPU_CATERR_LVT3_N);
+
 	if ((gpio_get(RST_PLTRST_BIC_N) == GPIO_HIGH)) {
 		if (k_work_cancel_delayable(&CAT_ERR_work) != 0) {
-			printf("Cancel caterr delay work fail\n");
+			LOG_ERR("Cancel caterr delay work fail");
 		}
 		/* start thread CatErr_handler after 2 seconds */
 		k_work_schedule(&CAT_ERR_work, K_SECONDS(CATERR_START_DELAY_SECOND));
@@ -210,16 +247,20 @@ void ISR_CATERR()
 
 void ISR_PLTRST()
 {
+	isr_dbg_print(RST_PLTRST_BIC_N);
 	send_gpio_interrupt(RST_PLTRST_BIC_N);
 }
 
 void ISR_DBP_PRSNT()
 {
+	isr_dbg_print(FM_DBP_PRESENT_N);
 	send_gpio_interrupt(FM_DBP_PRESENT_N);
 }
 
 void ISR_FM_THROTTLE()
 {
+	isr_dbg_print(FM_THROTTLE_R_N);
+
 	common_addsel_msg_t sel_msg;
 	if (gpio_get(PWRGD_CPU_LVC3) == GPIO_HIGH) {
 		if (gpio_get(FM_THROTTLE_R_N) == GPIO_HIGH) {
@@ -234,13 +275,15 @@ void ISR_FM_THROTTLE()
 		sel_msg.event_data2 = 0xFF;
 		sel_msg.event_data3 = 0xFF;
 		if (!common_add_sel_evt_record(&sel_msg)) {
-			printf("FM Throttle addsel fail\n");
+			LOG_ERR("FM Throttle addsel fail");
 		}
 	}
 }
 
 void ISR_HSC_THROTTLE()
 {
+	isr_dbg_print(IRQ_SML1_PMBUS_BMC_ALERT_N);
+
 	common_addsel_msg_t sel_msg;
 	static bool is_hsc_throttle_assert = false; // Flag for filt out fake alert
 	if (gpio_get(RST_RSMRST_BMC_N) == GPIO_HIGH) {
@@ -266,7 +309,7 @@ void ISR_HSC_THROTTLE()
 			sel_msg.event_data2 = 0xFF;
 			sel_msg.event_data3 = 0xFF;
 			if (!common_add_sel_evt_record(&sel_msg)) {
-				printf("HSC Throttle addsel fail\n");
+				LOG_ERR("HSC Throttle addsel fail");
 			}
 		}
 	}
@@ -274,11 +317,13 @@ void ISR_HSC_THROTTLE()
 
 void ISR_MB_THROTTLE()
 {
-	// TODO
+	isr_dbg_print(P12V_HS_D_OC_R_N);
 }
 
 void ISR_SOC_THMALTRIP()
 {
+	isr_dbg_print(FM_CPU_THERMTRIP_LATCH_LVT3_N);
+
 	common_addsel_msg_t sel_msg;
 	if (gpio_get(RST_PLTRST_PLD_N) == GPIO_HIGH) {
 		if (gpio_get(H_CPU_MEMTRIP_LVC3_N) ==
@@ -295,9 +340,9 @@ void ISR_SOC_THMALTRIP()
 		sel_msg.event_data3 = 0xFF;
 		if (!common_add_sel_evt_record(&sel_msg)) {
 			if (sel_msg.event_data1 == IPMI_OEM_EVENT_OFFSET_SYS_THERMAL_TRIP) {
-				printf("SOC Thermal trip addsel fail\n");
+				LOG_ERR("SOC Thermal trip addsel fail");
 			} else {
-				printf("Memory Thermal trip addsel fail\n");
+				LOG_ERR("Memory Thermal trip addsel fail");
 			}
 		}
 	}
@@ -305,6 +350,8 @@ void ISR_SOC_THMALTRIP()
 
 void ISR_SYS_THROTTLE()
 {
+	isr_dbg_print(FM_CPU_BIC_PROCHOT_LVT3_N);
+
 	common_addsel_msg_t sel_msg;
 	if ((gpio_get(RST_PLTRST_PLD_N) == GPIO_HIGH) && (gpio_get(PWRGD_SYS_PWROK) == GPIO_HIGH)) {
 		if (gpio_get(FM_CPU_BIC_PROCHOT_LVT3_N) == GPIO_HIGH) {
@@ -319,13 +366,15 @@ void ISR_SYS_THROTTLE()
 		sel_msg.event_data2 = 0xFF;
 		sel_msg.event_data3 = 0xFF;
 		if (!common_add_sel_evt_record(&sel_msg)) {
-			printf("System Throttle addsel fail\n");
+			LOG_ERR("System Throttle addsel fail");
 		}
 	}
 }
 
 void ISR_PCH_THMALTRIP()
 {
+	isr_dbg_print(FM_PCHHOT_N);
+
 	common_addsel_msg_t sel_msg;
 	static bool is_pch_assert = 0;
 	if (gpio_get(FM_PCHHOT_N) == GPIO_LOW) {
@@ -347,7 +396,7 @@ void ISR_PCH_THMALTRIP()
 	sel_msg.event_data2 = 0xFF;
 	sel_msg.event_data3 = 0xFF;
 	if (!common_add_sel_evt_record(&sel_msg)) {
-		printf("PCH Thermal trip addsel fail\n");
+		LOG_ERR("PCH Thermal trip addsel fail");
 	}
 }
 
@@ -358,6 +407,8 @@ void ISR_HSC_OC()
 
 void ISR_CPU_MEMHOT()
 {
+	isr_dbg_print(H_CPU0_MEMHOT_OUT_LVC3_N);
+
 	common_addsel_msg_t sel_msg;
 	if ((gpio_get(RST_PLTRST_PLD_N) == GPIO_HIGH) && (gpio_get(PWRGD_SYS_PWROK) == GPIO_HIGH)) {
 		if (gpio_get(H_CPU0_MEMHOT_OUT_LVC3_N) == GPIO_HIGH) {
@@ -372,13 +423,15 @@ void ISR_CPU_MEMHOT()
 		sel_msg.event_data2 = 0xFF;
 		sel_msg.event_data3 = 0xFF;
 		if (!common_add_sel_evt_record(&sel_msg)) {
-			printf("CPU MEM HOT addsel fail\n");
+			LOG_ERR("CPU MEM HOT addsel fail");
 		}
 	}
 }
 
 void ISR_CPUVR_HOT()
 {
+	isr_dbg_print(IRQ_CPU0_VRHOT_N);
+
 	common_addsel_msg_t sel_msg;
 	if ((gpio_get(RST_PLTRST_PLD_N) == GPIO_HIGH) && (gpio_get(PWRGD_SYS_PWROK) == GPIO_HIGH)) {
 		if (gpio_get(IRQ_CPU0_VRHOT_N) == GPIO_HIGH) {
@@ -393,13 +446,15 @@ void ISR_CPUVR_HOT()
 		sel_msg.event_data2 = 0xFF;
 		sel_msg.event_data3 = 0xFF;
 		if (!common_add_sel_evt_record(&sel_msg)) {
-			printf("CPU VR HOT addsel fail\n");
+			LOG_ERR("CPU VR HOT addsel fail");
 		}
 	}
 }
 
 void ISR_PCH_PWRGD()
 {
+	isr_dbg_print(RST_RSMRST_BMC_N);
+
 	common_addsel_msg_t sel_msg;
 	if (gpio_get(FM_SLPS3_PLD_N) == GPIO_HIGH) {
 		sel_msg.InF_target = BMC_IPMB;
@@ -410,13 +465,15 @@ void ISR_PCH_PWRGD()
 		sel_msg.event_data2 = 0xFF;
 		sel_msg.event_data3 = 0xFF;
 		if (!common_add_sel_evt_record(&sel_msg)) {
-			printf("PCH PWROK failure addsel fail\n");
+			LOG_ERR("PCH PWROK failure addsel fail");
 		}
 	}
 }
 
 void ISR_RMCA()
 {
+	isr_dbg_print(FM_CPU_RMCA_LVT3_N);
+
 	if ((gpio_get(RST_PLTRST_BIC_N) == GPIO_HIGH) || (gpio_get(PWRGD_CPU_LVC3) == GPIO_HIGH)) {
 		common_addsel_msg_t sel_msg;
 		sel_msg.InF_target = BMC_IPMB;
@@ -427,7 +484,7 @@ void ISR_RMCA()
 		sel_msg.event_data2 = 0xFF;
 		sel_msg.event_data3 = 0xFF;
 		if (!common_add_sel_evt_record(&sel_msg)) {
-			printf("RMCA addsel fail\n");
+			LOG_ERR("RMCA addsel fail");
 		}
 	}
 }
