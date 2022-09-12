264db70fc2132992691c442365df996b002cd72c
Eason Chen <EasonChen1@quantatw.com>
2022-09-12 12:35:42 -0700

    Common: Fix erorr parsing TMP75 negative value (#542)
    Summary:
    - Fix erorr parsing TMP75 negative value.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/542
    
    Test Plan: - Build code: PASS
    
    Reviewed By: zhdaniel12
    
    Differential Revision: D39434072
    
    Pulled By: GoldenBug
    
    fbshipit-source-id: fe5ecb7d7dc6bb92f1142cd2de9714eabf111e29

--- common/dev/tmp75.c
+++ common/dev/tmp75.c
@@ -21,7 +21,7 @@ uint8_t tmp75_read(uint8_t sensor_num, int *reading)
 		return SENSOR_FAIL_TO_ACCESS;
 
 	sensor_val *sval = (sensor_val *)reading;
-	sval->integer = msg.data[0];
+	sval->integer = (int8_t)msg.data[0];
 	sval->fraction = 0;
 	return SENSOR_READ_SUCCESS;
 }
