1f885d155ba39cba486f40e0b05b08767ec994c7
Peter Fetros <goldenbug@fb.com>
2022-09-13 13:38:25 -0700

    Add Proper Copyright Header to Source Files
    Summary:
    After the major refactoring most files were missing their copyright
    header. This commit adds them back.
    
    Several files were also formatted according to the clang-format file.
    Apparently they didn't match the style.
    
    Reviewed By: amithash
    
    Differential Revision: D39446014
    
    fbshipit-source-id: 2d3792d6d04fa04367ada0ea559d96d274fd0ec8

--- meta-facebook/gt-cc/src/platform/plat_fru.h
+++ meta-facebook/gt-cc/src/platform/plat_fru.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef PLAT_FRU_H
 #define PLAT_FRU_H
 
@@ -12,11 +28,12 @@
 #define HSC_MODULE_FRU_MUX_ADDR (0xE0 >> 1)
 #define HSC_MODULE_FRU_MUX_CHAN 6
 
-enum { SWB_FRU_ID,
-       FIO_FRU_ID,
-       HSC_MODULE_FRU_ID,
-       // OTHER_FRU_ID,
-       MAX_FRU_ID,
+enum {
+	SWB_FRU_ID,
+	FIO_FRU_ID,
+	HSC_MODULE_FRU_ID,
+	// OTHER_FRU_ID,
+	MAX_FRU_ID,
 };
 
 #endif
