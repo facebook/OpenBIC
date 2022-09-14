c74703696df0701129d7d81cb33e1065fc512cc8
MouchenHung <Mouchen.Hung@quantatw.com>
2022-09-14 13:58:41 +0800

    common: shell: Add shell flash commands

---
+++ common/shell/commands/flash_shell.h
@@ -0,0 +1,25 @@
+#ifndef FLASH_SHELL_H
+#define FLASH_SHELL_H
+
+#include <stdlib.h>
+#include <shell/shell.h>
+#include <drivers/spi_nor.h>
+#include <drivers/flash.h>
+
+#define SPI_DEVICE_PREFIX "spi"
+#define SFDP_BUFF_SIZE 256
+
+void cmd_flash_re_init(const struct shell *shell, size_t argc, char **argv);
+void cmd_flash_sfdp_read(const struct shell *shell, size_t argc, char **argv);
+void device_spi_name_get(size_t idx, struct shell_static_entry *entry);
+
+SHELL_DYNAMIC_CMD_CREATE(spi_device_name, device_spi_name_get);
+
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_flash_cmds,
+			       SHELL_CMD(re_init, &spi_device_name, "Re-init spi config",
+					 cmd_flash_re_init),
+			       SHELL_CMD(sfpd_read, &spi_device_name, "SFPD read",
+					 cmd_flash_sfdp_read),
+			       SHELL_SUBCMD_SET_END);
+
+#endif
--- common/shell/shell_platform.c
+++ common/shell/shell_platform.c
@@ -1,6 +1,7 @@
 #include "commands/gpio_shell.h"
 #include "commands/info_shell.h"
 #include "commands/sensor_shell.h"
+#include "commands/flash_shell.h"
 #include "commands/ipmi_shell.h"
 
 /* MAIN command */
@@ -8,6 +9,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 	sub_platform_cmds, SHELL_CMD(info, NULL, "Platform info.", cmd_info_print),
 	SHELL_CMD(gpio, &sub_gpio_cmds, "GPIO relative command.", NULL),
 	SHELL_CMD(sensor, &sub_sensor_cmds, "SENSOR relative command.", NULL),
+	SHELL_CMD(flash, &sub_flash_cmds, "FLASH(spi) relative command.", NULL),
 	SHELL_CMD(ipmi, &sub_ipmi_cmds, "IPMI relative command.", NULL), SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(platform, &sub_platform_cmds, "Platform commands", NULL);
