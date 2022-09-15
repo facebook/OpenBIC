d147af50f27a8aa96e633f1eb637b6afd479115d
Eason Chen <EasonChen1@quantatw.com>
2022-09-15 13:13:21 -0700

    fby3.5: hd: Version commit for oby35-hd-2022.37.01 (#568)
    Summary:
    - Version commit for Yv3.5 HalfDome BIC oby35-hd-2022.37.01.
    
    Dependency: https://github.com/facebook/OpenBIC/issues/558
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/568
    
    Test Plan:
    - Build code: Pass
    - Get BIC version: Pass
    
    Log:
    root@bmc-oob:~# fw-util slot1 --version bic
    SB Bridge-IC Version: oby35-hd-v2022.37.01
    
    root@bmc-oob:~# bic-util slot1 0x18 0x1
    00 80 11 05 02 BF 15 A0 00 00 00 00 00 00 00
    
    Reviewed By: garnermic
    
    Differential Revision: D39551638
    
    Pulled By: GoldenBug
    
    fbshipit-source-id: 15a146beaff0c0ac69524e2d64817a1447c339ca

--- meta-facebook/yv35-hd/src/platform/plat_version.h
+++ meta-facebook/yv35-hd/src/platform/plat_version.h
@@ -21,24 +21,24 @@
 
 #define PLATFORM_NAME "Yosemite 3.5"
 #define PROJECT_NAME "Half Dome"
-#define PROJECT_STAGE POC
+#define PROJECT_STAGE EVT
 
 #define BOARD_ID 0x01
 #define DEVICE_ID 0x00
 #define DEVICE_REVISION 0x80
 
-#define FIRMWARE_REVISION_1 0xff
-#define FIRMWARE_REVISION_2 0xff
+#define FIRMWARE_REVISION_1 GET_FW_VERSION1(BOARD_ID, PROJECT_STAGE)
+#define FIRMWARE_REVISION_2 0x05
 
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
 #define PRODUCT_ID 0x0000
 #define AUXILIARY_FW_REVISION 0x00000000
 
-#define BIC_FW_YEAR_MSB 0xff
-#define BIC_FW_YEAR_LSB 0xff
-#define BIC_FW_WEEK 0xff
-#define BIC_FW_VER 0xff
+#define BIC_FW_YEAR_MSB 0x20
+#define BIC_FW_YEAR_LSB 0x22
+#define BIC_FW_WEEK 0x37
+#define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x68 // char: h
 #define BIC_FW_platform_1 0x64 // char: d
 #define BIC_FW_platform_2 0x00 // char: '\0'
