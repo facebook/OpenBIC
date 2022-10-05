d03a53f00e55a4621e266730884d21f65fd4d6ec
MouchenHung <Mouchen.Hung@quantatw.com>
2022-10-04 21:11:27 -0700

    wc: mb: Change monitor PMIC polling time (#615)
    Summary:
    - To avoid PMIC error SEL show too delay, change monitor PMIC time from 30s to 3s.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/615
    
    Test Plan: - Build Code: PASS
    
    Reviewed By: garnermic
    
    Differential Revision: D40037873
    
    Pulled By: garnermic
    
    fbshipit-source-id: 972cedb455dabc5a8c53ef98e95fa9bb1dc1121d

--- meta-facebook/wc-mb/src/platform/plat_pmic.h
+++ meta-facebook/wc-mb/src/platform/plat_pmic.h
@@ -20,7 +20,7 @@
 #include <stdint.h>
 
 #define MONITOR_PMIC_ERROR_STACK_SIZE 4096
-#define MONITOR_PMIC_ERROR_TIME_MS (30 * 1000) // 30s
+#define MONITOR_PMIC_ERROR_TIME_MS (3 * 1000) // 3s
 
 #define MAX_LEN_GET_PMIC_ERROR_INFO 6 //include R05,  R06,  R08,  R09,  R0A,  R0B
 
