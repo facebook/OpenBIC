f1c0026604f10c28e1de3b0799af803782aa7cc6
Eason Chen <EasonChen1@quantatw.com>
2022-10-07 10:57:35 +0800

    fby3.5: hd: Send 4-byte post code to BMC
    Summary:
    - Add IPMI OEM command(NetFn:38h,Cmd:33h) to send latest 4-byte post code to BMC.
    
    Test Plan:
    - Build code: PASS

--- common/dev/pcc.c
+++ common/dev/pcc.c
@@ -18,14 +18,15 @@
 
 #include <zephyr.h>
 #include <device.h>
-#include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <drivers/misc/aspeed/pcc_aspeed.h>
 #include "ipmb.h"
 #include "ipmi.h"
 #include "libutil.h"
 #include "pcc.h"
+#include <logging/log.h>
+
+LOG_MODULE_REGISTER(pcc);
 
 const struct device *pcc_dev;
 static uint32_t pcc_read_buffer[PCC_BUFFER_LEN];
@@ -38,30 +39,58 @@ uint16_t copy_pcc_read_buffer(uint16_t start, uint16_t length, uint8_t *buffer, 
 		return 0;
 	}
 
-	uint16_t start_index, current_index;
-	uint16_t i = 0;
+	uint16_t current_index, i = 0;
 	uint16_t current_read_len = pcc_read_len;
-	if (start < pcc_read_index) {
-		start_index = pcc_read_index - start - 1;
+	uint16_t current_read_index = pcc_read_index;
+	if (start < current_read_index) {
+		current_index = current_read_index - start - 1;
 	} else {
-		start_index = pcc_read_index + PCC_BUFFER_LEN - start - 1;
+		current_index = current_read_index + PCC_BUFFER_LEN - start - 1;
 	}
 
 	for (; (i < length) && ((i + start) < current_read_len); i++) {
-		current_index = start_index;
-		if (start_index == 0) {
-			start_index = PCC_BUFFER_LEN - 1;
-		} else {
-			start_index--;
-		}
-
 		buffer[4 * i] = pcc_read_buffer[current_index] & 0xFF;
 		buffer[(4 * i) + 1] = (pcc_read_buffer[current_index] >> 8) & 0xFF;
 		buffer[(4 * i) + 2] = (pcc_read_buffer[current_index] >> 16) & 0xFF;
 		buffer[(4 * i) + 3] = (pcc_read_buffer[current_index] >> 24) & 0xFF;
+
+		if (current_index == 0) {
+			current_index = PCC_BUFFER_LEN - 1;
+		} else {
+			current_index--;
+		}
 	}
 	return 4 * i;
 }
+
+void send_post_code_to_bmc()
+{
+	ipmi_msg *msg = (ipmi_msg *)malloc(sizeof(ipmi_msg));
+	if (msg == NULL) {
+		LOG_ERR("Memory allocation failed.");
+		return;
+	}
+
+	memset(msg, 0, sizeof(ipmi_msg));
+	msg->InF_source = SELF;
+	msg->InF_target = BMC_IPMB;
+	msg->netfn = NETFN_OEM_1S_REQ;
+	msg->cmd = CMD_OEM_1S_SEND_4BYTE_POST_CODE_TO_BMC;
+	msg->data_len = 8;
+	msg->data[0] = IANA_ID & 0xFF;
+	msg->data[1] = (IANA_ID >> 8) & 0xFF;
+	msg->data[2] = (IANA_ID >> 16) & 0xFF;
+	msg->data[3] = 4;
+	copy_pcc_read_buffer(0, 1, &msg->data[4], 4);
+
+	ipmb_error status = ipmb_read(msg, IPMB_inf_index_map[msg->InF_target]);
+	SAFE_FREE(msg);
+	if (status != IPMB_ERROR_SUCCESS) {
+		LOG_ERR("Failed to send 4-byte post code to BMC, status %d.", status);
+	}
+}
+
+K_WORK_DEFINE(send_post_code_work, send_post_code_to_bmc);
 
 void pcc_rx_callback(const uint8_t *rb, uint32_t rb_sz, uint32_t st_idx, uint32_t ed_idx)
 {
@@ -94,13 +123,16 @@ void pcc_rx_callback(const uint8_t *rb, uint32_t rb_sz, uint32_t st_idx, uint32_
 		}
 		i = (i + 2) % rb_sz;
 	} while (i != ed_idx);
+
+	/* Send 4-byte post code to BMC */
+	k_work_submit(&send_post_code_work);
 }
 
 void pcc_init()
 {
 	pcc_dev = device_get_binding(DT_LABEL(DT_NODELABEL(pcc)));
 	if (!pcc_dev) {
-		printf("[%s] No pcc device found\n", __func__);
+		LOG_ERR("No pcc device found.");
 		return;
 	}
 	/* set registers to enable pcc */
@@ -114,7 +146,7 @@ void pcc_init()
 	sys_write32((reg_data | 0x00080000) & ~0x0001ffff, 0x7E789084);
 
 	if (pcc_aspeed_register_rx_callback(pcc_dev, pcc_rx_callback)) {
-		printk("[%s] Cannot register PCC RX callback\n", __func__);
+		LOG_ERR("Cannot register PCC RX callback.");
 	}
 	return;
 }
