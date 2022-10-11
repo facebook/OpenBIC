347429444a1b73c0656463aa56e215b4db3541b2
Lora Lin <Lora_Lin@wiwynn.com>
2022-10-11 11:29:57 +0800

    at-cc: Support 2-channel i2c switch and multiplexer
    Summary:
    - Support mux common function to set channel.
    - Support 2-channel i2c-bus switch (TCA9543A) device.
    - Support 2-channel i2c-bus multiplexer (PI4MSD5V9542A) device.
    
    Test Plan:
    - Build code: Pass

---
+++ common/dev/include/i2c-mux-pi4msd5v9542.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef I2C_MUX_PI4MSD5V9542_H
+#define I2C_MUX_PI4MSD5V9542_H
+
+#define PI4MSD5V9542_ENABLE_BIT BIT(2)
+#define PI4MSD5V9542_SELECT_CHANNEL_0 0
+#define PI4MSD5V9542_SELECT_CHANNEL_1 1
+
+enum PI4MSD5V9542_CHANNEL {
+	PI4MSD5V9542_CHANNEL_0,
+	PI4MSD5V9542_CHANNEL_1,
+};
+
+#endif
