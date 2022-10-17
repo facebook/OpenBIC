35a16b51cc39f92193fbad268af9d85b72ccffb4
Lora Lin <Lora_Lin@wiwynn.com>
2022-10-17 14:25:35 +0800

    fby3.5: cl: Recode BIOS firmware version in EEPROM
    Summary:
    - Record BIOS firmware version in EEPROM.
    - Add ipmi oem command (netfn = 0x38, cmd = 0xA2) to support BMC get BIOS firmware version.
    
    Note:
    - IPMI command format
      - Request
        Byte 1:3 – MFG ID – 00A015h, LS byte first
      - Response
        Byte 1 – Completion Code
        Byte 2:18 BIOS version
    
    Test Plan:
    - Build code: Pass
    - Check there are no unexpected error logs: Pass
    - Check BIOS firmware version was stored in EEPROM after updating BIOS firmware: Pass
    - Check BIOS firmware version was stored in EEPROM after power cycle/reset: Pass
    - Check BIOS firmware version wasn't written to EEPROM if the written version is the same with the stored version in EEPROM: Pass
    
    Log:
    - Check BIOS firmware version was stored in EEPROM after updating BIOS firmware
      - Before updating BIOS firmware
        root@bmc-oob:~# fw-util slot3 --version bios
        BIOS Version: Y35CLD07
        root@bmc-oob:~# bic-util slot2 0xE0 0xA2 0x15 0xA0 0x0
        15 A0 00 00 00 08 59 33 35 43 4C 44 30 37 00 00
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
        00 00 00 00 00
        root@bmc-oob:~# cfg-util dump-all
        ......
        sysfw_ver_slot3:000008593335434c443037000000000000
        ......
    
      - After updating BIOS firmware
        root@bmc-oob:~# fw-util slot3 --version bios
        BIOS Version: Y35CLD08
        root@bmc-oob:~# bic-util slot2 0xE0 0xA2 0x15 0xA0 0x0
        15 A0 00 00 00 08 59 33 35 43 4C 44 30 38 00 00
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
        00 00 00 00 00
        root@bmc-oob:~# cfg-util dump-all
        ......
        sysfw_ver_slot3:000008593335434c443038000000000000
        ......
    
    - Check there are no unexpected error logs
      - log-util
        root@bmc-oob:~# log-util slot3 --print
        2022 Oct 16 23:35:48 log-util: User cleared FRU: 3 logs
        3    slot3    2022-10-25 23:24:35    gpiod            FRU: 3, System powered OFF
        3    slot3    2022-10-25 23:24:36    power-util       SERVER_POWER_OFF successful for FRU: 3
        3    slot3    2022-10-25 23:24:40    ipmid            SEL Entry: FRU: 3, Record: Standard (0x02), Time: 2022-10-25 23:24:40, Sensor: ME_POWER_STATE (0x16), Event Data: (000000) RUNNING Assertion
        3    slot3    2022-10-25 23:25:14    power-util       SERVER_POWER_ON successful for FRU: 3
        3    slot3    2022-10-25 23:25:14    gpiod            FRU: 3, System powered ON
    
    - Check BIOS firmware version wasn't written to EEPROM if the written version is the same with the stored version in EEPROM
      - If the written version is the same with the stored version in EEPROM
        [BIC console]
        [00:07:06.337,000] <wrn> power_status: DC_STATUS: on
        [00:07:06.476,000] <wrn> power_status: CPU_PWR_GOOD: yes
        [00:07:06.337,000] <wrn> power_status: DC_STATUS: on
        [00:07:06.476,000] <wrn> power_status: CPU_PWR_GOOD: yes
        ......
        [00:00:38.352,000] <dbg> plat_ipmi.pal_record_bios_fw_version: Write bios version is the same with stored bios version in EEPROM
        [00:00:56.078,000] <wrn> power_status: POST_COMPLETE: yes
        [00:00:56.078,000] <wrn> power_status: POST_COMPLETE: yes
    
      - If the written version is different with the stored version in EEPROM
        [BIC console]
        [00:13:46.228,000] <wrn> power_status: DC_STATUS: on
        [00:13:46.460,000] <wrn> power_status: CPU_PWR_GOOD: yes
        [00:13:46.228,000] <wrn> power_status: DC_STATUS: on
        [00:13:46.460,000] <wrn> power_status: CPU_PWR_GOOD: yes
        ......
        [00:16:06.534,000] <dbg> plat_ipmi.pal_record_bios_fw_version: Set bios version
        [00:16:36.576,000] <wrn> power_status: POST_COMPLETE: yes
        [00:16:36.576,000] <wrn> power_status: POST_COMPLETE: yes

--- common/service/ipmi/include/oem_1s_handler.h
+++ common/service/ipmi/include/oem_1s_handler.h
@@ -100,6 +100,7 @@ void OEM_1S_CLEAR_CMOS(ipmi_msg *msg);
 void OEM_1S_NOTIFY_PMIC_ERROR(ipmi_msg *msg);
 void OEM_1S_GET_SDR(ipmi_msg *msg);
 void OEM_1S_BMC_IPMB_ACCESS(ipmi_msg *msg);
+void OEM_1S_GET_BIOS_VERSION(ipmi_msg *msg);
 
 #ifdef CONFIG_SNOOP_ASPEED
 void OEM_1S_GET_POST_CODE(ipmi_msg *msg);
