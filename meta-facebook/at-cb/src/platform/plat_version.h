9df4cdd2d50f26fc970d5b9512998513e4a7b209
KaidanWu <kaidan_wu@wiwynn.com>
2022-10-20 14:33:32 -0700

    at: cb: Version commit for obat-cb-2022.42.01 (#640)
    Summary:
    [Summary]
    - Version commit for ColterBay BIC obat-cb-2022.42.01
    - First release for MFG pre-burn
    
    [Test Plan & Log]
    1. Build code:	Artemis CB			[ Pass ]
    2. Booting on Y35 platform:			[ Pass ]
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/640
    
    Reviewed By: garnermic
    
    Differential Revision: D40529861
    
    Pulled By: GoldenBug
    
    fbshipit-source-id: 8bc6d287a8b9413942e8c8f5f106b28525a22c1d

--- meta-facebook/at-cb/src/platform/plat_version.h
+++ meta-facebook/at-cb/src/platform/plat_version.h
@@ -42,7 +42,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x22
-#define BIC_FW_WEEK 0x38
+#define BIC_FW_WEEK 0x42
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x63 // char: c
 #define BIC_FW_platform_1 0x62 // char: b
