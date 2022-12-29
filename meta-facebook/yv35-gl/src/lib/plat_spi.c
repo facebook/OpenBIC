d47a2f550609f0e9440142b6e37e331b9797b72b
RenChen-wiwynn <ren_chen@wiwynn.com>
2022-12-29 13:07:11 +0800

    fby35: gl: Support BIOS and PRoT frimware update
    Summary:
    - Support BIOS and PRoT firmware update
       SPI1 CS0 is connected to BIOS flash
       SPI2 CS0 is connected to PRoT flash
    
    Test Plan:
    - Build code: pass

--- meta-facebook/yv35-gl/src/lib/plat_spi.c
+++ meta-facebook/yv35-gl/src/lib/plat_spi.c
@@ -15,10 +15,48 @@
  */
 
 #include "hal_gpio.h"
-#include "plat_gpio.h"
+#include "hal_i2c.h"
+#include "plat_i2c.h"
 #include "util_spi.h"
+
+#define CPLD_ADDR 0x21
+#define CPLD_SPI_OOB_CONTROL_REG 0x0C
+#define CPLD_SPI_OOB_FROM_CPU 0x02
+#define CPLD_SPI_OOB_FROM_BIC 0x0B
 
 int pal_get_bios_flash_position()
 {
 	return DEVSPI_SPI1_CS0;
+}
+
+int pal_get_prot_flash_position()
+{
+	return DEVSPI_SPI2_CS0;
+}
+
+bool pal_switch_bios_spi_mux(int gpio_status)
+{
+	uint8_t retry = 5;
+	I2C_MSG msg;
+
+	/* BIC switch the MUX selection pin by setting CPLD register.
+	 * Serverboard CPLD register:
+	 * Offset 0Ch - BIC SPI OOB0 control
+	 * bit[3:0]=0010b, boot from CPU(default)
+	 * bit[3:0]=1011b, boot from BIC
+	 */
+	msg.bus = I2C_BUS1;
+	msg.target_addr = CPLD_ADDR;
+	msg.tx_len = 2;
+	msg.data[0] = CPLD_SPI_OOB_CONTROL_REG;
+	if (gpio_status == GPIO_HIGH) {
+		msg.data[1] = CPLD_SPI_OOB_FROM_BIC;
+	} else {
+		msg.data[1] = CPLD_SPI_OOB_FROM_CPU;
+	}
+
+	if (i2c_master_write(&msg, retry)) {
+		return false;
+	}
+	return true;
 }
