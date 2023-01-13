69cb6b4b330b18c033225e57ea576796d5e70046
HungYi <Hung-Yi.Li@quantatw.com>
2023-01-13 15:19:01 -0800

    common: add nvme pec check (#820)
    Summary:
    - modify nvme read len from 4 to 8
    - add nvme pec check
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/820
    
    Test Plan:
    - Build code : Pass
    - Read NVMe temperature : Pass
    
    root@bmc-oob:~# sensor-util slot1 | grep "DEV.*Temp"
    E1S DEV0 Temp                (0x62) :   26.00 C     | (ok)
    E1S DEV1 Temp                (0x6A) :   37.00 C     | (ok)
    E1S DEV2 Temp                (0x72) :   31.00 C     | (ok)
    
    Reviewed By: williamspatrick
    
    Differential Revision: D42501159
    
    Pulled By: garnermic
    
    fbshipit-source-id: ceda38836ad094ec51bcdbcf1666c0c8101deaf3

--- common/dev/nvme.c
+++ common/dev/nvme.c
@@ -15,11 +15,36 @@
  */
 
 #include <stdio.h>
+#include "libutil.h"
 #include "sensor.h"
 #include "hal_i2c.h"
 
+#include <logging/log.h>
+#include <sys/crc.h>
+
 #define NVMe_NOT_AVAILABLE 0x80
 #define NVMe_TMP_SENSOR_FAILURE 0x81
+
+#define NVMe_READ_LEN 8
+
+LOG_MODULE_REGISTER(nvme);
+
+static uint8_t nvme_cal_pec(uint8_t addr, uint8_t oft, uint8_t *buf, uint32_t len, uint8_t *pec)
+{
+	CHECK_NULL_ARG_WITH_RETURN(buf, 1);
+	CHECK_NULL_ARG_WITH_RETURN(pec, 1);
+
+	uint8_t pec_buf[len + 3];
+	pec_buf[0] = addr << 1;
+	pec_buf[1] = oft;
+	pec_buf[2] = (addr << 1) + 1;
+
+	memcpy(pec_buf + 3, buf, len);
+
+	*pec = crc8(pec_buf, sizeof(pec_buf), 0x07, 0x00, false);
+
+	return 0;
+}
 
 uint8_t nvme_read(uint8_t sensor_num, int *reading)
 {
@@ -35,9 +60,23 @@ uint8_t nvme_read(uint8_t sensor_num, int *reading)
 	msg.target_addr = sensor_config[sensor_config_index_map[sensor_num]].target_addr;
 	msg.data[0] = sensor_config[sensor_config_index_map[sensor_num]].offset;
 	msg.tx_len = 1;
-	msg.rx_len = 4;
+	msg.rx_len = NVMe_READ_LEN;
 
 	if (!i2c_master_read(&msg, retry)) {
+		/* Check PEC */
+		uint8_t pec = 0;
+		if (nvme_cal_pec(sensor_config[sensor_config_index_map[sensor_num]].target_addr,
+				 sensor_config[sensor_config_index_map[sensor_num]].offset,
+				 msg.data, NVMe_READ_LEN - 1, &pec)) {
+			LOG_ERR("sensor_num 0x%02x cal nvme pec fail!\n", sensor_num);
+			return SENSOR_UNSPECIFIED_ERROR;
+		}
+		if (msg.data[7] != pec) {
+			LOG_ERR("sensor_num 0x%02x check nvme pec error! (%02x/%02x)\n", sensor_num,
+				msg.data[7], pec);
+			return SENSOR_PEC_ERROR;
+		}
+
 		/* Check SSD drive ready */
 		is_drive_ready = ((msg.data[1] & 0x40) == 0 ? true : false);
 		if (!is_drive_ready)
@@ -50,6 +89,7 @@ uint8_t nvme_read(uint8_t sensor_num, int *reading)
 		if (msg.data[3] == NVMe_TMP_SENSOR_FAILURE) {
 			return SENSOR_UNSPECIFIED_ERROR;
 		}
+
 	} else {
 		return SENSOR_FAIL_TO_ACCESS;
 	}
