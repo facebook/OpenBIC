5bc8bde5c3e0b549832d1f361cb239abfcf6fccd
HungYi <Hung-Yi.Li@quantatw.com>
2023-02-09 09:14:07 +0800

    fby3: vf: Version commit for ob-vf2-2023.06.01
    Summary:
    - Version commit for Vernalfall 2.0 BIC ob-vf2-2023.06.01
    
    Test Plan:
    - Build code: Pass
    - Check BIC version: Pass

--- meta-facebook/yv3-vf/src/platform/plat_version.h
+++ meta-facebook/yv3-vf/src/platform/plat_version.h
@@ -31,7 +31,7 @@
 #define DEVICE_REVISION 0x80
 
 #define FIRMWARE_REVISION_1 GET_FW_VERSION1(BOARD_ID, PROJECT_STAGE)
-#define FIRMWARE_REVISION_2 0x06
+#define FIRMWARE_REVISION_2 0x07
 
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
@@ -41,8 +41,8 @@
 #define IANA_ID2 0x00A015 // for OEM 1S command supports YV3.5
 
 #define BIC_FW_YEAR_MSB 0x20
-#define BIC_FW_YEAR_LSB 0x22
-#define BIC_FW_WEEK 0x51
+#define BIC_FW_YEAR_LSB 0x23
+#define BIC_FW_WEEK 0x06
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x76 // char: v
 #define BIC_FW_platform_1 0x66 // char: f
