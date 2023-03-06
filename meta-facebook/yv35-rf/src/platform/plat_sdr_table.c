facebook/OpenBIC

commit 7005966d6bbf37b74ea5f0da7f7e1fd683b32efa

fby3.5: rf: modify the sensor name (#943)
Summary:
- modify the sensor name

Pull Request resolved: https://github.com/facebook/OpenBIC/pull/943

Test Plan: Build code: Pass

Reviewed By: williamspatrick

Differential Revision: D43713912

Pulled By: garnermic

fbshipit-source-id: ebde29c9a5e22c596ab512090f0ac11d84e11c44

meta-facebook/yv35-rf/src/platform/plat_sdr_table.c
@@ -81,7 +81,7 @@ SDR_Full_sensor plat_sdr_table[] = {
 		0x00, // reserved
 		0x00, // OEM
 		IPMI_SDR_STRING_TYPE_ASCII_8, // ID len, should be same as "size of struct"
-		"RF_CXL_TEMP_C",
+		"RF_CXL_CNTR_TEMP_C",
 	},
 	{
 		// RF DIMMA temperature
@@ -447,7 +447,7 @@ SDR_Full_sensor plat_sdr_table[] = {
 		0x00, // reserved
 		0x00, // OEM
 		IPMI_SDR_STRING_TYPE_ASCII_8, // ID len, should be same as "size of struct"
-		"RF CXL_Cntr Temp",
+		"RF_CXL_TEMP_C",
 	},
 	{
 		// P5V STBY ADC voltage

commit 40fde45be00ac5210c74bfd3d4b0c54d04c90e0d

fby35:rf Fix merge issue caused by PR #943
Summary:
PR#943 caused an issue where the RF BIC code would not build due to a
conflict.  This commit resolves that issue by fixing the merge issue.

Reviewed By: williamspatrick

Differential Revision: D43814532

fbshipit-source-id: bda2a02e0a204ebdf07988c9ee14cbc53a2db195

meta-facebook/yv35-rf/src/platform/plat_sdr_table.c
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software