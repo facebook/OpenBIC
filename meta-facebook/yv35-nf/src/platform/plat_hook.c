0ab27e1221836dd4aba5375f2baa47dbd8c8769d
SaraLin-wiwynn <Sara_SY_Lin@wiwynn.com>
2023-03-20 14:57:46 +0800

    fby35: NF: Support sensor reading
    Summary:
    - Suport monitor temperature.
    - Support monitor ADC voltage.
    - Support monitor VR (xdpe12284c) voltage, current, and power.
    - Support monitor INA233 voltage, current, and power.
    
    Test plan:
    - Build code: Pass
    - Check sensor table: Pass
    
    Note: Currently, we don't have NF board, sensor readings aren't verified.
    
    Log:
    uart:~$ platform sensor list_all
    ---------------------------------------------------------------------------------
    [0x50] NF_1OU_BOARD_INLET_TEMP_C: tmp75      | access[O] | poll[O] 1    sec | 4byte_acur_read_success   |    24.000
    [0x56] NF_ADC_P1V2_STBY_VOLT_V  : adc        | access[O] | poll[O] 1    sec | 4byte_acur_read_success   |     0.317
    [0x57] NF_ADC_P1V2_ASIC_VOLT_V  : adc        | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x58] NF_ADC_P1V8_ASIC_VOLT_V  : adc        | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x59] NF_ADC_PVPP_AB_VOLT_V    : adc        | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x5a] NF_ADC_PVPP_CD_VOLT_V    : adc        | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x5b] NF_ADC_PVTT_AB_VOLT_V    : adc        | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x5c] NF_ADC_PVTT_CD_VOLT_V    : adc        | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x5d] NF_ADC_P0V75_ASIC_VOLT_V : adc        | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x5e] NF_INA233_P12V_STBY_VOLT_V: ina233     | access[O] | poll[O] 1    sec | 4byte_acur_read_success   |    12.463
    [0x5f] NF_INA233_P3V3_STBY_VOLT_V: ina233     | access[O] | poll[O] 1    sec | 4byte_acur_read_success   |     3.318
    [0x64] NF_INA233_P12V_STBY_CURR_A: ina233     | access[O] | poll[O] 1    sec | 4byte_acur_read_success   |     0.154
    [0x65] NF_INA233_P3V3_STBY_CURR_A: ina233     | access[O] | poll[O] 1    sec | 4byte_acur_read_success   |     0.112
    [0x6a] NF_INA233_P12V_STBY_PWR_W: ina233     | access[O] | poll[O] 1    sec | 4byte_acur_read_success   |     1.924
    [0x6b] NF_INA233_P3V3_STBY_PWR_W: ina233     | access[O] | poll[O] 1    sec | 4byte_acur_read_success   |     0.375
    [0x52] NF_VR_P0V85_ASIC_TEMP_C  : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x53] NF_VR_PVDDQ_AB_TEMP_C    : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x54] NF_VR_P0V8_ASIC_TEMP_C   : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x55] NF_VR_PVDDQ_CD_TEMP_C    : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x60] NF_VR_P0V85_ASIC_VOLT_V  : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x61] NF_VR_PVDDQ_AB_VOLT_V    : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x62] NF_VR_P0V8_ASIC_VOLT_V   : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x63] NF_VR_PVDDQ_CD_VOLT_V    : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x6c] NF_VR_P0V85_ASIC_PWR_W   : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x6d] NF_VR_PVDDQ_AB_PWR_W     : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x6e] NF_VR_P0V8_ASIC_PWR_W    : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x6f] NF_VR_PVDDQ_CD_PWR_W     : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x66] NF_VR_P0V85_ASIC_CURR_A  : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x67] NF_VR_PVDDQ_AB_CURR_A    : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x68] NF_VR_P0V8_ASIC_CURR_A   : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    [0x69] NF_VR_PVDDQ_CD_CURR_A    : xdpe12284c | access[X] | poll[O] 1    sec | not_accesible             | na
    ---------------------------------------------------------------------------------

---
+++ meta-facebook/yv35-nf/src/platform/plat_hook.c
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <logging/log.h>
+#include "sensor.h"
+#include "hal_i2c.h"
+#include "plat_sensor_table.h"
+#include "plat_hook.h"
+
+LOG_MODULE_REGISTER(plat_hook);
+
+adc_asd_init_arg adc_asd_init_args[] = { [0] = { .is_init = false } };
+
+ina233_init_arg ina233_init_args[] = {
+	// P12V
+	[0] = {
+	.is_init = false,
+	.current_lsb = 0.001,
+	.r_shunt = 0.005,
+	.mfr_config_init = true,
+	.mfr_config = {
+		.operating_mode =0b111,
+		.shunt_volt_time = 0b100,
+		.bus_volt_time = 0b100,
+		.aver_mode = 0b011,
+		.rsvd = 0b0100,
+	},
+	},
+	// P3V3
+	[1] = {
+	.is_init = false,
+	.current_lsb = 0.001,
+	.r_shunt = 0.005,
+	.mfr_config_init = true,
+	.mfr_config = {
+		.operating_mode =0b111,
+		.shunt_volt_time = 0b100,
+		.bus_volt_time = 0b100,
+		.aver_mode = 0b011,
+		.rsvd = 0b0100,
+	},
+	},
+};
+
+xdpe12284c_pre_arg xdpe12284c_pre_read_args[] = {
+	[0] = { 0x0 },
+	[1] = { 0x1 },
+};
+
+bool pre_xdpe12284c_read(uint8_t sensor_num, void *args)
+{
+	CHECK_NULL_ARG_WITH_RETURN(args, false);
+
+	xdpe12284c_pre_arg *page_selection = (xdpe12284c_pre_arg *)args;
+	uint8_t retry = 5;
+	I2C_MSG i2c_msg;
+	int ret = 0;
+
+	// Set page
+	memset(&i2c_msg, 0, sizeof(I2C_MSG));
+	i2c_msg.bus = sensor_config[sensor_config_index_map[sensor_num]].port;
+	i2c_msg.target_addr = sensor_config[sensor_config_index_map[sensor_num]].target_addr;
+	i2c_msg.tx_len = 2;
+	i2c_msg.data[0] = VR_PAGE_OFFSET;
+	i2c_msg.data[1] = page_selection->vr_page;
+
+	ret = i2c_master_write(&i2c_msg, retry);
+	if (ret != 0) {
+		LOG_ERR("Failed to set vr page due to write i2c address 0x%x failed, ret: %d",
+			i2c_msg.target_addr, ret);
+		return false;
+	}
+
+	return true;
+}
--- meta-facebook/yv35-nf/src/platform/plat_sensor_table.c
+++ meta-facebook/yv35-nf/src/platform/plat_sensor_table.c
@@ -16,8 +16,136 @@
 
 #include <stdio.h>
 #include "sensor.h"
+#include "ast_adc.h"
+#include "pmbus.h"
+#include "plat_i2c.h"
+#include "plat_hook.h"
 #include "plat_sensor_table.h"
 
-sensor_cfg plat_sensor_config[] = {};
+sensor_cfg plat_sensor_config[] = {
+	/* number,                  type,       port,      address,      offset,
+     access check arg0, arg1, cache, cache_status, mux_ADDRess, mux_offset,
+     pre_sensor_read_fn, pre_sensor_read_args, post_sensor_read_fn, post_sensor_read_fn  */
 
-const int SENSOR_CONFIG_SIZE = 0;
+	// Temperature =========================================================================
+	{ SENSOR_NUM_TEMP_TMP75, sensor_dev_tmp75, I2C_BUS3, TMP75_1OU_BOARD_ADDR,
+	  TMP75_TEMP_OFFSET, stby_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, NULL, NULL, NULL, NULL, NULL },
+
+	// ADC =================================================================================
+	{ SENSOR_NUM_VOL_P1V2_STBY, sensor_dev_ast_adc, ADC_PORT3, NONE, NONE, stby_access, 1, 1,
+	  SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS,
+	  NULL, NULL, NULL, NULL, &adc_asd_init_args[0] },
+	{ SENSOR_NUM_VOL_P1V2_ASIC, sensor_dev_ast_adc, ADC_PORT5, NONE, NONE, dc_access, 1, 1,
+	  SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS,
+	  NULL, NULL, NULL, NULL, &adc_asd_init_args[0] },
+	{ SENSOR_NUM_VOL_P1V8_ASIC, sensor_dev_ast_adc, ADC_PORT6, NONE, NONE, dc_access, 1, 1,
+	  SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS,
+	  NULL, NULL, NULL, NULL, &adc_asd_init_args[0] },
+	{ SENSOR_NUM_VOL_PVPP_AB, sensor_dev_ast_adc, ADC_PORT8, NONE, NONE, dc_access, 1, 1,
+	  SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS,
+	  NULL, NULL, NULL, NULL, &adc_asd_init_args[0] },
+	{ SENSOR_NUM_VOL_PVPP_CD, sensor_dev_ast_adc, ADC_PORT9, NONE, NONE, dc_access, 1, 1,
+	  SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS,
+	  NULL, NULL, NULL, NULL, &adc_asd_init_args[0] },
+	{ SENSOR_NUM_VOL_PVTT_AB, sensor_dev_ast_adc, ADC_PORT10, NONE, NONE, dc_access, 1, 1,
+	  SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS,
+	  NULL, NULL, NULL, NULL, &adc_asd_init_args[0] },
+	{ SENSOR_NUM_VOL_PVTT_CD, sensor_dev_ast_adc, ADC_PORT11, NONE, NONE, dc_access, 1, 1,
+	  SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS,
+	  NULL, NULL, NULL, NULL, &adc_asd_init_args[0] },
+	{ SENSOR_NUM_VOL_P0V75_ASIC, sensor_dev_ast_adc, ADC_PORT14, NONE, NONE, dc_access, 1, 1,
+	  SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS,
+	  NULL, NULL, NULL, NULL, &adc_asd_init_args[0] },
+
+	// INA233 ==============================================================================
+	{ SENSOR_NUM_VOL_P12V_STBY, sensor_dev_ina233, I2C_BUS1, INA233_12V_ADDR, PMBUS_READ_VOUT,
+	  stby_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0,
+	  SENSOR_INIT_STATUS, NULL, NULL, NULL, NULL, &ina233_init_args[0] },
+	{ SENSOR_NUM_VOL_P3V3_STBY, sensor_dev_ina233, I2C_BUS1, INA233_3V3_ADDR, PMBUS_READ_VOUT,
+	  stby_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0,
+	  SENSOR_INIT_STATUS, NULL, NULL, NULL, NULL, &ina233_init_args[1] },
+	{ SENSOR_NUM_CUR_P12V_STBY, sensor_dev_ina233, I2C_BUS1, INA233_12V_ADDR, PMBUS_READ_IOUT,
+	  stby_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0,
+	  SENSOR_INIT_STATUS, NULL, NULL, NULL, NULL, &ina233_init_args[0] },
+	{ SENSOR_NUM_CUR_P3V3_STBY, sensor_dev_ina233, I2C_BUS1, INA233_3V3_ADDR, PMBUS_READ_IOUT,
+	  stby_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0,
+	  SENSOR_INIT_STATUS, NULL, NULL, NULL, NULL, &ina233_init_args[1] },
+	{ SENSOR_NUM_PWR_P12V_STBY, sensor_dev_ina233, I2C_BUS1, INA233_12V_ADDR, PMBUS_READ_POUT,
+	  stby_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0,
+	  SENSOR_INIT_STATUS, NULL, NULL, NULL, NULL, &ina233_init_args[0] },
+	{ SENSOR_NUM_PWR_P3V3_STBY, sensor_dev_ina233, I2C_BUS1, INA233_3V3_ADDR, PMBUS_READ_POUT,
+	  stby_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT, ENABLE_SENSOR_POLLING, 0,
+	  SENSOR_INIT_STATUS, NULL, NULL, NULL, NULL, &ina233_init_args[1] },
+
+	// VR ===============================================================================
+	{ SENSOR_NUM_TEMP_P0V85_ASIC, sensor_dev_xdpe12284c, I2C_BUS6, VR_P0V85_ASIC_ADDR,
+	  PMBUS_READ_TEMPERATURE_1, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[1], NULL, NULL, NULL },
+	{ SENSOR_NUM_TEMP_PVDDQ_AB, sensor_dev_xdpe12284c, I2C_BUS6, VR_PVDDQAB_ADDR,
+	  PMBUS_READ_TEMPERATURE_1, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[0], NULL, NULL, NULL },
+	{ SENSOR_NUM_TEMP_P0V8_ASIC, sensor_dev_xdpe12284c, I2C_BUS6, VR_P0V8_ASIC_ADDR,
+	  PMBUS_READ_TEMPERATURE_1, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[1], NULL, NULL, NULL },
+	{ SENSOR_NUM_TEMP_PVDDQ_CD, sensor_dev_xdpe12284c, I2C_BUS6, VR_PVDDQCD_ADDR,
+	  PMBUS_READ_TEMPERATURE_1, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[0], NULL, NULL, NULL },
+
+	{ SENSOR_NUM_VOL_P0V85_ASIC, sensor_dev_xdpe12284c, I2C_BUS6, VR_P0V85_ASIC_ADDR,
+	  PMBUS_READ_VOUT, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[1], NULL, NULL, NULL },
+	{ SENSOR_NUM_VOL_PVDDQ_AB, sensor_dev_xdpe12284c, I2C_BUS6, VR_PVDDQAB_ADDR,
+	  PMBUS_READ_VOUT, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[0], NULL, NULL, NULL },
+	{ SENSOR_NUM_VOL_P0V8_ASIC, sensor_dev_xdpe12284c, I2C_BUS6, VR_P0V8_ASIC_ADDR,
+	  PMBUS_READ_VOUT, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[1], NULL, NULL, NULL },
+	{ SENSOR_NUM_VOL_PVDDQ_CD, sensor_dev_xdpe12284c, I2C_BUS6, VR_PVDDQCD_ADDR,
+	  PMBUS_READ_VOUT, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[0], NULL, NULL, NULL },
+
+	{ SENSOR_NUM_PWR_P0V85_ASIC, sensor_dev_xdpe12284c, I2C_BUS6, VR_P0V85_ASIC_ADDR,
+	  PMBUS_READ_POUT, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[1], NULL, NULL, NULL },
+	{ SENSOR_NUM_PWR_PVDDQ_AB, sensor_dev_xdpe12284c, I2C_BUS6, VR_PVDDQAB_ADDR,
+	  PMBUS_READ_POUT, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[0], NULL, NULL, NULL },
+	{ SENSOR_NUM_PWR_P0V8_ASIC, sensor_dev_xdpe12284c, I2C_BUS6, VR_P0V8_ASIC_ADDR,
+	  PMBUS_READ_POUT, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[1], NULL, NULL, NULL },
+	{ SENSOR_NUM_PWR_PVDDQ_CD, sensor_dev_xdpe12284c, I2C_BUS6, VR_PVDDQCD_ADDR,
+	  PMBUS_READ_POUT, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[0], NULL, NULL, NULL },
+
+	{ SENSOR_NUM_CUR_P0V85_ASIC, sensor_dev_xdpe12284c, I2C_BUS6, VR_P0V85_ASIC_ADDR,
+	  PMBUS_READ_IOUT, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[1], NULL, NULL, NULL },
+	{ SENSOR_NUM_CUR_PVDDQ_AB, sensor_dev_xdpe12284c, I2C_BUS6, VR_PVDDQAB_ADDR,
+	  PMBUS_READ_IOUT, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[0], NULL, NULL, NULL },
+	{ SENSOR_NUM_CUR_P0V8_ASIC, sensor_dev_xdpe12284c, I2C_BUS6, VR_P0V8_ASIC_ADDR,
+	  PMBUS_READ_IOUT, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[1], NULL, NULL, NULL },
+	{ SENSOR_NUM_CUR_PVDDQ_CD, sensor_dev_xdpe12284c, I2C_BUS6, VR_PVDDQCD_ADDR,
+	  PMBUS_READ_IOUT, dc_access, 0, 0, SAMPLE_COUNT_DEFAULT, POLL_TIME_DEFAULT,
+	  ENABLE_SENSOR_POLLING, 0, SENSOR_INIT_STATUS, pre_xdpe12284c_read,
+	  &xdpe12284c_pre_read_args[0], NULL, NULL, NULL },
+};
+
+const int SENSOR_CONFIG_SIZE = ARRAY_SIZE(plat_sensor_config);
