6827121cb7c647bfa650b7f7fee2c71f95acf844
SaraLin-wiwynn <Sara_SY_Lin@wiwynn.com>
2023-03-28 14:33:58 -0700

    fby35: nf: Initialize Niagara Falls BIC (#1002)
    Summary:
    - Initialize Niagara Falls BIC.
    - Add project code in meta-facebook/y35-nf/.
    - Add platform to build scripts and sign image script.
    - Initialize NF IPMB and FRU table.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1002
    
    Test Plan:
    - Build code: Pass
    - Login BIC console: Pass
    
    Log:
    1. Can build code successfully. [22/28] Linking C executable zephyr/zephyr_prebuilt.elf
    
    [28/28] Linking C executable zephyr/Y35BNF.elf
    Memory region         Used Size  Region Size  %age Used
             SRAM_NC:         12 KB       320 KB      3.75%
               FLASH:          0 GB         0 GB
                SRAM:      407576 B       448 KB     88.84%
            IDT_LIST:          0 GB         2 KB      0.00%
    
    2. Can login into NF BIC console. [00:00:00.002,000] <wrn*** Booting Zephyr OS build v00.01.06-35-g4c1cc44ed1d3  *** Hello, welcome to Yosemite V3.5 Niagara Falls 2023.11.1
    > usb_dc_aspeed: pre-selected ep[0x2] as IN endpoint
    [00:00:00.002,000] <inf> usb_dc_aspeed: select ep[0x3] as OUT endpoint [00:00:00.055,000] <err> spi_nor_multi_dev: [1347][spi1_cs0]SFDP magic 00000000 invalid [00:00:00.055,000] <err> spi_nor_multi_dev: [1609]SFDP read failed: -22 [00:00:00.084,000] <err> sensor: Init sensor size is zero [00:00:00.084,000] <inf> ipmb: Initial IPMB TX/RX threads, bus(0x8), addr(0x20) [00:00:00.087,000] <inf> usb_cdc_acm: Device suspended
    
    uart:~$ [00:00:00.539,000] <inf> usb_cdc_acm: Device resumed
    [00:00:00.539,000] <inf> usb_cdc_acm: from suspend
    [00:00:00.863,000] <inf> usb_cdc_acm: Device configured
    
    uart:~$ platform info
    ========================{SHELL COMMAND INFO}========================================
    * NAME:          Platform command
    * DESCRIPTION:   Commands that could be used to debug or validate.
    * DATE/VERSION:  none
    * CHIP/OS:       ast1030_evb - Zephyr
    * Note:          none
    ------------------------------------------------------------------
    * PLATFORM:      Yosemite V3.5-Niagara Falls
    * BOARD ID:      3
    * STAGE:         0
    * SYSTEM:        255
    * FW VERSION:    3.1
    * FW DATE:       2023.11.1
    * FW IMAGE:      Y35BNF.bin
    ========================{SHELL COMMAND INFO}========================================
    uart:~$
    
    Reviewed By: zhdaniel12
    
    Differential Revision: D44443419
    
    Pulled By: amithash
    
    fbshipit-source-id: f5d11de4234c3181d1646f41bf7b4a60fe884e25

---
+++ meta-facebook/yv35-nf/src/platform/plat_ipmb.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "ipmb.h"
+#include "plat_ipmb.h"
+
+IPMB_config pal_IPMB_config_table[] = {
+	// index, interface, channel, bus, channel_target_address, enable_status,
+	// self_address, rx_thread_name, tx_thread_name
+	{ GL_BIC_IPMB_IDX, I2C_IF, GL_BIC_IPMB, IPMB_GL_BIC_BUS, GL_BIC_I2C_ADDRESS, ENABLE,
+	  SELF_I2C_ADDRESS, "RX_GL_BIC_IPMB_TASK", "TX_GL_BIC_IPMB_TASK" },
+	{ RESERVED_IDX, RESERVED_IF, RESERVED, RESERVED_BUS, RESERVED_ADDRESS, DISABLE,
+	  RESERVED_ADDRESS, "RESERVED_ATTR", "RESERVED_ATTR" },
+};
+
+bool pal_load_ipmb_config(void)
+{
+	memcpy(&IPMB_config_table[0], &pal_IPMB_config_table[0], sizeof(pal_IPMB_config_table));
+	return true;
+};
