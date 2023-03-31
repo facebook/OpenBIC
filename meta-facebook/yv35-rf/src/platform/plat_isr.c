adf03a55732c8d9c529232e6b94f4d60f366e867
Irene <Irene.Lin@quantatw.com>
2023-03-31 23:30:19 +0800

    oby35: rf: Fix Power Sequence

--- meta-facebook/yv35-rf/src/platform/plat_isr.c
+++ meta-facebook/yv35-rf/src/platform/plat_isr.c
@@ -32,6 +32,7 @@
 #include "plat_isr.h"
 #include "plat_i2c.h"
 #include "plat_sensor_table.h"
+#include "util_worker.h"
 
 #define POWER_SEQ_CTRL_STACK_SIZE 1000
 #define DC_ON_5_SECOND 5
@@ -44,67 +45,8 @@
 #define ADCUNLOCK_BBEVENT_ERROR 0b10001000 /*ADCUNLOCK and BBEVENT occurred*/
 
 LOG_MODULE_REGISTER(plat_isr);
-K_WORK_DELAYABLE_DEFINE(set_DC_on_5s_work, set_DC_on_delayed_status);
-
-K_THREAD_STACK_DEFINE(power_thread, POWER_SEQ_CTRL_STACK_SIZE);
-struct k_thread power_thread_handler;
-k_tid_t power_tid;
-
-void control_power_sequence()
-{
-	if (gpio_get(FM_POWER_EN) == POWER_ON) { // CraterLake DC on
-		if (gpio_get(PWRGD_CARD_PWROK) == POWER_OFF) {
-			// If power on sequence not finished or not started , abort power off thread before creating power on thread
-			abort_power_thread();
-			init_power_on_thread();
-		} else {
-			// If the last stage of power on sequence already power on , no need to recheck power on sequence
-			// Update the flag of power on sequence number
-			set_power_on_seq(NUMBER_OF_POWER_ON_SEQ);
-		}
-	} else { // CraterLake DC off
-		if (gpio_get(CLK_100M_OSC_EN) == HIGH_ACTIVE) {
-			// If power off sequence not finished or not started , abort power on thread before creating power off thread
-			abort_power_thread();
-			init_power_off_thread();
-		} else {
-			// If the last stage of power off sequence already power off , no need to recheck power off sequence
-			// Update the flag of power off sequence number
-			set_power_off_seq(NUMBER_OF_POWER_OFF_SEQ);
-		}
-	}
-}
-
-void init_power_on_thread()
-{
-	// Avoid re-create thread by checking thread status and thread id
-	if (power_tid != NULL && strcmp(k_thread_state_str(power_tid), "dead") != 0) {
-		return;
-	}
-	power_tid = k_thread_create(&power_thread_handler, power_thread,
-				    K_THREAD_STACK_SIZEOF(power_thread), control_power_on_sequence,
-				    NULL, NULL, NULL, CONFIG_MAIN_THREAD_PRIORITY, 0, K_NO_WAIT);
-	k_thread_name_set(&power_thread_handler, "power_on_sequence_thread");
-}
-
-void init_power_off_thread()
-{
-	// Avoid re-create thread by checking thread status and thread id
-	if (power_tid != NULL && strcmp(k_thread_state_str(power_tid), "dead") != 0) {
-		return;
-	}
-	power_tid = k_thread_create(&power_thread_handler, power_thread,
-				    K_THREAD_STACK_SIZEOF(power_thread), control_power_off_sequence,
-				    NULL, NULL, NULL, CONFIG_MAIN_THREAD_PRIORITY, 0, K_NO_WAIT);
-	k_thread_name_set(&power_thread_handler, "power_off_sequence_thread");
-}
-
-void abort_power_thread()
-{
-	if (power_tid != NULL && strcmp(k_thread_state_str(power_tid), "dead") != 0) {
-		k_thread_abort(power_tid);
-	}
-}
+K_WORK_DEFINE(cxl_power_on_work, control_power_on_sequence);
+K_WORK_DEFINE(cxl_power_off_work, control_power_off_sequence);
 
 void check_power_abnormal(uint8_t power_good_gpio_num, uint8_t control_power_gpio_num)
 {
@@ -112,9 +54,8 @@ void check_power_abnormal(uint8_t power_good_gpio_num, uint8_t control_power_gpi
 		// If the control power pin is high when the power good pin is falling
 		// This means that the behavior of turning off is not expected
 		if (gpio_get(control_power_gpio_num) == CONTROL_ON) {
-			// Sequentially turn off the power
-			abort_power_thread();
-			init_power_off_thread();
+			// submit power off work
+			k_work_submit_to_queue(&plat_work_q, &cxl_power_off_work);
 		}
 	}
 }
@@ -122,25 +63,10 @@ void check_power_abnormal(uint8_t power_good_gpio_num, uint8_t control_power_gpi
 void ISR_MB_DC_STATE()
 {
 	set_MB_DC_status(FM_POWER_EN);
-	control_power_sequence();
-}
-
-void ISR_DC_STATE()
-{
-	set_DC_status(PWRGD_CARD_PWROK);
-
-	// Set a access flag after DC on 5 secs
-	if (get_DC_status() == true) {
-		gpio_set(LED_CXL_POWER, GPIO_HIGH);
-		k_work_schedule(&set_DC_on_5s_work, K_SECONDS(DC_ON_5_SECOND));
-
-	} else {
-		gpio_set(LED_CXL_POWER, GPIO_LOW);
-		if (k_work_cancel_delayable(&set_DC_on_5s_work) != 0) {
-			LOG_ERR("Cancel set dc off delay work fail");
-		}
-		set_DC_on_delayed_status();
-	}
+	if (gpio_get(FM_POWER_EN) == GPIO_HIGH)
+		k_work_submit_to_queue(&plat_work_q, &cxl_power_on_work);
+	else
+		k_work_submit_to_queue(&plat_work_q, &cxl_power_off_work);
 }
 
 void ISR_MB_RST()
