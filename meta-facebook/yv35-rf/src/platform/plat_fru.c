947252ec0832d31d89e15b250aff1fc6ad2b8225
Irene <Irene.Lin@quantatw.com>
2023-04-11 20:12:19 +0800

    oby35: rf: Record the cxl version

--- meta-facebook/yv35-rf/src/platform/plat_fru.c
+++ meta-facebook/yv35-rf/src/platform/plat_fru.c
@@ -17,6 +17,11 @@
 #include <string.h>
 #include "fru.h"
 #include "plat_fru.h"
+#include <logging/log.h>
+#include "eeprom.h"
+#include "libutil.h"
+
+LOG_MODULE_REGISTER(plat_fru);
 
 #define RF_FRU_PORT 0x02
 #define RF_FRU_ADDR (0xA8 >> 1)
@@ -32,6 +37,50 @@ const EEPROM_CFG plat_fru_config[] = {
 		FRU_SIZE,
 	},
 };
+
+const EEPROM_CFG plat_cxl_version_area_config = {
+	NV_ATMEL_24C128,
+	RF_FRU_ID,
+	RF_FRU_PORT,
+	RF_FRU_ADDR,
+	FRU_DEV_ACCESS_BYTE,
+	CXL_FW_VERSION_START,
+	CXL_FW_VERSION_MAX_SIZE,
+};
+
+bool set_cxl_version(EEPROM_ENTRY *entry)
+{
+	CHECK_NULL_ARG_WITH_RETURN(entry, false);
+
+	bool ret = false;
+	entry->config = plat_cxl_version_area_config;
+	entry->data_len = CXL_FW_VERSION_MAX_SIZE;
+
+	ret = eeprom_write(entry);
+	if (ret == false) {
+		printf("eeprom_write fail");
+		return ret;
+	}
+
+	return ret;
+}
+
+bool get_cxl_version(EEPROM_ENTRY *entry)
+{
+	CHECK_NULL_ARG_WITH_RETURN(entry, false);
+
+	bool ret = false;
+	entry->config = plat_cxl_version_area_config;
+	entry->data_len = CXL_FW_VERSION_MAX_SIZE;
+
+	ret = eeprom_read(entry);
+	if (ret == false) {
+		printf("eeprom_read fail");
+		return ret;
+	}
+
+	return ret;
+}
 
 void pal_load_fru_config(void)
 {
