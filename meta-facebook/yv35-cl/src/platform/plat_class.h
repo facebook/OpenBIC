858902fa2b55709b36dc9811bcc4ab19b14ecb5b
Andy Chung <Andy_Chung@wiwynn.com>
2023-05-19 17:28:21 +0800

    fby3.5: cl: adjust HSC type ADC tolerance
    Description:
    Adjust HSC type ADC tolerance from 15% to 0.2V.
    
    Motivation:
    We've added the “HSC_TYPE_ADC” waveforms with about 1min timeframe,
    and the absolute peak voltages are within the range of +-0.2V for all three types of HSC.
    
    Test plan:
    Build and test pass on fby35 system.
    1. HSC sensor reading
    root@bmc-oob:~# sensor-util slot1 | grep -i hsc
    MB_HSC_TEMP_C                (0xE) :   25.94 C     | (ok)
    MB_HSC_INPUT_VOLT_V          (0x29) :   12.53 Volts  | (ok)
    MB_HSC_OUTPUT_CURR_A         (0x30) :    6.30 Amps  | (ok)
    MB_HSC_INPUT_PWR_W           (0x39) :   77.85 Watts  | (ok)

--- meta-facebook/yv35-cl/src/platform/plat_class.h
+++ meta-facebook/yv35-cl/src/platform/plat_class.h
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -32,6 +32,7 @@
 #define I2C_DATA_SIZE 5
 #define NUMBER_OF_ADC_CHANNEL 16
 #define AST1030_ADC_BASE_ADDR 0x7e6e9000
+#define HSC_TYPE_ADC_TOLERANCE 0.2 // V
 
 #define MAX_1OU_M2_COUNT 4
 
