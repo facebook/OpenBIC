9d715e21f8c2efdf8725299b45a441fafe63cdd6
Yang Chen <Yang.Chen@quantatw.com>
2023-06-12 15:14:53 -0700

    GT: Disable NCT7718 alert function (#1160)
    Summary:
    - Disable NCT7718 alert function by setting configuration register bit 7 to 1.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1160
    
    Test Plan: - Build code: Pass
    
    Reviewed By: wangx6f
    
    Differential Revision: D46549270
    
    Pulled By: amithash
    
    fbshipit-source-id: 6c810bd619d1ab47ed641726a3f2cb5a7804e722

--- meta-facebook/gt-cc/src/platform/plat_hook.h
+++ meta-facebook/gt-cc/src/platform/plat_hook.h
@@ -46,6 +46,7 @@ extern ina230_init_arg ina230_ssd_sensor_init_args[];
 extern pex89000_init_arg pex_sensor_init_args[];
 extern ltc4282_init_arg ltc4282_hsc_init_args[];
 extern ltc4286_init_arg ltc4286_hsc_init_args[];
+extern nct7718w_init_arg nct7718w_init_args[];
 /**************************************************************************************************
  *  PRE-HOOK/POST-HOOK ARGS
  **************************************************************************************************/
