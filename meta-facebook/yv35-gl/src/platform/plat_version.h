aac51066f2e287776ccfba485dfd4c55b6c4625d
SaraLin-wiwynn <Sara_SY_Lin@wiwynn.com>
2023-06-29 17:34:16 +0800

    fby3.5: gl: Version commit for oby35-gl-2023.27.01
    Description
    - Version commit for Yv3.5 Great Lakes BIC oby35-gl-2023.27.01.
    
    Motivation
    - Plan to release Yv3.5 Great Lakes BIC oby35-gl-2023.27.01.
    
    Test Plan
    - Build code: Pass
    - Get BIC version: Pass
    
    Log:
    root@bmc-oob:~# fw-util slot1 --version bic
    SB Bridge-IC Version: oby35-gl-v2023.27.01

--- meta-facebook/yv35-gl/src/platform/plat_version.h
+++ meta-facebook/yv35-gl/src/platform/plat_version.h
@@ -32,7 +32,7 @@
 #define DEVICE_REVISION 0x80
 
 #define FIRMWARE_REVISION_1 GET_FW_VERSION1(BOARD_ID, PROJECT_STAGE)
-#define FIRMWARE_REVISION_2 0x4
+#define FIRMWARE_REVISION_2 0x5
 
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
@@ -41,7 +41,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x23
-#define BIC_FW_WEEK 0x26
+#define BIC_FW_WEEK 0x27
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x67 // char: g
 #define BIC_FW_platform_1 0x6c // char: l
