812330476a29ef27e92a40c28c05a065a87ab951
Yang Chen <Yang.Chen@quantatw.com>
2023-07-13 11:50:35 -0700

    GT: Modify NIC FRU read function (#1234)
    Summary:
    - Modify NIC FRU read function.
    - Add SRAM size to 708 KB.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1234
    
    Test Plan: - Build code: Pass
    
    Reviewed By: amithash
    
    Differential Revision: D47437919
    
    fbshipit-source-id: 229d82a94623c6a8ca9251e76f491bfe03d7c5a9

--- meta-facebook/gt-cc/src/platform/plat_fru.c
+++ meta-facebook/gt-cc/src/platform/plat_fru.c
@@ -16,11 +16,15 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <logging/log.h>
+#include "libutil.h"
 #include "fru.h"
 #include "plat_fru.h"
 #include "hal_gpio.h"
 #include "plat_sensor_table.h"
 #include "plat_pldm_monitor.h"
+
+LOG_MODULE_REGISTER(plat_fru);
 
 extern struct k_mutex i2c_bus6_mutex;
 
@@ -136,34 +140,85 @@ const EEPROM_CFG plat_fru_config[] = {
 	},
 };
 
+static bool find_nic_product_name(uint8_t dev_id, uint8_t *part_number_p)
+{
+	CHECK_NULL_ARG_WITH_RETURN(part_number_p, false);
+
+	EEPROM_ENTRY fru_entry;
+	fru_entry.config.dev_id = dev_id;
+	fru_entry.offset = 0x0000;
+	fru_entry.data_len = 8;
+
+	if (FRU_read(&fru_entry) != FRU_READ_SUCCESS)
+		return false;
+
+	uint16_t board_area_offset;
+
+	fru_entry.offset = board_area_offset = (fru_entry.data[3] * 8);
+	fru_entry.data_len = 2;
+
+	if (FRU_read(&fru_entry) != FRU_READ_SUCCESS)
+		return false;
+
+	uint16_t board_area_len = fru_entry.data[1] * 8;
+	uint8_t board_data[board_area_len];
+
+	uint16_t total_read_byte = 0;
+	uint8_t bytes_read = 0;
+	uint8_t buf_ofs = 0;
+
+	for (; total_read_byte < board_area_len; total_read_byte += bytes_read) {
+		bytes_read = ((total_read_byte + bytes_read) > board_area_len) ?
+				     (board_area_len - total_read_byte) :
+				     EEPROM_WRITE_SIZE;
+
+		fru_entry.offset = board_area_offset + buf_ofs;
+		fru_entry.data_len = bytes_read;
+
+		if (FRU_read(&fru_entry) != FRU_READ_SUCCESS)
+			return false;
+
+		memcpy(&board_data[buf_ofs], fru_entry.data, bytes_read);
+		buf_ofs += bytes_read;
+	}
+
+	buf_ofs = 6; // board manufactor type/length offset
+
+	// Get board part number type/length offset
+	for (uint8_t i = 0; i < 3; i++)
+		buf_ofs += (board_data[buf_ofs] & 0x1F) + 1;
+
+	// Get board part number offset
+	buf_ofs += 1;
+
+	memcpy(part_number_p, &board_data[buf_ofs], 4);
+	return true;
+}
+
 uint8_t check_nic_type_by_fru()
 {
 	uint8_t config = 0x00; // 0: default is CX7, 1: unknown or FRU read failed
 
-	for (uint8_t i = 0; i <= NIC_MAX_NUMBER; i++) {
+	for (uint8_t i = 0; i < NIC_MAX_NUMBER; i++) {
 		if (gpio_get(nic_prsnt_pin[i]))
 			continue;
 
-		EEPROM_ENTRY fru_entry;
-		fru_entry.config.dev_id = NIC0_FRU_ID + i;
-		fru_entry.offset = 0x48;
-		fru_entry.data_len = 4;
+		uint8_t part_number_data[4];
+		bool ret = find_nic_product_name(NIC0_FRU_ID + i, part_number_data);
 
-		uint8_t ret = FRU_read(&fru_entry);
-
-		if ((ret == FRU_READ_SUCCESS) && !strncmp(&fru_entry.data[0], "MCX7", 4)) {
-			printf("NIC%d is CX7 IB NIC\n", i);
+		if (ret && !strncmp(part_number_data, "MCX7", 4)) {
+			LOG_INF("NIC%d is CX7 IB NIC", i);
 			return NIC_CONFIG_IB_CX7;
-		} else if ((ret == FRU_READ_SUCCESS) && !strncmp(&fru_entry.data[0], "CX7", 3)) {
-			printf("NIC%d is CX7 NIC\n", i);
+		} else if (ret && !strncmp(part_number_data, "CX7", 3)) {
+			LOG_INF("NIC%d is CX7 NIC", i);
 			continue;
 		} else {
-			printf("NIC%d is UNKNOWN NIC\n", i);
+			LOG_WRN("NIC%d is UNKNOWN NIC", i);
 			config |= 1 << i;
 		}
 	}
 
-	return config ? NIC_CONFIG_UNKNOWN : NIC_CONFIG_CX7;
+	return (config ? NIC_CONFIG_UNKNOWN : NIC_CONFIG_CX7);
 }
 
 void pal_load_fru_config(void)
