60d2876ffbff1f853dd62a0f6afdffe225758bc8
SeanChuangWiwynn <Sean_Chuang@wiwynn.com>
2023-07-14 16:50:16 -0700

    fby3.5: cl: Version commit for oby35-cl-2023.28.02 (#1239)
    Summary:
    # Description:
    Version commit for Yv3.5 Craterlake BIC oby35-cl-2023.28.02
    
    # Motivation:
    Version commit for Yv3.5 Craterlake BIC oby35-cl-2023.28.02
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1239
    
    Test Plan:
    root@bmc-oob:~# fw-util slot1 --version bic
    SB Bridge-IC Version: oby35-cl-v2023.28.02
    
    Reviewed By: wangx6f
    
    Differential Revision: D47466284
    
    fbshipit-source-id: 29f6e940cdb69c74aa13fc9211c9be29845f41d9

--- meta-facebook/yv35-cl/src/platform/plat_version.h
+++ meta-facebook/yv35-cl/src/platform/plat_version.h
@@ -32,7 +32,7 @@
 #define DEVICE_REVISION 0x80
 
 #define FIRMWARE_REVISION_1 GET_FW_VERSION1(BOARD_ID, PROJECT_STAGE)
-#define FIRMWARE_REVISION_2 0x02
+#define FIRMWARE_REVISION_2 0x03
 
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
@@ -42,7 +42,7 @@
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x23
 #define BIC_FW_WEEK 0x28
-#define BIC_FW_VER 0x01
+#define BIC_FW_VER 0x02
 #define BIC_FW_platform_0 0x63 // char: c
 #define BIC_FW_platform_1 0x6c // char: l
 #define BIC_FW_platform_2 0x00 // char: '\0'
