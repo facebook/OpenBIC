1060e481238a6324162665e6637bc49b5b1542ea
marshallZhan <Marshall_Zhan@wiwynn.com>
2023-08-14 13:26:16 -0700

    artemis: support BIC pldm update (#1280)
    Summary:
    # Description
    - Support ACB/MEB BIC pldm update to avoid use force for normal update.
    - Modify the python command to python3 in pldm_update_pkg_gen.py.
    
    # Motivation
    - Support ACB/MEB BIC pldm update to avoid use force for normal update.
    
    Note
    Jira link: https://metainfra.atlassian.net/browse/T17GTART-156
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1280
    
    Test Plan:
    Build code: Pass
    
    Update acb bic:
    root@bmc-oob:~# fw-util cb --update bic at_cb_ast1030_v2023.31.a1.pldm RequestUpdate Success.
    PassComponentTable Success.
    UpdateComponent Success.
    Download offset : 0x00045700/0x00045720, size : 0x00000020 TransferComplete.
    VerifyComplete.
    ApplyComplete.
    ActivateFirmwareComplete.
    Upgrade of cb : bic succeeded
    
    Update meb bic:
    root@bmc-oob:~# fw-util mc --update bic at_mc_ast1030_v2023.31.01.pldm RequestUpdate Success.
    PassComponentTable Success.
    UpdateComponent Success.
    Download offset : 0x00045200/0x000452d8, size : 0x000000d8 TransferComplete.
    VerifyComplete.
    ApplyComplete.
    ActivateFirmwareComplete.
    Upgrade of mc : bic succeeded
    
    Reviewed By: williamspatrick
    
    Differential Revision: D48304981
    
    fbshipit-source-id: 6cb2664792af3b5d7c3f89b5f6f254c32e6ddc42

---
+++ meta-facebook/at-cb/src/platform/plat_pldm_fw_update.c
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <logging/log.h>
+
+#include "libutil.h"
+#include "util_spi.h"
+#include "hal_jtag.h"
+#include "sensor.h"
+#include "pldm_firmware_update.h"
+#include "plat_pldm_fw_update.h"
+#include "plat_ipmi.h"
+#include "plat_gpio.h"
+#include "plat_i2c.h"
+#include "plat_sensor_table.h"
+#include "plat_hook.h"
+#include "plat_class.h"
+
+LOG_MODULE_REGISTER(plat_fwupdate);
+
+/* PLDM FW update table */
+pldm_fw_update_info_t PLDMUPDATE_FW_CONFIG_TABLE[] = {
+	{
+		.enable = true,
+		.comp_classification = COMP_CLASS_TYPE_DOWNSTREAM,
+		.comp_identifier = CB_COMPNT_BIC,
+		.comp_classification_index = 0x00,
+		.pre_update_func = NULL,
+		.update_func = pldm_bic_update,
+		.pos_update_func = NULL,
+		.inf = COMP_UPDATE_VIA_SPI,
+		.activate_method = COMP_ACT_SELF,
+		.self_act_func = pldm_bic_activate,
+		.get_fw_version_fn = NULL,
+	},
+};
+
+void load_pldmupdate_comp_config(void)
+{
+	if (comp_config) {
+		LOG_WRN("PLDM update component table has already been load");
+		return;
+	}
+
+	comp_config_count = ARRAY_SIZE(PLDMUPDATE_FW_CONFIG_TABLE);
+	comp_config = malloc(sizeof(pldm_fw_update_info_t) * comp_config_count);
+	if (!comp_config) {
+		LOG_ERR("comp_config malloc failed");
+		return;
+	}
+
+	memcpy(comp_config, PLDMUPDATE_FW_CONFIG_TABLE, sizeof(PLDMUPDATE_FW_CONFIG_TABLE));
+}
+
+void clear_pending_version(uint8_t activate_method)
+{
+	if (!comp_config || !comp_config_count) {
+		LOG_ERR("Component configuration is empty");
+		return;
+	}
+
+	for (uint8_t i = 0; i < comp_config_count; i++) {
+		if (comp_config[i].activate_method == activate_method)
+			SAFE_FREE(comp_config[i].pending_version_p);
+	}
+}
