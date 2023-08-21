46a9cc8658714bc6e6128861d089d7f3443dc7aa
SeanChuangWiwynn <Sean_Chuang@wiwynn.com>
2023-08-21 11:35:37 -0700

    fby3.5: cl: Optimize BIC memory (#1298)
    Summary:
    Description:
    1. Reduced thread stack size.
    2. Optimized IPMB functionality
    3. Eliminate UART SHELL commands
    
    Motivation:
    To optimize BIC memory utilization for future PLDM/MCTP implementation.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1298
    
    Test Plan:
    1. Check the memory utilization after optimization.
    Memory region         Used Size  Region Size  %age Used
             SRAM_NC:        140 KB       192 KB     72.92%
               FLASH:          0 GB         0 GB
                SRAM:      537872 B       576 KB     91.19%
            IDT_LIST:          0 GB         2 KB      0.00%
    
    2. Stress test pass by our QE team. pass
    
    Reviewed By: williamspatrick
    
    Differential Revision: D48508362
    
    fbshipit-source-id: 39539e8c41bb75c3c64367e3b77a0e4c57b8897a

--- meta-facebook/yv35-cl/src/platform/plat_ipmb.h
+++ meta-facebook/yv35-cl/src/platform/plat_ipmb.h
@@ -32,14 +32,11 @@
 #define BIC1_I2C_ADDRESS 0x20
 #define BIC2_I2C_ADDRESS 0x20
 #define BB_BIC_I2C_ADDRESS 0x20
-#define MAX_IPMB_IDX 5
+#define MAX_IPMB_IDX 3
 
 enum {
-	BMC_IPMB_IDX,
 	ME_IPMB_IDX,
 	EXP1_IPMB_IDX,
-	BB_IPMB_IDX = EXP1_IPMB_IDX,
-	EXP2_IPMB_IDX,
 };
 
 extern IPMB_config pal_IPMB_config_table[];
