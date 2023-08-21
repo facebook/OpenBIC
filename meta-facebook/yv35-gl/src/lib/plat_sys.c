0b08ae9939f5a7a968514640682b50602636f523
JeffShih-wiwynn <Jeff_Shih@wiwynn.com>
2023-08-21 11:34:34 -0700

    fby35: gl: Support to reset BMC by IPMI command (#1293)
    Summary:
    # Description:
        - If BMC get command of reseting BMC , it will pull RST_BMC_R_N.
    
    # Motivation:
        - Support IPMI command of reseting BMC.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1293
    
    Test Plan:
    [root@localhost ~]# ipmitool mc reset cold
        Sent cold reset command to MC
        [root@localhost ~]#
        Broadcast message from root@bmc-oob. (Mon Aug 14 11:12:24 2023):
    
        The system is going down for reboot NOW!
        /*Reconnect to BMC*/
        root@bmc-oob:~# log-util all --print | grep reboot
        0    all      2023-08-14 11:13:22    healthd          BMC Reboot detected - caused by reboot command
        root@bmc-oob:~#
    
    Reviewed By: williamspatrick
    
    Differential Revision: D48458014
    
    fbshipit-source-id: d7ccf665a23ea501b7ba436c02373e85e8848660

--- meta-facebook/yv35-gl/src/lib/plat_sys.c
+++ meta-facebook/yv35-gl/src/lib/plat_sys.c
@@ -19,15 +19,20 @@
 #include "util_sys.h"
 #include "hal_gpio.h"
 #include "plat_gpio.h"
+#include "util_worker.h"
 
 /* BMC reset */
-
 void BMC_reset_handler()
 {
-	/*TODO :
-        1. set GPIO "RST_BMC_R_N" LOW
-        2. k_msleep(10)
-        3. set GPIO "RST_BMC_R_N" HIGH
-    */
-	return;
+    gpio_set(RST_BMC_R_N, GPIO_LOW);
+    k_msleep(10);
+    gpio_set(RST_BMC_R_N, GPIO_HIGH);
 }
+
+K_WORK_DELAYABLE_DEFINE(BMC_reset_work, BMC_reset_handler);
+int pal_submit_bmc_cold_reset()
+{
+    k_work_schedule_for_queue(&plat_work_q, &BMC_reset_work, K_MSEC(BMC_COLD_RESET_DELAY_MS));
+    return 0;
+}
+/* BMC reset */
