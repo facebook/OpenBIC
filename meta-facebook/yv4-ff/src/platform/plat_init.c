d763b5b3181f6f953f1507cab828e7484c9eee04
JeffShih-wiwynn <Jeff_Shih@wiwynn.com>
2023-08-28 14:37:26 -0700

    fby4: ff: Initialize GPIO table (#1310)
    Summary:
    Description:
    - Implement GPIO table according to the "YV4_Floating Falls_BIC_GPIO table_20230810"
    
    Motivation:
    - Initialize server board's GPIO table.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1310
    
    Test Plan: 1. build code: Pass
    
    Reviewed By: jagpalgill
    
    Differential Revision: D48674353
    
    fbshipit-source-id: 3823a1b6a54af9d8f77a39c60ace213d747c554f

--- meta-facebook/yv4-ff/src/platform/plat_init.c
+++ meta-facebook/yv4-ff/src/platform/plat_init.c
@@ -18,5 +18,33 @@
 
 #define DEF_PROJ_GPIO_PRIORITY 78
 
+/*
+ * The operating voltage of GPIO input pins are lower than actual voltage because the chip 
+ * internal pull-down is enabled.
+ * BIC disables the internal GPIO pull-down for all input pins.
+ *
+ * Disabled GPIO list (Refer to chapter 18 of AST1030's datasheet) :
+ * GPIOA3
+ * GPIOE0~GPIOE7
+ * GPIOH1~GPIOU7
+ */
+
+// clang-format off
+SCU_CFG scu_cfg[] = {
+	//register    value
+	{ 0x7e6e2610, 0xFFFFFFF7 },
+	{ 0x7e6e2614, 0x01FFFF00 },
+	{ 0x7e6e2618, 0x00000000 },
+	{ 0x7e6e261c, 0x00000000 },
+	{ 0x7e6e2630, 0x00000000 },
+	{ 0x7e6e2634, 0x00000000 },
+};
+// clang-format on
+
+void pal_pre_init()
+{
+	scu_init(scu_cfg, sizeof(scu_cfg) / sizeof(SCU_CFG));
+}
+
 DEVICE_DEFINE(PRE_DEF_PROJ_GPIO, "PRE_DEF_PROJ_GPIO_NAME", &gpio_init, NULL, NULL, NULL,
 	      POST_KERNEL, DEF_PROJ_GPIO_PRIORITY, NULL);
