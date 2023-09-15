df3f10033a1c580c0a2873481f06ce5eac1b3c03
Victor-Jhong <Victor.Jhong@quantatw.com>
2023-09-15 09:59:26 -0700

    GT: Version commit for obgt-cc-2023.37.01 (#1354)
    Summary:
    - Version commit for Grand Teton switch board BIC obgt-cc-2023.37.01.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1354
    
    Test Plan:
    - Build code: Pass
    - Get BIC version: Pass
    
    Log:
    ```
    root@bmc-oob:~# fw-util swb --version bic
    SWB BIC Version: 2023.37.01
    ```
    
    Reviewed By: jagpalgill
    
    Differential Revision: D49312463
    
    fbshipit-source-id: 42261cabfd033be834a2c4d1acf831063adf1b72

--- meta-facebook/gt-cc/src/platform/plat_version.h
+++ meta-facebook/gt-cc/src/platform/plat_version.h
@@ -31,7 +31,7 @@
 #define DEVICE_REVISION 0x80
 
 #define FIRMWARE_REVISION_1 GET_FW_VERSION1(BOARD_ID, PROJECT_STAGE)
-#define FIRMWARE_REVISION_2 0x03
+#define FIRMWARE_REVISION_2 0x04
 
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
@@ -40,7 +40,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x23
-#define BIC_FW_WEEK 0x28
+#define BIC_FW_WEEK 0x37
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x47 // char: G
 #define BIC_FW_platform_1 0x54 // char: T
