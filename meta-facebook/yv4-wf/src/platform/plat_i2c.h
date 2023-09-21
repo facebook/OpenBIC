3c8610cffc779d45868ed8309c65562463529c98
Jeff_Shih <Jeff_Shih@wiwynn.com>
2023-09-20 17:42:45 -0700

    fby4: wf: Initialize Wailua Falls BIC layer (#1365)
    Summary:
    Description
        - Create Wailua Falls BIC layer.
        - Add project code in meta-facebook/yv4-wf/.
    
    Motivation
        - Support Yv4 1OU board Wailua Falls BIC.
    
    Test log:
    - Build code: Pass
    - Access to BIC console: Pass root@bmc-oob:~# sol-util slot1 sb bic ------------------TERMINAL MULTIPLEXER---------------------
    	  CTRL-l ?   : Display help message.
    	  CTRL-l x : Terminate the connection.
    	  /var/log/mTerm_slot1.log : Log location
    	  CTRL-l + b : Send Break
    
    	-----------------------------------------------------------
    	uart:~$ platform info
    	========================{SHELL COMMAND INFO}========================================
    	* NAME:          Platform command
    	* DESCRIPTION:   Commands that could be used to debug or validate.
    	* DATE/VERSION:  none
    	* CHIP/OS:       ast1030_evb - Zephyr
    	* Note:          none
    	------------------------------------------------------------------
    	* PLATFORM:      Yosemite V4-Wailua Falls
    	* BOARD ID:      4
    	* STAGE:         0
    	* SYSTEM:        255
    	* FW VERSION:    4.1
    	* FW DATE:       2023.36.1
    	* FW IMAGE:      Y4BWF.bin
    	========================{SHELL COMMAND INFO}========================================
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1365
    
    Reviewed By: amithash
    
    Differential Revision: D49453837
    
    fbshipit-source-id: 4449a74ee7aa4a2ab497b9aaa8e171d06e5017c7

---
+++ meta-facebook/yv4-wf/src/platform/plat_i2c.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PLAT_I2C_h
+#define PLAT_I2C_h
+
+#include "hal_i2c.h"
+
+// map i2c bus to peripherial bus
+// i2c peripheral 1 based, as used i2c index 0 in firmware.
+enum name {
+	I2C_BUS1 = 0,
+	I2C_BUS2,
+	I2C_BUS3,
+	I2C_BUS4,
+	I2C_BUS5,
+	I2C_BUS6,
+	I2C_BUS7,
+	I2C_BUS8,
+	I2C_BUS9,
+	I2C_BUS10,
+	I2C_BUS_MAX_NUM,
+};
+
+#endif
