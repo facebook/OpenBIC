b6fb55a4e92b4151b76d335214235233808534f4
Sara Lin <sara_sy_lin@wiwynn.com>
2023-10-31 14:13:44 -0700

    fby4: wf: Support get/set GPIO and switch UART (#1437)
    Summary:
    # Description
    - Support switch UART between BIC, CXL1 and CXL2. Implement PLDM_switch_UART handler to get the request from BMC.
    - Support BMC to get/set GPIO of BIC
    
    # Motivation:
    - Support get/set GPIO and switch UART.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1437
    
    Test Plan:
    - Build code: Pass
    - Switch UART: Pass
    - Get/set GPIO: Pass
    
    Reviewed By: jagpalgill
    
    Differential Revision: D50722279
    
    fbshipit-source-id: abadc73b7be3f6219cafa69ff3d8dbc5b6d0c056

---
+++ meta-facebook/yv4-wf/src/platform/plat_pldm_monitor.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PLAT_PLDM_MONITOR_H
+#define PLAT_PLDM_MONITOR_H
+
+// Refer to Chapter32: LPC Controller in AST1030 datasheet v1.0
+#define LPC_BASE_ADDR 0x7E789000
+#define LPC_HICR9_REG (LPC_BASE_ADDR + 0x98)
+#define LPC_HICRA_REG (LPC_BASE_ADDR + 0x9C)
+
+#define PLAT_PLDM_MAX_STATE_EFFECTER_IDX 169
+
+enum pldm_plat_effecter_id_high_byte {
+	PLAT_EFFECTER_ID_GPIO_HIGH_BYTE = (0xFF << 8),
+};
+
+enum plat_pldm_effecter_id {
+	PLAT_PLDM_EFFECTER_ID_UART_SWITCH = 0x0003,
+};
+
+enum plat_pldm_uart_number {
+	UART_BIC = 0,
+	UART_VISTARA_CXL1,
+	UART_VISTARA_CXL2,
+	UART_MAX,
+};
+
+extern struct pldm_state_effecter_info plat_state_effecter_table[];
+
+#endif
