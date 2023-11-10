7f872c0dc1e3a9548205ea35857ad4f7115376b1
PeterHo-wiwynn <Peter_MH_Ho@wiwynn.com>
2023-11-10 10:27:14 -0800

    yv4-wf: add mctp routing table (#1434)
    Summary:
    # Description:
    Add mctp routing table for yv4-wf.
    
    # Motivation:
    We need to support mctp over i2c/i3c for yv4.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1434
    
    Test Plan:
    - Build code: Pass
    - Test on yv4 platform: Pass
    
    Reviewed By: jagpalgill, amithash
    
    Differential Revision: D50634534
    
    fbshipit-source-id: c655582a3edcbfbf845f270f6d2f083972add80c

--- meta-facebook/yv4-wf/src/platform/plat_init.c
+++ meta-facebook/yv4-wf/src/platform/plat_init.c
@@ -22,8 +22,13 @@
 #include "plat_power_seq.h"
 #include "plat_pldm_monitor.h"
 #include "plat_isr.h"
+#include "plat_mctp.h"
+#include "plat_i2c_target.h"
 
 #define DEF_PROJ_GPIO_PRIORITY 78
+
+DEVICE_DEFINE(PRE_DEF_PROJ_GPIO, "PRE_DEF_PROJ_GPIO_NAME", &gpio_init, NULL, NULL, NULL,
+	      POST_KERNEL, DEF_PROJ_GPIO_PRIORITY, NULL);
 
 void pal_set_sys_status()
 {
@@ -33,12 +38,20 @@ void pal_set_sys_status()
 	set_ioe4_pin();
 }
 
+void pal_pre_init()
+{
+	/* init i2c target */
+	for (int index = 0; index < MAX_TARGET_NUM; index++) {
+		if (I2C_TARGET_ENABLE_TABLE[index])
+			i2c_target_control(
+				index, (struct _i2c_target_config *)&I2C_TARGET_CONFIG_TABLE[index],
+				1);
+	}
+}
+
 void pal_post_init()
 {
 	pldm_load_state_effecter_table(PLAT_PLDM_MAX_STATE_EFFECTER_IDX);
 	pldm_assign_gpio_effecter_id(PLAT_EFFECTER_ID_GPIO_HIGH_BYTE);
+	plat_mctp_init();
 }
-
-
-DEVICE_DEFINE(PRE_DEF_PROJ_GPIO, "PRE_DEF_PROJ_GPIO_NAME", &gpio_init, NULL, NULL, NULL,
-	      POST_KERNEL, DEF_PROJ_GPIO_PRIORITY, NULL);
