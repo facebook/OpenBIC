e3afc9aefab43f28d5de506603197130cadecc1e
Sara Lin <sara_sy_lin@wiwynn.com>
2023-11-16 09:23:50 -0800

    fby4: wf: Correct power sequence log (#1469)
    Summary:
    # Description
    - Change CXL number of message from 0 base to 1 base.
    
    # Motivation
    - Avoid user confusion.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1469
    
    Test Plan:
    - Build code: Pass
    - Get log: Pass
    
    Reviewed By: williamspatrick
    
    Differential Revision: D51335643
    
    fbshipit-source-id: 161f05e70c0e26ec3eb36b9170aaa8fba334d550

--- meta-facebook/yv4-wf/src/platform/plat_power_seq.c
+++ meta-facebook/yv4-wf/src/platform/plat_power_seq.c
@@ -301,7 +301,7 @@ bool is_power_controlled(int cxl_id, int power_pin, uint8_t check_power_status, 
 	} else {
 		// TODO: Add event to BMC
 		LOG_ERR("Failed to power %s CXL %d %s (gpio num %d)", check_power_status ? "on" : "off",
-			cxl_id, power_name, power_pin);
+			cxl_id + 1, power_name, power_pin);
 		return false;
 	}
 }
