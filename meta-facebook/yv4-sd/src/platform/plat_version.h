cd584793d6e2bc2dae27a562a9db0d0ac7066635
Eli_Huang <Eli_Huang@wiwynn.com>
2023-12-05 17:52:43 +0800

    fby4: sd: Version commit for oby4-sd-2023.49.01
    Description:
    - Version commit for Sentinel Dome BIC oby4-sd-2023.49.01.
    
    Motivation:
    - Version commit for Sentinel Dome BIC oby4-sd-2023.49.01.
    
    Test Plan: - Build code: Pass

--- meta-facebook/yv4-sd/src/platform/plat_version.h
+++ meta-facebook/yv4-sd/src/platform/plat_version.h
@@ -41,7 +41,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x23
-#define BIC_FW_WEEK 0x46
+#define BIC_FW_WEEK 0x49
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x73 // char: s
 #define BIC_FW_platform_1 0x64 // char: d
