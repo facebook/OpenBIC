66e2658830d8612fdf9c1f1a71b168b0d1c00dc0
Eli_Huang <Eli_Huang@wiwynn.com>
2024-02-06 11:35:23 -0800

    fby4: wf: Version commit for oby4-wf-2024.06.01 (#1615)
    Summary:
    Description:
    - Version commit for Wailua Falls BIC oby4-wf-2024.06.01.
    
    Motivation:
    - Version commit for Wailua Falls BIC oby4-wf-2024.06.01.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1615
    
    Test Plan: - Build code: Pass
    
    Reviewed By: jagpalgill
    
    Differential Revision: D53395525
    
    fbshipit-source-id: d043c40dd46e6a0a8799802769a5ded0dee89d95

--- meta-facebook/yv4-wf/src/platform/plat_version.h
+++ meta-facebook/yv4-wf/src/platform/plat_version.h
@@ -37,7 +37,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x24
-#define BIC_FW_WEEK 0x03
+#define BIC_FW_WEEK 0x06
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x77 // char: w
 #define BIC_FW_platform_1 0x66 // char: f
