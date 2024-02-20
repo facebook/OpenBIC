d772e7faefb004066b386a26f03376049f810fae
MouchenHung <Mouchen.Hung@quantatw.com>
2024-02-20 10:48:09 +0800

    Add new platform code Java Island

---
+++ meta-facebook/yv35-ji/src/platform/plat_sdr_table.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "plat_sdr_table.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "sdr.h"
+#include "sensor.h"
+#include "plat_class.h"
+#include "plat_ipmb.h"
+#include "plat_sensor_table.h"
+#include <logging/log.h>
+
+LOG_MODULE_REGISTER(plat_sdr_table);
+
+SDR_Full_sensor plat_sdr_table[] = {};
+
+const int SDR_TABLE_SIZE = ARRAY_SIZE(plat_sdr_table);
+
+void load_sdr_table(void)
+{
+	memcpy(full_sdr_table, plat_sdr_table, sizeof(plat_sdr_table));
+	sdr_count = ARRAY_SIZE(plat_sdr_table);
+}
