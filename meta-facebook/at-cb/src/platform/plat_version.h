8b9ad01795c559e15f71e487d70b839ae0cd2ae2
MarshallZhan-wiwynn <Marshall_Zhan@wiwynn.com>
2024-03-08 10:53:23 +0800

    Artemis: cb: Version commit for 2024.11.01 Summary: Description: - Version commit for ColterBay BIC 2024.11.01.
    Motivation:
    - Committing the version to release new bic firmware.
    
    Test Plan:
    - Build code: Pass
    - Get firmware version: Pass
    
    Log:
    root@bmc-oob:~# fw-util cb --version bic
    CB BIC Version: cb2024.11.01

--- meta-facebook/at-cb/src/platform/plat_version.h
+++ meta-facebook/at-cb/src/platform/plat_version.h
@@ -33,7 +33,7 @@
 #define DEVICE_REVISION 0x80
 
 #define FIRMWARE_REVISION_1 GET_FW_VERSION1(BOARD_ID, PROJECT_STAGE)
-#define FIRMWARE_REVISION_2 0x27
+#define FIRMWARE_REVISION_2 0x28
 
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
@@ -42,8 +42,8 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x24
-#define BIC_FW_WEEK 0x09
-#define BIC_FW_VER 0x02
+#define BIC_FW_WEEK 0x11
+#define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x63 // char: c
 #define BIC_FW_platform_1 0x62 // char: b
 #define BIC_FW_platform_2 0x00 // char: '\0'
