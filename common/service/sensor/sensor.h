8c5dc4b7143e96fec0bd05acbac92cfe97bcb790
KevinHuang <142553919+khoung76642@users.noreply.github.com>
2024-03-18 10:06:09 +0800

    Update sensor.h modify annotation

--- common/service/sensor/sensor.h
+++ common/service/sensor/sensor.h
@@ -715,7 +715,7 @@ typedef struct _nct7363_init_arg {
 	bool is_init;
 	uint8_t value;
 	uint8_t pin_type
-		[16]; // According to the right pin position of the component, from top to bottom, they are 0 to 16 respectively.
+		[16]; // According to the pin position on the right side of the component, from top to bottom, there are 16 pins in total.
 	uint8_t fan_poles;
 	uint8_t duty;
 	uint16_t threshold;
