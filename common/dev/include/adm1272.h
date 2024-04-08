bcab88b2bc12fa1554d6caf77732da8372940ddf
Kevin Huang <KevinHuang4@quntatw.com>
2024-04-08 10:32:45 +0800

    Restore edit

--- common/dev/include/adm1272.h
+++ common/dev/include/adm1272.h
@@ -17,6 +17,16 @@
 #ifndef ADM1272_H
 #define ADM1272_H
 
+enum ADM1272_IRANGE {
+	IRANGE_0MV_TO_15MV = 0x0,
+	IRANGE_0MV_TO_30MV = 0x1,
+};
+
+enum ADM1272_VRANGE {
+	VRANGE_0V_TO_60V = 0x0,
+	VRANGE_0V_TO_100V = 0x1,
+};
+
 bool enable_adm1272_hsc(uint8_t bus,uint8_t addr, bool enable_flag);
 
 #endif
