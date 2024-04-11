5f2752cee9be88fa89c1ceb111b98da28c9c6b0e
MouchenHung <Mouchen.Hung@quantatw.com>
2024-04-11 16:40:06 +0800

    common-Support_mp289x_and_mpq8746_device

--- common/service/pldm/pldm_firmware_update.c
+++ common/service/pldm/pldm_firmware_update.c
@@ -31,6 +31,8 @@
 #include "pt5161l.h"
 #include "mp2985.h"
 #include "raa229621.h"
+#include "mpq8746.h"
+#include "mp289x.h"
 
 LOG_MODULE_DECLARE(pldm);
 
@@ -229,6 +231,16 @@ uint8_t pldm_vr_update(void *fw_update_param)
 			     ARRAY_SIZE(KEYWORD_VR_RAA229621) - 1))) {
 		if (raa229621_fwupdate(p->bus, p->addr, hex_buff, fw_update_cfg.image_size) ==
 		    false)
+			goto exit;
+	} else if (!strncmp(p->comp_version_str, KEYWORD_VR_MPQ8746,
+			    ARRAY_SIZE(KEYWORD_VR_MPQ8746) - 1)) {
+		if (mpq8746_fwupdate(p->bus, p->addr, hex_buff, fw_update_cfg.image_size) == false)
+			goto exit;
+	} else if ((!strncmp(p->comp_version_str, KEYWORD_VR_MP2898,
+			     ARRAY_SIZE(KEYWORD_VR_MP2898) - 1)) ||
+		   (!strncmp(p->comp_version_str, KEYWORD_VR_MP2894,
+			     ARRAY_SIZE(KEYWORD_VR_MP2894) - 1))) {
+		if (mp289x_fwupdate(p->bus, p->addr, hex_buff, fw_update_cfg.image_size) == false)
 			goto exit;
 	} else {
 		LOG_ERR("Non-support VR detected with component string %s!",
