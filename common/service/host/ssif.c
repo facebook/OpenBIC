51042a608a30344552f24d8ceca6690c471507c4
MouchenHung <Mouchen.Hung@quantatw.com>
2024-04-11 15:38:53 +0800

    common-Fix_cppcheck_error

--- common/service/host/ssif.c
+++ common/service/host/ssif.c
@@ -321,6 +321,7 @@ static bool ssif_data_handle(ssif_dev *ssif_inst, ssif_action_t action, uint8_t 
 		/* Message to BIC */
 		if (pal_request_msg_to_BIC_from_HOST(ssif_inst->current_ipmi_msg.buffer.netfn,
 						     ssif_inst->current_ipmi_msg.buffer.cmd)) {
+			bool skip_ipmi_handle = false;
 #ifdef ENABLE_SBMR
 			if (ssif_inst->current_ipmi_msg.buffer.netfn == NETFN_DCMI_REQ) {
 				if (smbr_cmd_handler(&ssif_inst->current_ipmi_msg.buffer) == true) {
@@ -329,9 +330,12 @@ static bool ssif_data_handle(ssif_dev *ssif_inst, ssif_action_t action, uint8_t 
 						LOG_ERR("Failed to write ssif response data");
 					}
 				}
-				goto exit;
+				skip_ipmi_handle = true;
 			}
 #endif
+			if (skip_ipmi_handle)
+				goto exit;
+
 			while (k_msgq_put(&ipmi_msgq, &ssif_inst->current_ipmi_msg, K_NO_WAIT) !=
 			       0) {
 				k_msgq_purge(&ipmi_msgq);
@@ -398,7 +402,7 @@ static bool ssif_data_handle(ssif_dev *ssif_inst, ssif_action_t action, uint8_t 
 				if (!pal_is_interface_use_ipmb(IPMB_inf_index_map[BMC_IPMB])) {
 					msg.InF_target = PLDM;
 					// Send request to MCTP/PLDM thread to ask BMC
-					int ret = pldm_send_ipmi_request(&msg);
+					ret = pldm_send_ipmi_request(&msg);
 					if (ret < 0) {
 						LOG_ERR("SSIF[%d] Failed to send SSIF msg to BMC via PLDM with ret: 0x%x",
 							ssif_inst->index, ret);
@@ -416,7 +420,7 @@ static bool ssif_data_handle(ssif_dev *ssif_inst, ssif_action_t action, uint8_t 
 #else
 				msg.InF_target = PLDM;
 				// Send request to MCTP/PLDM thread to ask BMC
-				int ret = pldm_send_ipmi_request(&msg);
+				ret = pldm_send_ipmi_request(&msg);
 				if (ret < 0) {
 					LOG_ERR("SSIF[%d] Failed to send SSIF msg to BMC via PLDM with ret: 0x%x",
 						ssif_inst->index, ret);
