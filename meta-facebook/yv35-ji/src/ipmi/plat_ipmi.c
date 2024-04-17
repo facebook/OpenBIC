3b85017dafdfb11529f63141c092fd6c67059337
MouchenHung <Mouchen.Hung@quantatw.com>
2024-04-17 11:17:41 +0800

    fby35-ji-Collect_post_code_from_ssif

--- meta-facebook/yv35-ji/src/ipmi/plat_ipmi.c
+++ meta-facebook/yv35-ji/src/ipmi/plat_ipmi.c
@@ -21,6 +21,7 @@
 #include <logging/log.h>
 #include "libutil.h"
 #include "ipmi.h"
+#include "plat_fru.h"
 
 LOG_MODULE_REGISTER(plat_ipmi);
 
@@ -32,10 +33,54 @@ bool pal_request_msg_to_BIC_from_HOST(uint8_t netfn, uint8_t cmd)
 		    (cmd == CMD_OEM_1S_GET_BIC_FW_INFO))
 			return true;
 	} else if (netfn == NETFN_APP_REQ) {
-		if (cmd == CMD_APP_GET_SYSTEM_GUID) {
+		if (cmd == CMD_APP_GET_SYSTEM_GUID)
 			return true;
-		}
+	} else if (netfn == NETFN_DCMI_REQ) {
+		if ((cmd == CMD_DCMI_SEND_BOOT_PROGRESS_CODE) ||
+		    (cmd == CMD_DCMI_GET_BOOT_PROGRESS_CODE))
+			return true;
 	}
 
 	return false;
+}
+
+int pal_record_bios_fw_version(uint8_t *buf, uint8_t size)
+{
+	CHECK_NULL_ARG_WITH_RETURN(buf, -1);
+
+	int ret = -1;
+	EEPROM_ENTRY set_bios_ver = { 0 };
+	EEPROM_ENTRY get_bios_ver = { 0 };
+
+	const uint8_t block_index = buf[3];
+	if (block_index >= BIOS_FW_VERSION_BLOCK_NUM) {
+		LOG_ERR("BIOS version block index is out of range");
+		return -1;
+	}
+
+	ret = get_bios_version(&get_bios_ver, block_index);
+	if (ret == -1) {
+		LOG_ERR("Get BIOS version failed");
+		return -1;
+	}
+
+	set_bios_ver.data_len = size - 3; // skip netfn, cmd and command code
+	memcpy(&set_bios_ver.data[0], &buf[3], set_bios_ver.data_len);
+
+	// Check the written BIOS version is the same with the stored
+	ret = memcmp(&get_bios_ver.data[0], &set_bios_ver.data[0],
+		     BIOS_FW_VERSION_BLOCK_MAX_SIZE * sizeof(uint8_t));
+	if (ret == 0) {
+		LOG_DBG("The Written BIOS version is the same as the stored BIOS version in EEPROM");
+	} else {
+		LOG_DBG("BIOS version set successfully");
+
+		ret = set_bios_version(&set_bios_ver, block_index);
+		if (ret == -1) {
+			LOG_ERR("Set BIOS version failed");
+			return -1;
+		}
+	}
+
+	return 0;
 }
