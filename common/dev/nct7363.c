d11aa80af182eb4f8964d87d1ccde0b3875bab53
Kevin Huang <KevinHuang4@quntatw.com>
2024-05-06 09:42:20 +0800

    Fix if fan_pole < 0

--- common/dev/nct7363.c
+++ common/dev/nct7363.c
@@ -329,6 +329,11 @@ uint8_t nct7363_init(sensor_cfg *cfg)
 
 	nct7363_init_arg *nct7363_init_arg_data = (nct7363_init_arg *)cfg->init_args;
 
+	if (nct7363_init_arg_data->fan_poles < 0) {
+		LOG_ERR("Invalid, fan_poles is setting");
+		return SENSOR_INIT_UNSPECIFIED_ERROR;
+	}
+	
 	if (nct7363_init_arg_data->is_init)
 		goto skip_init;
 
