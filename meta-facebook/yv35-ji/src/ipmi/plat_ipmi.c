092c5f1c4f17f4cf3ca6f6a52f81a643fabac572
MouchenHung <Mouchen.Hung@quantatw.com>
2024-05-10 17:44:15 +0800

    fby35-ji-Workaround_gpio_index

--- meta-facebook/yv35-ji/src/ipmi/plat_ipmi.c
+++ meta-facebook/yv35-ji/src/ipmi/plat_ipmi.c
@@ -21,9 +21,14 @@
 #include <logging/log.h>
 #include "libutil.h"
 #include "ipmi.h"
+#include "hal_gpio.h"
+#include "oem_1s_handler.h"
 #include "plat_fru.h"
 
 LOG_MODULE_REGISTER(plat_ipmi);
+
+#define WORK_AROUND_BIOS_DEBUG_PIN_IDX 62
+#define WORK_AROUND_BIOS_DEBUG_PIN_IDX_REAL 23
 
 bool pal_request_msg_to_BIC_from_HOST(uint8_t netfn, uint8_t cmd)
 {
@@ -80,6 +85,96 @@ int pal_record_bios_fw_version(uint8_t *buf, uint8_t size)
 			LOG_ERR("Set BIOS version failed");
 			return -1;
 		}
+	}
+
+	return 0;
+}
+
+/* work around - Modify gpio index */
+void OEM_1S_GET_GPIO(ipmi_msg *msg)
+{
+	CHECK_NULL_ARG(msg);
+
+	// only input enable status
+	if (msg->data_len != 0) {
+		msg->completion_code = CC_INVALID_LENGTH;
+		return;
+	}
+
+	uint8_t eight_bit_value = 0, gpio_value, gpio_cnt, data_len;
+	// Bump up the gpio_ind_to_num_table_cnt to multiple of 8.
+	gpio_cnt = gpio_ind_to_num_table_cnt + (8 - (gpio_ind_to_num_table_cnt % 8));
+	data_len = gpio_cnt / 8;
+	msg->data_len = data_len;
+
+	int tmp_gpio_idx = 0;
+	for (uint8_t i = 0; i < gpio_cnt; i++) {
+		if (i == WORK_AROUND_BIOS_DEBUG_PIN_IDX)
+			tmp_gpio_idx = WORK_AROUND_BIOS_DEBUG_PIN_IDX_REAL;
+		else if (i >= WORK_AROUND_BIOS_DEBUG_PIN_IDX_REAL &&
+			 i < WORK_AROUND_BIOS_DEBUG_PIN_IDX)
+			tmp_gpio_idx = i + 1;
+		else
+			tmp_gpio_idx = i;
+
+		gpio_value = (tmp_gpio_idx >= gpio_ind_to_num_table_cnt) ?
+				     0 :
+				     gpio_get(gpio_ind_to_num_table[tmp_gpio_idx]);
+
+		// clear temporary variable to avoid return wrong GPIO value
+		if (i % 8 == 0) {
+			eight_bit_value = 0;
+		}
+		eight_bit_value = eight_bit_value | (gpio_value << (i % 8));
+		msg->data[i / 8] = eight_bit_value;
+	}
+	msg->completion_code = CC_SUCCESS;
+
+	return;
+}
+
+/* work around - Modify gpio index */
+__weak uint8_t gpio_idx_exchange(ipmi_msg *msg)
+{
+	CHECK_NULL_ARG_WITH_RETURN(msg, 1);
+
+	if (msg->data_len < 2)
+		return 1;
+
+	int need_change = 1;
+	switch (msg->data[0]) {
+	case GET_GPIO_STATUS:
+	case GET_GPIO_DIRECTION_STATUS:
+		if (msg->data_len == 3) {
+			if (msg->data[2] == GLOBAL_GPIO_IDX_KEY) {
+				need_change = 0;
+			}
+			// Ignore last data byte if provided.
+			msg->data_len--;
+		}
+		break;
+	case SET_GPIO_OUTPUT_STATUS:
+	case SET_GPIO_DIRECTION_STATUS:
+		if (msg->data_len == 4) {
+			if (msg->data[3] == GLOBAL_GPIO_IDX_KEY) {
+				need_change = 0;
+			}
+			// Ignore last data byte if provided.
+			msg->data_len--;
+		}
+		break;
+	default:
+		break;
+	}
+
+	if (need_change) {
+		if (msg->data[1] == WORK_AROUND_BIOS_DEBUG_PIN_IDX)
+			msg->data[1] = WORK_AROUND_BIOS_DEBUG_PIN_IDX_REAL;
+		else if (msg->data[1] >= WORK_AROUND_BIOS_DEBUG_PIN_IDX_REAL &&
+			 msg->data[1] < WORK_AROUND_BIOS_DEBUG_PIN_IDX)
+			msg->data[1]++;
+
+		msg->data[1] = gpio_ind_to_num_table[msg->data[1]];
 	}
 
 	return 0;
