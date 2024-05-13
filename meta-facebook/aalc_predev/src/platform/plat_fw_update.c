0da21fea3f9c111a9536fe25ed4c2e427614f9ca
Hung-Yi <hung-yi.li@quantatw.com>
2024-05-13 15:49:50 +0800

    modify_update_action

--- meta-facebook/aalc_predev/src/platform/plat_fw_update.c
+++ meta-facebook/aalc_predev/src/platform/plat_fw_update.c
@@ -16,11 +16,12 @@ uint8_t modbus_get_fw_reversion(modbus_command_mapping *cmd)
 {
 	CHECK_NULL_ARG_WITH_RETURN(cmd, MODBUS_EXC_ILLEGAL_DATA_VAL);
 
-	uint16_t byte_val[4]  = {BIC_FW_YEAR_MSB_ASCII, BIC_FW_YEAR_LSB_ASCII, BIC_FW_WEEK_ASCII, BIC_FW_VER_ASCII}; 
+	uint16_t byte_val[4] = { BIC_FW_YEAR_MSB_ASCII, BIC_FW_YEAR_LSB_ASCII, BIC_FW_WEEK_ASCII,
+				 BIC_FW_VER_ASCII };
 	memcpy(cmd->data, &byte_val[0], sizeof(uint16_t) * cmd->cmd_size);
-	
+
 	regs_reverse(cmd->data_len, cmd->data);
-	
+
 	return MODBUS_EXC_NONE;
 }
 
@@ -30,7 +31,7 @@ uint8_t modbus_fw_download(modbus_command_mapping *cmd)
 
 	uint32_t offset = cmd->data[0] << 16 | cmd->data[1]; // offset
 	uint16_t msg_len = cmd->data[2] & 0x7FFF; // length
-	uint8_t flag = (cmd->data[2] & BIT(15)) ? SECTOR_END_FLAG : 0;
+	uint8_t flag = (cmd->data[2] & BIT(15)) ? (SECTOR_END_FLAG | NO_RESET_FLAG) : 0;
 
 	if (cmd->data_len <= UPADTE_FW_DATA_LENGTH_MIN)
 		return MODBUS_EXC_ILLEGAL_DATA_VAL;
@@ -38,7 +39,8 @@ uint8_t modbus_fw_download(modbus_command_mapping *cmd)
 	if (msg_len != ((cmd->data_len - UPADTE_FW_DATA_LENGTH_MIN) * 2))
 		return MODBUS_EXC_ILLEGAL_DATA_VAL;
 
-    regs_reverse(cmd->data_len, cmd->data);
+	regs_reverse(cmd->data_len, cmd->data);
 
-	return fw_update(offset, msg_len, (uint8_t *) &cmd->data[UPADTE_FW_DATA_LENGTH_MIN], flag, DEVSPI_FMC_CS0);
+	return fw_update(offset, msg_len, (uint8_t *)&cmd->data[UPADTE_FW_DATA_LENGTH_MIN], flag,
+			 DEVSPI_FMC_CS0);
 }
