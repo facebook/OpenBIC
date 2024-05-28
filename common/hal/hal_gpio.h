654c2cad26b1f00e4328318e37a356965a58e4e2
victor <Victor.Jhong@quantatw.com>
2024-05-28 13:54:50 +0800

    common: modify the hal_gpio to support npcm400f
    Summary:
    - modify the hal_gpio to support npcm400f
    
    Test Plan:
    - Build code: PASS
    - npcm400f gpio test: PASS

--- common/hal/hal_gpio.h
+++ common/hal/hal_gpio.h
@@ -20,52 +20,75 @@
 #include <drivers/gpio.h>
 #include <devicetree.h>
 
-#if DT_NODE_EXISTS(DT_NODELABEL(gpio0_a_d))
-#define DEV_GPIO_A_D
-#endif
+#define GEN_ENUM(ENUM) ENUM,
+#define GEN_STR(STR) #STR,
 
-#if DT_NODE_EXISTS(DT_NODELABEL(gpio0_e_h))
-#define DEV_GPIO_E_H
-#endif
-
-#if DT_NODE_EXISTS(DT_NODELABEL(gpio0_i_l))
-#define DEV_GPIO_I_L
-#endif
-
-#if DT_NODE_EXISTS(DT_NODELABEL(gpio0_m_p))
-#define DEV_GPIO_M_P
-#endif
-
-#if DT_NODE_EXISTS(DT_NODELABEL(gpio0_q_t))
-#define DEV_GPIO_Q_T
-#endif
-
-#if DT_NODE_EXISTS(DT_NODELABEL(gpio0_u_v))
-#define DEV_GPIO_U_V
-#endif
+#if defined(CONFIG_GPIO_ASPEED)
+#define FOREACH_GPIO(APPLY)                                                                        \
+	APPLY(GPIO0_A_D)                                                                            \
+	APPLY(GPIO0_E_H)                                                                            \
+	APPLY(GPIO0_I_L)                                                                            \
+	APPLY(GPIO0_M_P)                                                                            \
+	APPLY(GPIO0_Q_T)                                                                            \
+	APPLY(GPIO0_U_V)
 
 #define TOTAL_GPIO_NUM 168
+#define GPIO_GROUP_NUM 6
+#define GPIO_GROUP_SIZE 32
+#define REG_GPIO_BASE 0x7e780000
+#elif (CONFIG_GPIO_NPCM4XX)
+#define FOREACH_GPIO(APPLY)                                                                        \
+	APPLY(GPIO_0)                                                                              \
+	APPLY(GPIO_1)                                                                              \
+	APPLY(GPIO_2)                                                                              \
+	APPLY(GPIO_3)                                                                              \
+	APPLY(GPIO_4)                                                                              \
+	APPLY(GPIO_5)                                                                              \
+	APPLY(GPIO_6)                                                                              \
+	APPLY(GPIO_7)                                                                              \
+	APPLY(GPIO_8)                                                                              \
+	APPLY(GPIO_9)                                                                              \
+	APPLY(GPIO_A)                                                                              \
+	APPLY(GPIO_B)                                                                              \
+	APPLY(GPIO_C)                                                                              \
+	APPLY(GPIO_D)                                                                              \
+	APPLY(GPIO_E)                                                                              \
+	APPLY(GPIO_F)
+
+#define TOTAL_GPIO_NUM 124
+#define GPIO_GROUP_NUM 16
+#define GPIO_GROUP_SIZE 8
+#define REG_GPIO_BASE 0x40081000
+#else
+#error "Unsupported GPIO driver"
+#endif /* CONFIG_GPIO_ASPEED */
+
+enum GPIO_GROUP { FOREACH_GPIO(GEN_ENUM) };
+
 #define ENABLE 1
 #define DISABLE 0
 #define CHIP_GPIO 0
 #define CHIP_SGPIO 1
 #define GPIO_LOW 0
 #define GPIO_HIGH 1
-#define GPIO_GROUP_NUM 6
-#define GPIO_GROUP_SIZE 32
 #define GPIO_STACK_SIZE 3072
 
 #define OPEN_DRAIN 0
 #define PUSH_PULL 1
 
+#if defined(CONFIG_GPIO_ASPEED)
 #define REG_ADC_BASE 0x7e6e9000
-#define REG_GPIO_BASE 0x7e780000
 #define REG_SCU 0x7E6E2000
 #define REG_DIRECTION_OFFSET 4
 #define REG_INTERRUPT_ENABLE_OFFSET 8
 #define REG_INTERRUPT_TYPE0_OFFSET 0x0C
 #define REG_INTERRUPT_TYPE1_OFFSET 0x10
 #define REG_INTERRUPT_TYPE2_OFFSET 0x14
+#elif (CONFIG_GPIO_NPCM4XX)
+#define REG_DEVALTX 0x400C3000
+#define REG_DIRECTION_OFFSET 2
+#else /* defined(CONFIG_GPIO_ASPEED) */
+#endif /* defined(CONFIG_GPIO_ASPEED) */
 
 extern uint32_t GPIO_GROUP_REG_ACCESS[];
 extern uint32_t GPIO_MULTI_FUNC_PIN_CTL_REG_ACCESS[];
@@ -90,15 +113,6 @@ typedef struct _SET_GPIO_VALUE_CFG_ {
 } SET_GPIO_VALUE_CFG;
 
 extern GPIO_CFG gpio_cfg[];
-
-enum GPIO_GROUP {
-	GPIO_A_D,
-	GPIO_E_H,
-	GPIO_I_L,
-	GPIO_M_P,
-	GPIO_Q_T,
-	GPIO_U_V,
-};
 
 enum POWER_STATUS {
 	POWER_ON = GPIO_HIGH,
@@ -135,7 +149,7 @@ bool pal_load_gpio_config(void);
 int gpio_init(const struct device *args);
 int gpio_interrupt_conf(uint8_t, gpio_flags_t);
 uint8_t gpio_get_reg_value(uint8_t gpio_num, uint8_t reg_offset);
-uint8_t gpio_conf(uint8_t gpio_num, int dir);
+int gpio_conf(uint8_t gpio_num, int dir);
 int gpio_get_direction(uint8_t gpio_num);
 void scu_init(SCU_CFG cfg[], size_t size);
 
