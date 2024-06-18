7aa5ebc3e57e658a4879bbe2cf5a33f07a8a392b
Kevin Huang <KevinHuang4@quntatw.com>
2024-06-18 13:43:48 +0800

    aalc:Fix sensor polling issue
    Summary:
    
    	-modify aalc-rpu plat_sensortable.c to fix polling problem
    
    	-modify aalc-rpu plat_hook.c
    		-Change mux address from 8-bit to 7-bit,
    		-Add init_args and fix post_read function
    
    	-modify common driver nct7363.c and nct7363.h
    		-Add setting gpio input initial value
    		-Fix gpio reading gpio input and output address
    
    Description:
    
    	-Fix some problems when doing sensor polling which test was based on real sensors.
    
    Test Plan:
    
    	-Build code: PASS

--- meta-facebook/aalc-rpu/src/platform/plat_util.h
+++ meta-facebook/aalc-rpu/src/platform/plat_util.h
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 bool modbus_i2c_master_write_read(const uint16_t *modbus_data, uint8_t data_len);
 void modbus_i2c_master_write_read_response(uint16_t *modbus_data);
 void regs_reverse(uint16_t reg_len, uint16_t *data);
