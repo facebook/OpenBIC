5f6e32fa102b7b02b448ec138ec26e3586d2fc99
MouchenHung <Mouchen.Hung@quantatw.com>
2024-06-26 10:16:03 +0800

    fby35-ji-Improve_post_complete_status_judgement

--- meta-facebook/yv35-ji/src/platform/plat_isr.h
+++ meta-facebook/yv35-ji/src/platform/plat_isr.h
@@ -24,7 +24,7 @@ void ISR_RTC_ALERT();
 void ISR_GPIOA5();
 void ISR_GPIOA6();
 void ISR_GPIOB0();
-void ISR_GPIOB7();
+void ISR_POST_COMPLETE();
 void ISR_HSC_OC();
 void ISR_GPIOC1();
 void ISR_GPIOC3();
