78d405e33a1c8ba4157dcfd47141f61557a6ada9
MouchenHung <Mouchen.Hung@quantatw.com>
2024-06-27 16:40:50 +0800

    fby35-ji-Modify_rt8848c_init_config_data

--- meta-facebook/yv35-ji/src/platform/plat_power_status.c
+++ meta-facebook/yv35-ji/src/platform/plat_power_status.c
@@ -36,6 +36,35 @@ void reset_post_end_work_status()
 	is_post_end_work_done = false;
 }
 
+struct rt8848c_cfg_tune {
+	uint8_t reg;
+	uint8_t val;
+};
+
+struct rt8848c_cfg_tune rt8848c_cfg_tune[] = {
+	{ 0x00, 0x00 }, // unlock register 0x01~0x5F
+	{ 0x54, 0x6B }, // CCM Ramp Hold Voltage for Jitter improvement (original:0x67)
+	{ 0x4F, 0x19 }, // Internal Ramp Magnitude for jitter improvement , 700mV (original:0x13)
+	{ 0x4D, 0x03 }, // [1:0]=11 , enable per-phase OCP function and digital flag detection
+	{ 0x11, 0x1F }, // Current balance gain 32/16 for phase1
+	{ 0x13, 0x00 }, // Current balance gain 1/16 for phase2
+	{ 0x15, 0x1F }, // Current balance gain 32/16 for phase3
+	{ 0x17, 0x00 }, // Current balance gain 1/16 for phase4
+	{ 0x19, 0x1F }, // Current balance gain 32/16 for phase5
+	{ 0x1B, 0x00 }, // Current balance gain 1/16 for phase6
+	{ 0x1D, 0x1F }, // Current balance gain 32/16 for phase7
+	{ 0x1F, 0x00 }, // Current balance gain 1/16 for phase8
+	{ 0x12, 0x12 }, // Current balance offset -45mV for phase1
+	{ 0x14, 0x00 }, // Current balance offset 15mV for phase2
+	{ 0x16, 0x01 }, // Current balance offset 30mV for phase3
+	{ 0x18, 0x00 }, // Current balance offset 15mV for phase4
+	{ 0x1A, 0x12 }, // Current balance offset -45mV for phase5
+	{ 0x1C, 0x01 }, // Current balance offset 30mV for phase6
+	{ 0x1E, 0x00 }, // Current balance offset 15mV for phase7
+	{ 0x20, 0x01 }, // Current balance offset 30mV for phase8
+	{ 0x00, 0xA6 }, // lock register 0x01~0x5F
+};
+
 static void set_rt8848c_config()
 {
 	/* switch VR bus to BIC */
@@ -46,49 +75,15 @@ static void set_rt8848c_config()
 	i2c_msg.bus = CPUVDD_I2C_BUS;
 	i2c_msg.target_addr = (RT8848C_I2C_ADDR >> 1);
 
-	/* unlock register 0x01~0x5F */
-	i2c_msg.tx_len = 2;
-	i2c_msg.data[0] = 0x00;
-	i2c_msg.data[1] = 0x00;
-	if (i2c_master_write(&i2c_msg, retry)) {
-		LOG_ERR("Failed to unlock RT8848C register 0x01~0x5F");
-		return;
-	}
-
-	/* CCM Ramp Hold Voltage for Jitter improvement (original:0x67) */
-	i2c_msg.tx_len = 2;
-	i2c_msg.data[0] = 0x54;
-	i2c_msg.data[1] = 0x6B;
-	if (i2c_master_write(&i2c_msg, retry)) {
-		LOG_ERR("Failed to do jitter improvement on RT8848C register 0x%x", 0x54);
-		return;
-	}
-
-	/* Internal Ramp Magnitude for jitter improvement , 700mV (original:0x13) */
-	i2c_msg.tx_len = 2;
-	i2c_msg.data[0] = 0x4F;
-	i2c_msg.data[1] = 0x19;
-	if (i2c_master_write(&i2c_msg, retry)) {
-		LOG_ERR("Failed to do jitter improvement on RT8848C register 0x%x", 0x4F);
-		return;
-	}
-
-	/* [1:0]=11 , enable per-phase OCP function and digital flag detection */
-	i2c_msg.tx_len = 2;
-	i2c_msg.data[0] = 0x4D;
-	i2c_msg.data[1] = 0x03;
-	if (i2c_master_write(&i2c_msg, retry)) {
-		LOG_ERR("Failed to enable per-phase OCP function and digital flag detection on RT8848C");
-		return;
-	}
-
-	/* lock register 0x01~0x5F */
-	i2c_msg.tx_len = 2;
-	i2c_msg.data[0] = 0x00;
-	i2c_msg.data[1] = 0xA6;
-	if (i2c_master_write(&i2c_msg, retry)) {
-		LOG_ERR("Failed to unlock RT8848C register 0x01~0x5F");
-		return;
+	for (int i = 0; i < ARRAY_SIZE(rt8848c_cfg_tune); i++) {
+		i2c_msg.tx_len = 2;
+		i2c_msg.data[0] = rt8848c_cfg_tune[i].reg;
+		i2c_msg.data[1] = rt8848c_cfg_tune[i].val;
+		if (i2c_master_write(&i2c_msg, retry)) {
+			LOG_ERR("Failed to config RT8848C while writing register 0x%02X to 0x%02X",
+				rt8848c_cfg_tune[i].reg, rt8848c_cfg_tune[i].val);
+			break;
+		}
 	}
 
 	/* switch VR bus to CPU */
