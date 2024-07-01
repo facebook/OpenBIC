610cf2e32934fae0ffee29c8b6ada19d1ce59727
Jeff_Shih <Jeff_Shih@wiwynn.com>
2024-07-01 10:07:34 -0700

    YV4: SD: Support for ge tting Http boot certification through PLDM OEM … (#1846)
    Summary:
    … command
    
    # Description:
    	OEM_GET_HTTP_BOOT_DATA(netfn: 0x30 cmd: 0x57): get Http Boot data
    	format: 0x30 0x57 ${low_byte_offset} ${high_byte_offset} ${read_length}
    
    	OEM_GET_HTTP_BOOT_ATTR(netfn: 0x30 cmd: 0x58): get Http Boot CRC/length
    	format: 0x30 0x58 ${attribute}
    	${attribute}: 0 -> get Http Boot size, 1 -> get Http Boot CRC32
    
    # Motivation:
    	Support BIC reading boot certification through PLDM read file IO command
    
    # Test Log:
    	Get Size:
    	uart:~$ platform ipmi raw 0x30 0x58 0
    	BIC self command netfn:0x30 cmd:0x58 response with cc 0x0
    	00000000: 4c 08                                            |L.               |
    
    	Get CRC:
    	uart:~$ platform ipmi raw 0x30 0x58 1
    	BIC self command netfn:0x30 cmd:0x58 response with cc 0x0
    	00000000: 37 7d de bf                                      |7}..             |
    
    	Get Data:
    	uart:~$ platform ipmi raw 0x30 0x57 0x05 0x00 0x30
    	BIC self command netfn:0x30 cmd:0x57 response with cc 0x0
    	00000000: 30 42 45 47 49 4e 20 43  45 52 54 49 46 49 43 41 |0BEGIN C ERTIFICA|
    	00000010: 54 45 2d 2d 2d 2d 2d 4d  49 49 47 44 6a 43 43 41 |TE-----M IIGDjCCA|
    	00000020: 2f 61 67 41 77 49 42 41  67 49 55 4e 78 59 61 63 |/agAwIBA gIUNxYac|
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1846
    
    Reviewed By: amithash
    
    Differential Revision: D59050807
    
    fbshipit-source-id: 8eb9e36b75a1d3bfe2b827454e4187b40aae8755

--- meta-facebook/yv4-sd/src/platform/plat_pldm.c
+++ meta-facebook/yv4-sd/src/platform/plat_pldm.c
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "plat_mctp.h"
+#include "plat_pldm.h"
+#include "hal_i2c.h"
+#include "util_sys.h"
 
 LOG_MODULE_REGISTER(plat_pldm);
 
@@ -12,4 +15,77 @@ uint8_t plat_pldm_get_tid()
 {
 	// Set TID as EID
 	return plat_get_eid();
+}
+
+uint8_t plat_pldm_get_http_boot_data(uint8_t *httpBootData, uint16_t *httpBootDataLen)
+{
+	pldm_msg pmsg = { 0 };
+
+	uint8_t bmc_bus = I2C_BUS_BMC;
+	uint8_t bmc_interface = pal_get_bmc_interface();
+	pmsg.ext_params.ep = MCTP_EID_BMC;
+
+	switch (bmc_interface) {
+	case BMC_INTERFACE_I3C:
+		bmc_bus = I3C_BUS_BMC;
+		pmsg.ext_params.type = MCTP_MEDIUM_TYPE_TARGET_I3C;
+		pmsg.ext_params.i3c_ext_params.addr = I3C_STATIC_ADDR_BMC;
+		break;
+	case BMC_INTERFACE_I2C:
+		bmc_bus = I2C_BUS_BMC;
+		pmsg.ext_params.type = MCTP_MEDIUM_TYPE_SMBUS;
+		pmsg.ext_params.smbus_ext_params.addr = I2C_ADDR_BMC;
+		break;
+	default:
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	pmsg.hdr.rq = PLDM_REQUEST;
+	pmsg.hdr.pldm_type = PLDM_TYPE_OEM;
+	pmsg.hdr.cmd = PLDM_OEM_READ_FILE_IO;
+
+	struct pldm_oem_read_file_io_req ptr = { 0 };
+	ptr.cmd_code = HTTP_BOOT;
+	ptr.data_length = BMC_PLDM_DATA_MAXIMUM - sizeof(struct pldm_oem_read_file_io_req);
+	ptr.transfer_flag = PLDM_START;
+	ptr.highOffset = 0;
+	ptr.lowOffset = 0;
+
+	pmsg.buf = (uint8_t *)&ptr;
+	pmsg.len = sizeof(struct pldm_oem_read_file_io_req);
+
+	uint16_t resp_len = BMC_PLDM_DATA_MAXIMUM;
+
+	struct pldm_oem_read_file_io_resp *rbuf =
+		(struct pldm_oem_read_file_io_resp *)malloc(sizeof(uint8_t) * resp_len);
+	if (rbuf == NULL) {
+		LOG_ERR("Failed to allocate response buffer");
+		return PLDM_ERROR;
+	}
+
+	uint16_t offset = 0;
+	while (true) {
+		if (!mctp_pldm_read(find_mctp_by_bus(bmc_bus), &pmsg, (uint8_t *)rbuf, resp_len)) {
+			LOG_ERR("Fail to send OEM HTTP BOOT DATA");
+			free(rbuf);
+			return PLDM_ERROR_INVALID_DATA;
+		}
+
+		memcpy(httpBootData + offset, rbuf->messages, rbuf->data_length);
+
+		ptr.transfer_flag = rbuf->transfer_flag;
+		ptr.highOffset = rbuf->highOffset;
+		ptr.lowOffset = rbuf->lowOffset;
+		pmsg.buf = (uint8_t *)&ptr;
+
+		if (rbuf->transfer_flag == PLDM_END || rbuf->transfer_flag == PLDM_START_AND_END) {
+			*httpBootDataLen = offset + rbuf->data_length;
+			break;
+		}
+
+		offset = ((uint16_t)ptr.highOffset << 8) | ptr.lowOffset;
+	}
+
+	free(rbuf);
+	return PLDM_SUCCESS;
 }
