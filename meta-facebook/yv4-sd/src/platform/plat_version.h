5549f421e14bc038cf8f030ce2d0ff44c1d46512
Sara Lin <sara_sy_lin@wiwynn.com>
2024-07-08 14:16:04 +0800

    fby4: sd/wf: Version commit for 2024.28.01
    Description
    - Version commit for Sentinel Dome BIC oby4-sd-2024.28.01.
    - Version commit for Wailua Falls BIC oby4-wf-2024.28.01.
    
    Motivation
    - Version commit for Sentinel Dome BIC oby4-sd-2024.28.01.
    - Version commit for Wailua Falls BIC oby4-wf-2024.28.01.

--- meta-facebook/yv4-sd/src/platform/plat_version.h
+++ meta-facebook/yv4-sd/src/platform/plat_version.h
@@ -32,7 +32,7 @@
 #define DEVICE_REVISION 0x80
 
 #define FIRMWARE_REVISION_1 GET_FW_VERSION1(BOARD_ID, PROJECT_STAGE)
-#define FIRMWARE_REVISION_2 0xb
+#define FIRMWARE_REVISION_2 0xc
 
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
@@ -41,7 +41,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x24
-#define BIC_FW_WEEK 0x26
+#define BIC_FW_WEEK 0x28
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x73 // char: s
 #define BIC_FW_platform_1 0x64 // char: d
