28b545e6404e85e7c7affc34d8b5b8e3a9f0da2f
MouchenHung <Mouchen.Hung@quantatw.com>
2024-07-09 10:43:16 +0800

    fby35-ji-Support_virtual_e1s_prsnt_pin

--- meta-facebook/yv35-ji/src/ipmi/plat_ipmi.c
+++ meta-facebook/yv35-ji/src/ipmi/plat_ipmi.c
@@ -31,6 +31,9 @@ LOG_MODULE_REGISTER(plat_ipmi);
 #define WORK_AROUND_BIOS_DEBUG_PIN_IDX 62
 #define WORK_AROUND_BIOS_DEBUG_PIN_IDX_REAL 23
 
+#define WORK_AROUND_V_E1S_PRSNT_PIN_IDX 63
+#define WORK_AROUND_V_E1S_PRSNT_PIN_IDX_REAL 55
+
 bool pal_request_msg_to_BIC_from_HOST(uint8_t netfn, uint8_t cmd)
 {
 	if (netfn == NETFN_OEM_1S_REQ) {
@@ -167,11 +170,16 @@ void OEM_1S_GET_GPIO(ipmi_msg *msg)
 
 	int tmp_gpio_idx = 0;
 	for (uint8_t i = 0; i < gpio_cnt; i++) {
-		if (i == WORK_AROUND_BIOS_DEBUG_PIN_IDX)
+		if (i == WORK_AROUND_V_E1S_PRSNT_PIN_IDX)
+			tmp_gpio_idx = WORK_AROUND_V_E1S_PRSNT_PIN_IDX_REAL;
+		else if (i == WORK_AROUND_BIOS_DEBUG_PIN_IDX)
 			tmp_gpio_idx = WORK_AROUND_BIOS_DEBUG_PIN_IDX_REAL;
 		else if (i >= WORK_AROUND_BIOS_DEBUG_PIN_IDX_REAL &&
-			 i < WORK_AROUND_BIOS_DEBUG_PIN_IDX)
+			 i < WORK_AROUND_V_E1S_PRSNT_PIN_IDX_REAL)
 			tmp_gpio_idx = i + 1;
+		else if (i >= WORK_AROUND_V_E1S_PRSNT_PIN_IDX_REAL &&
+			 i < WORK_AROUND_BIOS_DEBUG_PIN_IDX)
+			tmp_gpio_idx = i + 2;
 		else
 			tmp_gpio_idx = i;
 
@@ -192,7 +200,7 @@ void OEM_1S_GET_GPIO(ipmi_msg *msg)
 }
 
 /* work around - Modify gpio index */
-__weak uint8_t gpio_idx_exchange(ipmi_msg *msg)
+uint8_t gpio_idx_exchange(ipmi_msg *msg)
 {
 	CHECK_NULL_ARG_WITH_RETURN(msg, 1);
 
@@ -226,11 +234,16 @@ __weak uint8_t gpio_idx_exchange(ipmi_msg *msg)
 	}
 
 	if (need_change) {
-		if (msg->data[1] == WORK_AROUND_BIOS_DEBUG_PIN_IDX)
+		if (msg->data[1] == WORK_AROUND_V_E1S_PRSNT_PIN_IDX)
+			msg->data[1] = WORK_AROUND_V_E1S_PRSNT_PIN_IDX_REAL;
+		else if (msg->data[1] == WORK_AROUND_BIOS_DEBUG_PIN_IDX)
 			msg->data[1] = WORK_AROUND_BIOS_DEBUG_PIN_IDX_REAL;
 		else if (msg->data[1] >= WORK_AROUND_BIOS_DEBUG_PIN_IDX_REAL &&
-			 msg->data[1] < WORK_AROUND_BIOS_DEBUG_PIN_IDX)
+			 msg->data[1] < WORK_AROUND_V_E1S_PRSNT_PIN_IDX_REAL)
 			msg->data[1]++;
+		else if (msg->data[1] >= WORK_AROUND_V_E1S_PRSNT_PIN_IDX_REAL &&
+			 msg->data[1] < WORK_AROUND_BIOS_DEBUG_PIN_IDX)
+			msg->data[1] += 2;
 
 		msg->data[1] = gpio_ind_to_num_table[msg->data[1]];
 	}
