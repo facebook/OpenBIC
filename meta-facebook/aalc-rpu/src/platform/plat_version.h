e0501964d098523b5d34079ad6932808a1f3fd31
Kevin Huang <KevinHuang4@quntatw.com>
2024-07-18 20:28:25 +0800

    version 0xa6

--- meta-facebook/aalc-rpu/src/platform/plat_version.h
+++ meta-facebook/aalc-rpu/src/platform/plat_version.h
@@ -43,7 +43,7 @@
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x24
 #define BIC_FW_WEEK 0x27
-#define BIC_FW_VER 0x01
+#define BIC_FW_VER 0xa6
 #define BIC_FW_platform_0 0x61 // char: a
 #define BIC_FW_platform_1 0x61 // char: a
 #define BIC_FW_platform_2 0x00 // char: '\0'
