cac1e758a7510fb54d5aa2c12715f8c517af865a
victor <Victor.Jhong@quantatw.com>
2024-09-03 18:28:06 -0700

    GT: Version commit for obgt-cc-2024.36.01 (#1952)
    Summary:
    Version commit for Grand Teton switch board BIC obgt-cc-2024.36.01.
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/1952
    
    Test Plan:
    Build code: Pass
    Get BIC version: Pass
    
    Log:
    ```
    root@bmc-oob:~# fw-util swb --version bic
    SWB BIC Version: 2024.36.01
    ```
    
    Reviewed By: williamspatrick
    
    Differential Revision: D62090400
    
    fbshipit-source-id: 23d870f32b3b78d34d48f53360e6f16e0637c88b

--- meta-facebook/gt-cc/src/platform/plat_version.h
+++ meta-facebook/gt-cc/src/platform/plat_version.h
@@ -40,7 +40,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x24
-#define BIC_FW_WEEK 0x29
+#define BIC_FW_WEEK 0x36
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x47 // char: G
 #define BIC_FW_platform_1 0x54 // char: T
