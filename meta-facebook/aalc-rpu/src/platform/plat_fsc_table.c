87fbbcb14878f2f55c886595234202794fbd9e92
Hung-Yi <hung-yi.li@quantatw.com>
2024-10-07 21:47:00 -0700

    aalc: add pump redundant (#2009)
    Summary:
    - modify fsc related function
    - add pump redundant function
    - add high press error log
    - add low flow rate error log
    - add emergency button error log
    - add rpu fan lcr to fault led behavior
    - modify hex_fan/pump pwm tach modbus command
    - add platform sensor SB_HEX_AIR_INLET_AVG_TEMP_C
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/2009
    
    Test Plan: - Build code: PASS
    
    Reviewed By: wangx6f
    
    Differential Revision: D63892403
    
    fbshipit-source-id: 274636648c6f33f5b98ba073d0d33ad958d76b45

--- meta-facebook/aalc-rpu/src/platform/plat_fsc_table.c
+++ meta-facebook/aalc-rpu/src/platform/plat_fsc_table.c
@@ -4,39 +4,195 @@
 #include "plat_pwm.h"
 #include "plat_hwmon.h"
 
-stepwise_cfg stepwise_table[] = {};
+#define FAN_TABLE_TBD 0
 
-int stepwise_table_size = ARRAY_SIZE(stepwise_table);
-
-pid_cfg pid_table[] = {};
-
-int pid_table_size = ARRAY_SIZE(pid_table);
-
-zone_cfg zone_table[] = {
-	{ .table = (fsc_type_mapping[]){ { SENSOR_NUM_MB_RPU_AIR_INLET_TEMP_C, FSC_TYPE_DEFAULT } },
-	  .table_size = 1,
-	  .FF_gain = 1,
-	  .interval = 1,
-	  .set_duty = pwm_control,
-	  .set_duty_arg = PWM_GROUP_E_HEX_FAN },
+pid_cfg hex_fan_pid_table[] = {
+#if FAN_TABLE_TBD
 	{
-		.table = (fsc_type_mapping[]){ { SENSOR_NUM_MB_RPU_AIR_INLET_TEMP_C,
-						 FSC_TYPE_DEFAULT } },
-		.table_size = 1,
-		.FF_gain = 1,
-		.interval = 2,
-		.set_duty = pwm_control,
-		.set_duty_arg = PWM_GROUP_E_PUMP,
+		.sensor_num = SENSOR_NUM_BPB_RPU_COOLANT_OUTLET_TEMP_C,
+		.setpoint_type = SETPOINT_AIR_INTEL_AVG_C,
+		.kp = -7,
+		.ki = -0.1,
+		.kd = -3,
+		.i_limit_min = 0,
+		.i_limit_max = 0,
+		.pos_hyst = 1,
+		.neg_hyst = 1,
+	},
+#endif
+};
+
+stepwise_cfg hex_fan_stepwise_table[] = {
+#if FAN_TABLE_TBD
+	{
+		.sensor_num = SENSOR_NUM_SB_HEX_AIR_INLET_AVG_TEMP_C,
+		.step = {
+			{25, 20},
+			{26, 20},
+			{27, 20},
+			{28, 20},
+			{29, 20},
+			{30, 20},
+			{31, 21},
+			{32, 22},
+			{33, 23},
+			{34, 24},
+			{35, 25},
+			{36, 26},
+			{37, 27},
+			{38, 28},
+			{39, 29},
+			{40, 30},
+		},
 	},
 	{
-		.table = (fsc_type_mapping[]){ { SENSOR_NUM_MB_RPU_AIR_INLET_TEMP_C,
-						 FSC_TYPE_DEFAULT } },
-		.table_size = 1,
-		.FF_gain = 1,
+		.sensor_num = SENSOR_NUM_BPB_RPU_COOLANT_OUTLET_TEMP_C,
+		.step = {
+			{25, 15},
+			{26, 16},
+			{27, 17},
+			{28, 18},
+			{29, 19},
+			{30, 20},
+			{31, 21},
+			{32, 22},
+			{33, 23},
+			{34, 24},
+			{35, 25},
+			{36, 26},
+			{37, 27},
+			{38, 28},
+			{39, 29},
+			{40, 30},
+		},
+	},
+#endif
+};
+
+pid_cfg pump_pid_table[] = {
+#if FAN_TABLE_TBD
+	{
+		.sensor_num = SENSOR_NUM_BPB_RPU_COOLANT_OUTLET_TEMP_C,
+		.setpoint_type = SETPOINT_FLOW_RATE_LPM,
+		.kp = -7,
+		.ki = -0.1,
+		.kd = -3,
+		.i_limit_min = 0,
+		.i_limit_max = 0,
+		.pos_hyst = 1,
+		.neg_hyst = 1,
+	},
+#endif
+};
+
+stepwise_cfg pump_stepwise_table[] = {
+#if FAN_TABLE_TBD
+	{
+		.sensor_num = SENSOR_NUM_SB_HEX_AIR_INLET_AVG_TEMP_C,
+		.step = {
+			{25, 30},
+			{26, 32},
+			{27, 34},
+			{28, 36},
+			{29, 38},
+			{30, 40},
+			{31, 42},
+			{32, 44},
+			{33, 46},
+			{34, 48},
+			{35, 50},
+			{36, 52},
+			{37, 54},
+			{38, 56},
+			{39, 58},
+			{40, 60},
+		},
+	},
+	{
+		.sensor_num = SENSOR_NUM_BPB_RPU_COOLANT_OUTLET_TEMP_C,
+		.step = {
+			{30, 20},
+			{31, 21},
+			{32, 22},
+			{33, 23},
+			{34, 24},
+			{35, 25},
+			{36, 26},
+			{37, 27},
+			{38, 28},
+			{39, 29},
+			{40, 30},
+			{41, 31},
+			{42, 32},
+			{43, 33},
+			{44, 34},
+			{45, 35},
+		},
+	},
+#endif
+};
+
+stepwise_cfg rpu_fan_stepwise_table[] = {
+#if FAN_TABLE_TBD
+	{
+		.sensor_num = SENSOR_NUM_SB_HEX_AIR_INLET_AVG_TEMP_C,
+		.step = {
+			{25, 25},
+			{26, 26},
+			{27, 27},
+			{28, 28},
+			{29, 29},
+			{30, 30},
+			{31, 31},
+			{32, 32},
+			{33, 33},
+			{34, 34},
+			{35, 35},
+			{36, 36},
+			{37, 37},
+			{38, 38},
+			{39, 39},
+			{40, 40},
+		},
+	},
+#endif
+};
+
+zone_cfg zone_table[] = {
+	{
+		// zone 1 - hex fan
+		.sw_tbl = hex_fan_stepwise_table,
+		.sw_tbl_num = ARRAY_SIZE(hex_fan_stepwise_table),
+		.pid_tbl = hex_fan_pid_table,
+		.pid_tbl_num = ARRAY_SIZE(hex_fan_pid_table),
+		.interval = 1,
+		.set_duty = pwm_control,
+		.set_duty_arg = PWM_GROUP_E_HEX_FAN,
+		.out_limit_min = 20,
+		.out_limit_max = 100,
+	},
+	{
+		// zone 2 - pump
+		.sw_tbl = pump_stepwise_table,
+		.sw_tbl_num = ARRAY_SIZE(pump_stepwise_table),
+		.pid_tbl = pump_pid_table,
+		.pid_tbl_num = ARRAY_SIZE(pump_pid_table),
+		.interval = 1,
+		.set_duty = pwm_control,
+		.set_duty_arg = PWM_GROUP_E_PUMP,
+		.out_limit_min = 20,
+		.out_limit_max = 100,
+	},
+	{
+		// zone 3 - rpu fan
+		.sw_tbl = rpu_fan_stepwise_table,
+		.sw_tbl_num = ARRAY_SIZE(rpu_fan_stepwise_table),
 		.interval = 1,
 		.set_duty = pwm_control,
 		.set_duty_arg = PWM_GROUP_E_RPU_FAN,
+		.out_limit_min = 20,
+		.out_limit_max = 100,
 	},
 };
 
-int zone_table_size = ARRAY_SIZE(zone_table);
+uint32_t zone_table_size = ARRAY_SIZE(zone_table);
