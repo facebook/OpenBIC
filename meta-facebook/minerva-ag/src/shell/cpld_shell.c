903806eb72cda074af0d4fde9b0193b643f6987d
victor <Victor.Jhong@quantatw.com>
2024-10-18 01:40:19 -0700

    minerva-ag: Add cpld dump shell command (#2018)
    Summary:
    - Add cpld dump shell command
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/2018
    
    Test Plan:
    - Build code: PASS
    - CPLD dump test: PASS
    
    CPLD dump:
    ```
    uart:~$ test cpld dump 0 40
    bus 4, addr 0x26, offset 0x0, len 0x40.
    00000000: ff 00 00 00 00 00 80 00  00 00 00 00 00 00 00 00 |........ ........|
    00000010: 00 00 00 00 00 0f 00 00  00 00 00 00 00 00 00 00 |........ ........|
    00000020: 00 00 00 00 ff ff ff ff  ff ff ff ff ff ff df ff |........ ........|
    00000030: 27 ef c0 5f ff 40 ff ff  ff ff ff ff ff ff ff ff |'.._.@.. ........|
    ```
    
    Reviewed By: ifel
    
    Differential Revision: D64233398
    
    fbshipit-source-id: ba17e0a08798a109d0fb3172d8f93720d1c7d838

---
+++ meta-facebook/minerva-ag/src/shell/cpld_shell.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <shell/shell.h>
+#include "hal_i2c.h"
+#include "plat_i2c.h"
+#include <stdlib.h>
+
+#define AEGIS_CPLD_ADDR (0x4C >> 1)
+#define I2C_BUS_CPLD I2C_BUS5
+
+void cmd_cpld_dump(const struct shell *shell, size_t argc, char **argv)
+{
+	if (argc != 3) {
+		shell_warn(shell, "Help: test cpld dump <offset> <length>");
+		return;
+	}
+
+	int cpld_offset = strtol(argv[1], NULL, 16);
+	int cpld_length = strtol(argv[2], NULL, 16);
+
+	if (cpld_offset < 0x00 || cpld_offset > 0xFF) {
+		shell_error(shell, "<offset> value is out of range!");
+		return;
+	}
+
+	if (cpld_length <= 0x00 || cpld_length > 0xFF) {
+		shell_error(shell, "<length> value is out of range!");
+		return;
+	}
+
+	uint8_t retry = 3;
+	I2C_MSG i2c_msg = { 0 };
+
+	i2c_msg.bus = I2C_BUS_CPLD;
+	i2c_msg.target_addr = AEGIS_CPLD_ADDR;
+	i2c_msg.rx_len = cpld_length;
+	i2c_msg.tx_len = 1;
+	i2c_msg.data[0] = cpld_offset;
+
+	if (i2c_master_read(&i2c_msg, retry)) {
+		shell_error(shell, "Failed to test cpld dump");
+	} else {
+		shell_print(shell, "bus %d, addr 0x%x, offset 0x%x, len 0x%x.", i2c_msg.bus,
+			    i2c_msg.target_addr, cpld_offset, i2c_msg.rx_len);
+
+		shell_hexdump(shell, i2c_msg.data, i2c_msg.rx_len);
+		shell_print(shell, "");
+	}
+	return;
+}
