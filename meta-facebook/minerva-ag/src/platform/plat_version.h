8205fe823d73b0fad17406ce88067b2737da335f
victor <Victor.Jhong@quantatw.com>
2025-01-09 11:19:21 +0800

    minerva-ag: Version commit for ob-minerva-ag-2025.02.01
    Summary:
    - Version commit for Minerva Aegis BIC ob-minerva-ag-2025.02.01.
    
    Test Plan:
    - Build code: Pass
    - Get BIC version: Pass
    
    Log:
    ```
    uart:~$ platform info
    ========================{SHELL COMMAND INFO}========================================
    * NAME:          Platform command
    * DESCRIPTION:   Commands that could be used to debug or validate.
    * DATE/VERSION:  none
    * CHIP/OS:       npcm400f_evb - Zephyr
    * Note:          none
    ------------------------------------------------------------------
    * PLATFORM:      Minerva-Aegis
    * BOARD ID:      1
    * STAGE:         1
    * SYSTEM:        255
    * FW VERSION:    17.0
    * FW DATE:       2025.02.1
    * FW IMAGE:      MINERVA_AEGIS.bin
    ========================{SHELL COMMAND INFO}========================================
    ```

--- meta-facebook/minerva-ag/src/platform/plat_version.h
+++ meta-facebook/minerva-ag/src/platform/plat_version.h
@@ -39,8 +39,8 @@
 #define AUXILIARY_FW_REVISION 0x00000000
 
 #define BIC_FW_YEAR_MSB 0x20
-#define BIC_FW_YEAR_LSB 0x24
-#define BIC_FW_WEEK 0x52
+#define BIC_FW_YEAR_LSB 0x25
+#define BIC_FW_WEEK 0x02
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x61 // char: a
 #define BIC_FW_platform_1 0x67 // char: g
