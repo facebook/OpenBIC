8cb0bda4744831a663a52991f63a862bc5def9c1
lisa <Lisa-Chang@quantatw.com>
2025-02-03 15:24:11 +0800

    minerva-ag: Modify voltage_peak commands
    Summary:
    - Modify voltage_peak commands with parameter hint
    - Delete plat_voltage_peak_shell.h file
    
    Test Plan:
    - Build code: PASS

--- meta-facebook/minerva-ag/src/shell/plat_voltage_peak_shell.h
+++ meta-facebook/minerva-ag/src/shell/plat_voltage_peak_shell.h
@@ -18,8 +18,39 @@
 #define PLAT_VOLTAGE_PEAK_SHELL_H
 
 #include <shell/shell.h>
+#include "plat_class.h"
 
 void cmd_get_voltage_peak(const struct shell *shell, size_t argc, char **argv);
 void cmd_clear_voltage_peak(const struct shell *shell, size_t argc, char **argv);
+
+static void voltage_rname_get(size_t idx, struct shell_static_entry *entry)
+{
+	if ((get_board_type() == MINERVA_AEGIS_BD))
+		idx++;
+
+	uint8_t *name = NULL;
+	vr_rail_name_get((uint8_t)idx, &name);
+
+	if (idx == VR_RAIL_E_MAX)
+		name = (uint8_t *)"all";
+
+	entry->syntax = (name) ? (const char *)name : NULL;
+	entry->handler = NULL;
+	entry->help = NULL;
+	entry->subcmd = NULL;
+}
+
+SHELL_DYNAMIC_CMD_CREATE(voltage_rname, voltage_rname_get);
+
+/* Sub-command Level 1 of command voltage-peak */
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_voltage_peak_cmds,
+			       SHELL_CMD(get, &voltage_rname, "get_voltage_peak",
+					 cmd_get_voltage_peak),
+			       SHELL_CMD(clear, &voltage_rname, "clear_voltage_peak",
+					 cmd_clear_voltage_peak),
+			       SHELL_SUBCMD_SET_END);
+
+/* Root of command voltage-peak */
+SHELL_CMD_REGISTER(voltage_peak, &sub_voltage_peak_cmds, "Voltage Peak commands for AG", NULL);
 
 #endif
