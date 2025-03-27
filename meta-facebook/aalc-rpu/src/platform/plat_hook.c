b884fb187ff80119882fd833419cd78761789fec
Hung-Yi <hung-yi.li@quantatw.com>
2025-03-27 11:38:15 -0700

    aalc: add fan table R5 (#2299)
    Summary:
    - add fan table R5
    - modify ads112c temp formula
    - modify flow ready from 10s to 40s when pump start
    - modify pump threshold retry from 6 times to 10 times
    - modify logic for checking low pump speed
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/2299
    
    Test Plan: - Build code: Pass
    
    Reviewed By: wangx6f
    
    Differential Revision: D71943083
    
    fbshipit-source-id: fddadd00e5a0566dd431c3b0e915074c9a904639

--- meta-facebook/aalc-rpu/src/platform/plat_hook.c
+++ meta-facebook/aalc-rpu/src/platform/plat_hook.c
@@ -1085,14 +1085,10 @@ bool post_ads112c_read(sensor_cfg *cfg, void *args, int *reading)
 		val = 6.89475729 * val;
 		break;
 	case PLATFORM_ADS112C_TEMP_RACK:
-		val = (rawValue - 15664) * 0.015873;
-		val = 1.0678 * val - 5.8373;
-		val = 1.0031 * val + 0.1566;
+		val = (rawValue - 15960) * 0.0162;
 		break;
 	case PLATFORM_ADS112C_TEMP_RPU: //CDU_Inlet_Liq_T
-		val = (rawValue - 15888) * 0.015873;
-		val = 1.0678 * val - 5.8373;
-		val = 1.0031 * val + 0.1566;
+		val = (rawValue - 16040) * 0.01695;
 		break;
 	default:
 		val = rawValue * 0.0001007;
