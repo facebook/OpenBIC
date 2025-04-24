fbdda380ab57d0b3eef8d320529f390c8b46aaeb
lisa <Lisa-Chang@quantatw.com>
2025-04-24 10:27:03 +0800

    version_commit_for_ob-minerva-ag-2025.16.03

--- meta-facebook/minerva-ag/src/platform/plat_version.h
+++ meta-facebook/minerva-ag/src/platform/plat_version.h
@@ -41,7 +41,7 @@
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x25
 #define BIC_FW_WEEK 0x16
-#define BIC_FW_VER 0x02
+#define BIC_FW_VER 0x03
 #define BIC_FW_platform_0 0x61 // char: a
 #define BIC_FW_platform_1 0x67 // char: g
 #define BIC_FW_platform_2 0x00 // char: '\0'
