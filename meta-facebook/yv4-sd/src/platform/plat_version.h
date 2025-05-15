b31a95e97d92eaeb2994157852c68355c277f74f
LioraGuo-wiwynn <liora.guo.wiwynn@gmail.com>
2025-05-14 19:13:06 -0700

    fby4: sd/wf: Version Commit for 2025.20.02 (#2375)
    Summary:
    Description
    - Version commit for Sentinel Dome BIC oby4-sd-2025.20.02
    - Version commit for Wailua Falls BIC oby4-wf-2025.20.02
    
    Motivation
    - Version commit for Sentinel Dome BIC oby4-sd-2025.20.02
    - Version commit for Wailua Falls BIC oby4-wf-2025.20.02
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/2375
    
    Test Plan:
    - Build code: Pass
    - Check BIC version: Pass
    
    Reviewed By: waffle2k, jamesatha
    
    Differential Revision: D74646811
    
    fbshipit-source-id: 12804836e1def3c37528ddb1161594579de3f345

--- meta-facebook/yv4-sd/src/platform/plat_version.h
+++ meta-facebook/yv4-sd/src/platform/plat_version.h
@@ -42,7 +42,7 @@
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x25
 #define BIC_FW_WEEK 0x20
-#define BIC_FW_VER 0x01
+#define BIC_FW_VER 0x02
 #define BIC_FW_platform_0 0x73 // char: s
 #define BIC_FW_platform_1 0x64 // char: d
 #define BIC_FW_platform_2 0x00 // char: '\0'
