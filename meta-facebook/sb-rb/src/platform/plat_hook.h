acab1c03039021ce694f5e136f800b1b5eb2acfe
Hung-Yi <hung-yi.li@quantatw.com>
2025-06-18 09:24:58 +0800

    sb: rb: initialize first code
    Summary:
    - Initialize SantaBrabara Rainbow BIC
    - Add project code in meta-facebook/sb-rb/
    - Version Commit for 2025.24.01
    
    Test Plan:
    - Build code: Pass
    - Check BIC version: Pass

---
+++ meta-facebook/sb-rb/src/platform/plat_hook.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PLAT_HOOK_H
+#define PLAT_HOOK_H
+
+#include "sensor.h"
+
+#define VR_MAX_NUM 12
+#define VR_MUTEX_LOCK_TIMEOUT_MS 1000
+
+#include "plat_pldm_sensor.h"
+
+enum VR_INDEX_E {
+	VR_INDEX_E_1 = 0,
+	VR_INDEX_E_2,
+	VR_INDEX_E_3,
+	VR_INDEX_E_4,
+	VR_INDEX_E_5,
+	VR_INDEX_E_6,
+	VR_INDEX_E_7,
+	VR_INDEX_E_8,
+	VR_INDEX_E_9,
+	VR_INDEX_E_10,
+	VR_INDEX_E_11,
+	VR_INDEX_E_12,
+	VR_INDEX_MAX,
+};
+
+enum VR_RAIL_E {
+	VR_RAIL_E_ASIC_P0V85_MEDHA1_VDD = 0,
+	VR_RAIL_E_ASIC_P0V85_MEDHA0_VDD,
+	VR_RAIL_E_ASIC_P0V9_OWL_E_TRVDD,
+	VR_RAIL_E_ASIC_P0V75_OWL_E_TRVDD,
+	VR_RAIL_E_ASIC_P0V75_MAX_M_VDD,
+	VR_RAIL_E_ASIC_P0V75_VDDPHY_HBM1357,
+	VR_RAIL_E_ASIC_P0V75_OWL_E_VDD,
+	VR_RAIL_E_ASIC_P0V4_VDDQL_HBM1357,
+	VR_RAIL_E_ASIC_P1V1_VDDQC_HBM1357,
+	VR_RAIL_E_ASIC_P1V8_VPP_HBM1357,
+	VR_RAIL_E_ASIC_P0V75_MAX_N_VDD,
+	VR_RAIL_E_ASIC_P0V8_HAMSA_AVDD_PCIE,
+	VR_RAIL_E_ASIC_P1V2_HAMSA_VDDHRXTX_PCIE,
+	VR_RAIL_E_ASIC_P0V85_HAMSA_VDD,
+	VR_RAIL_E_ASIC_P1V1_VDDQC_HBM0246,
+	VR_RAIL_E_ASIC_P1V8_VPP_HBM0246,
+	VR_RAIL_E_ASIC_P0V4_VDDQL_HBM0246,
+	VR_RAIL_EASIC_P0V75_VDDPHY_HBM0246,
+	VR_RAIL_E_ASIC_P0V75_OWL_W_VDD,
+	VR_RAIL_E_ASIC_P0V75_MAX_S_VDD,
+	VR_RAIL_E_ASIC_P0V9_OWL_W_TRVDD,
+	VR_RAIL_E_ASIC_P0V75_OWL_W_TRVDD,
+	VR_RAIL_E_MAX,
+};
+
+typedef struct _vr_pre_proc_arg {
+	void *mutex;
+	uint8_t vr_page;
+} vr_pre_proc_arg;
+
+bool pre_vr_read(sensor_cfg *cfg, void *args);
+bool post_vr_read(sensor_cfg *cfg, void *args, int *const reading);
+bool is_mb_dc_on();
+void *vr_mutex_get(enum VR_INDEX_E vr_index);
+void vr_mutex_init(void);
+#endif
