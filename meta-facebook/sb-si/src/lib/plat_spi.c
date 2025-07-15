795472b1e5ca728111f8c4ddee5760b1ec54b9d0
Billy <Billy.Tang@quantatw.com>
2025-07-15 15:38:39 +0800

    sb: sitv3: support PLDM firmware update for PCIE switch Summary: - Enable PLDM-based firmware update for PCIe switch using spi_fiu0_cs1
    Test Plan:
    - Build code: Pass
    - spi_fiu0_cs1 functions correctly
    - The PCIE switch update function works normally

--- meta-facebook/sb-si/src/lib/plat_spi.c
+++ meta-facebook/sb-si/src/lib/plat_spi.c
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -14,26 +14,22 @@
  * limitations under the License.
  */
 
-#ifndef _PLAT_FWUPDATE_H_
-#define _PLAT_FWUPDATE_H_
+#include "plat_spi.h"
+#include "hal_gpio.h"
+#include "plat_gpio.h"
+#include "util_spi.h"
+#include "plat_i2c.h"
+#include "plat_i3c.h"
 
-#include <stdbool.h>
-#include <stdint.h>
-#include "pldm_firmware_update.h"
+int pal_get_pcie_switch_flash_position()
+{
+	return DEVSPI_SPI1_CS1;
+}
 
-enum FIRMWARE_COMPONENT {
-	SI_COMPNT_BIC,
-	SI_COMPNT_P0V895_PEX,
-	SI_COMPNT_P0V825_A0,
-	SI_COMPNT_P0V825_A1,
-	SI_COMPNT_P0V825_A2,
-	SI_COMPNT_PCIE_SWITCH,
-};
-
-void load_pldmupdate_comp_config(void);
-void clear_pending_version(uint8_t activate_method);
-bool find_sensor_id_and_name_by_firmware_comp_id(uint8_t comp_identifier, uint8_t *sensor_id,
-						 char *sensor_name);
-int get_si_vr_compnt_mapping_sensor_table_count(void);
-
-#endif /* _PLAT_FWUPDATE_H_ */
+bool pal_switch_pcie_switch_spi_mux(int gpio_status)
+{
+	if (gpio_set(SPI_MUX_SEL, gpio_status) == -1) {
+		return false;
+	};
+	return true;
+}
--- meta-facebook/sb-si/src/lib/plat_spi.h
+++ meta-facebook/sb-si/src/lib/plat_spi.h
@@ -14,26 +14,9 @@
  * limitations under the License.
  */
 
-#ifndef _PLAT_FWUPDATE_H_
-#define _PLAT_FWUPDATE_H_
+#ifndef PLAT_SPI_H
+#define PLAT_SPI_H
 
-#include <stdbool.h>
-#include <stdint.h>
-#include "pldm_firmware_update.h"
+/* This file is generated for build code passing. */
 
-enum FIRMWARE_COMPONENT {
-	SI_COMPNT_BIC,
-	SI_COMPNT_P0V895_PEX,
-	SI_COMPNT_P0V825_A0,
-	SI_COMPNT_P0V825_A1,
-	SI_COMPNT_P0V825_A2,
-	SI_COMPNT_PCIE_SWITCH,
-};
-
-void load_pldmupdate_comp_config(void);
-void clear_pending_version(uint8_t activate_method);
-bool find_sensor_id_and_name_by_firmware_comp_id(uint8_t comp_identifier, uint8_t *sensor_id,
-						 char *sensor_name);
-int get_si_vr_compnt_mapping_sensor_table_count(void);
-
-#endif /* _PLAT_FWUPDATE_H_ */
+#endif
