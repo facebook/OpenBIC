5c482ea9a84005271a540eb4c66e26dff091c037
lisa <Lisa-Chang@quantatw.com>
2025-07-29 10:57:35 +0800

    minerva-ag: Support trigger SEL on VR Power Fault event
    Summary:
    - Support trigger SEL on VR Power Fault event
    - Add MTIA_VR_FAULT(0x27)
    
    Test Plan:
    - Build code: PASS

--- meta-facebook/minerva-ag/src/platform/plat_event.h
+++ meta-facebook/minerva-ag/src/platform/plat_event.h
@@ -203,6 +203,8 @@ typedef struct _aegis_cpld_info_ {
 	//bit check mask
 	uint8_t bit_check_mask; //bit check mask
 
+	uint8_t event_type;
+
 } aegis_cpld_info;
 
 typedef struct {
@@ -210,6 +212,8 @@ typedef struct {
 	const char *reg_name;
 	const char *bit_name[8];
 } cpld_bit_name_table_t;
+
+enum event_type { VR_POWER_FAULT = 0x01 };
 
 const char *get_cpld_reg_name(uint8_t cpld_offset);
 const char *get_cpld_bit_name(uint8_t cpld_offset, uint8_t bit_pos);
