082dfbb628feec3f625e57e39290616fa45b123d
adamHSU <Adam.Hsu@.quantatw.com>
2025-08-14 20:42:38 +0800

    sb: sitv3: modify sensor's UCR and BCR value in PDR table Summary: - Modify sensor's USR and BCR value in PDR table - Version Commit for 2025.33.01 Test Plan: - Build code: Pass - Check BIC version: Pass

--- meta-facebook/sb-si/src/platform/plat_version.h
+++ meta-facebook/sb-si/src/platform/plat_version.h
@@ -37,7 +37,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x25
-#define BIC_FW_WEEK 0x31
+#define BIC_FW_WEEK 0x33
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x73 // char: s
 #define BIC_FW_platform_1 0x69 // char: i
