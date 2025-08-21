35b5613b1db5c50eaf70eb0302a36463a7ec97e4
victor <Victor.Jhong@quantatw.com>
2025-08-21 11:04:48 -0700

    GT: Version commit for gt-2025.34.01 (#2502)
    Summary:
    - Version commit for gt-2025.34.01
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/2502
    
    Test Plan: - Build code: Pass
    
    Reviewed By: waffle2k
    
    Differential Revision: D80684136
    
    fbshipit-source-id: bd254d8230795e371660ba12a7473f4fc55d72ee

--- meta-facebook/gt-cc/src/platform/plat_version.h
+++ meta-facebook/gt-cc/src/platform/plat_version.h
@@ -39,9 +39,9 @@
 #define AUXILIARY_FW_REVISION 0x00000000
 
 #define BIC_FW_YEAR_MSB 0x20
-#define BIC_FW_YEAR_LSB 0x24
-#define BIC_FW_WEEK 0x36
-#define BIC_FW_VER 0x02
+#define BIC_FW_YEAR_LSB 0x25
+#define BIC_FW_WEEK 0x34
+#define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x47 // char: G
 #define BIC_FW_platform_1 0x54 // char: T
 #define BIC_FW_platform_2 0x00 // char: '\0'
