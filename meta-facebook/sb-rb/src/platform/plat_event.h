8302446b6cced4edadcb890418d98e3b62bab90a
Kevin Huang <KevinHuang4@quntatw.com>
2025-09-09 09:13:11 +0800

    add vr power fault event log to bmc tmp

--- meta-facebook/sb-rb/src/platform/plat_event.h
+++ meta-facebook/sb-rb/src/platform/plat_event.h
@@ -16,5 +16,41 @@
 #ifndef PLAT_EVENT_H
 #define PLAT_EVENT_H
 #include <stdbool.h>
+#include <stdint.h>
+
+typedef struct _rainbow_cpld_info_ rainbow_cpld_info;
+
+typedef struct _rainbow_cpld_info_ {
+	uint8_t cpld_offset;
+	uint8_t dc_off_defaut;
+	uint8_t dc_on_defaut;
+	bool is_fault_log; // if true, check the value is defaut or not
+	uint8_t is_fault_bit_map; //flag for fault
+
+	//flag for 1st polling
+	bool is_first_polling;
+
+	//flag for 1st polling after changing DC status
+	bool is_first_polling_after_dc_change;
+
+	//temp data for last polling
+	uint8_t last_polling_value;
+
+	bool (*status_changed_cb)(rainbow_cpld_info *, uint8_t *);
+
+	//bit check mask
+	uint8_t bit_check_mask; //bit check mask
+
+	uint8_t event_type;
+
+} rainbow_cpld_info;
+
+typedef struct _vr_fault_info {
+	uint8_t mtia_event_source;
+	uint8_t cpld_reg_offset;
+	uint8_t cpld_reg_bit;
+	bool is_pmbus_vr;
+	uint8_t sensor_id;
+} vr_fault_info;
 
 #endif
