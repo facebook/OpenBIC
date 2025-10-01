facebook/OpenBIC

commit edb8cc671e085e3ec3104dfd97a8f552de09bd5c

sb: sitv3: modify PCIe switch access check before polling Summary: - Updated is_pcie_switch_access to retrieve bus/address by sensor ID - Ensure PEX90144 exists via I2C read before allowing sensor polling Test Plan: - Build: Pass - Verified sensor polling only enabled when PEX90144 is detected

meta-facebook/sb-si/src/platform/plat_pldm_sensor.c
@@ -2633,6 +2633,30 @@ bool is_temp_access(uint8_t cfg_idx)
 
 bool is_pcie_switch_access(uint8_t cfg_idx)
 {
+	I2C_MSG msg = { 0 };
+	uint8_t retry = 3;
+
+	uint8_t sensor_id = cfg_idx;
+	uint8_t bus = 0, addr = 0;
+	uint8_t sensor_dev = 0;
+
+	if (!get_sensor_info_by_sensor_id(sensor_id, &bus, &addr, &sensor_dev)) {
+		LOG_ERR("Can't find PCIe switch addr and bus by sensor id: %d", sensor_id);
+		return false;
+	}
+
+	msg.bus = bus;
+	msg.target_addr = addr;
+	msg.tx_len = 0;
+	msg.rx_len = 1;
+
+	if (i2c_master_read(&msg, retry) != 0) {
+		LOG_ERR("PEX90144 not found at bus %d addr 0x%X", msg.bus, msg.target_addr);
+		return false;
+	}
+
+	LOG_DBG("PEX90144 is present at bus %d addr 0x%X", msg.bus, msg.target_addr);
+
 	return (get_plat_sensor_temp_polling_enable_flag() &&
 		get_plat_sensor_polling_enable_flag());
 }

meta-facebook/sb-si/src/platform/plat_pldm_sensor.h
@@ -39,6 +39,7 @@
 #define VR_ASIC_P0V825_A2_ISL69260_ADDR (0xC2 >> 1)
 
 #define ADS7830_I2C_ADDR (0x90 >> 1)
+#define PEX90144_I2C_ADDR (0xD4 >> 1)
 
 /* Define the sensor numbers used in this platform */
 enum SENSOR_NUM_LIST {

commit de7c4a7bf6a16ce4f0f85efbbaf158584f150d3d

sb: sitv3: fix PCIe switch access index mismatch (#2562)
Summary:
Summery:
- Use plat_pldm_sensor_temp_table[cfg_idx] directly
- Fix 0-base vs 1-base mismatch in bus/address lookup
- Fixes: edb8cc671e085e3ec3104dfd97a8f552de09bd5c
- Version commit for 2025.40.01

Pull Request resolved: https://github.com/facebook/OpenBIC/pull/2562

Test Plan:
- Build: Pass
- Verified PEX90144 access check and polling

Reviewed By: jagpalgill

Differential Revision: D83623362

fbshipit-source-id: e38cf4e9b1f39151496e0841b20c79966abd820c

meta-facebook/sb-si/src/platform/plat_pldm_sensor.c
@@ -2635,15 +2635,8 @@ bool is_pcie_switch_access(uint8_t cfg_idx)
 {
 	I2C_MSG msg = { 0 };
 	uint8_t retry = 3;
-
-	uint8_t sensor_id = cfg_idx;
-	uint8_t bus = 0, addr = 0;
-	uint8_t sensor_dev = 0;
-
-	if (!get_sensor_info_by_sensor_id(sensor_id, &bus, &addr, &sensor_dev)) {
-		LOG_ERR("Can't find PCIe switch addr and bus by sensor id: %d", sensor_id);
-		return false;
-	}
+	uint8_t bus = plat_pldm_sensor_temp_table[cfg_idx].pldm_sensor_cfg.port;
+	uint8_t addr = plat_pldm_sensor_temp_table[cfg_idx].pldm_sensor_cfg.target_addr;
 
 	msg.bus = bus;
 	msg.target_addr = addr;

meta-facebook/sb-si/src/platform/plat_version.h
@@ -37,7 +37,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x25
-#define BIC_FW_WEEK 0x39
+#define BIC_FW_WEEK 0x40
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x73 // char: s
 #define BIC_FW_platform_1 0x69 // char: i