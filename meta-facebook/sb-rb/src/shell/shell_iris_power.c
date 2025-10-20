eaac1909221a637a61752bb3f22a4d131e4b2a51
Kevin Huang <KevinHuang4@quntatw.com>
2025-10-20 16:47:57 +0800

    add new pwr step on

--- meta-facebook/sb-rb/src/shell/shell_iris_power.c
+++ meta-facebook/sb-rb/src/shell/shell_iris_power.c
@@ -18,7 +18,7 @@ typedef struct steps_on_struct {
 
 static steps_on_struct steps_on[] = {
 	// follow pwr sequence order
-	//TODO: p12V
+	{ VR_AND_CLK_EN_PIN_CTRL, 1, "FM_PLD_UBC_EN" }, //p12V
 	{ VR_4_EN, 6, "FM_P3V3_EN" }, //FM_P3V3_EN
 	{ VR_4_EN, 7, "FM_P5V_EN" }, //FM_P5V_EN
 	{ VR_4_EN, 5, "LDO_IN_1V2_EN_R" }, //LDO_IN_1V2_EN_R
@@ -44,7 +44,7 @@ static steps_on_struct steps_on[] = {
 	{ VR_4_EN, 4, "FM_PLL_VDDA15_HBM5_HBM7_EN" }, //FM_PLL_VDDA15_HBM5_HBM7_EN
 	{ VR_AND_CLK_EN, 6, "FM_AEGIS_CLK_100MHZ_EN_N" }, //FM_AEGIS_CLK_100MHZ_EN_N
 	{ VR_AND_CLK_EN, 7, "FM_AEGIS_CLK_48MHZ_EN" }, //FM_AEGIS_CLK_48MHZ_EN
-	//TODO: IRIS_CLK_312.5MHZ
+	{ VR_AND_CLK_EN_PIN_CTRL, 0, "FM_IRIS_CLK_312MHZ_EN_N" }, //FM_IRIS_CLK_312MHZ_EN_N
 	{ VR_2_EN, 5, "FM_VPP_HBM0_HBM2_HBM4_HBM6_EN" }, //FM_VPP_HBM0_HBM2_HBM4_HBM6_EN
 	{ VR_2_EN, 1, "FM_VPP_HBM1_HBM3_HBM5_HBM7_EN" }, //FM_VPP_HBM1_HBM3_HBM5_HBM7_EN
 	{ VR_2_EN, 6, "FM_VDDQC_HBM0_HBM2_HBM4_HBM6_EN" }, //FM_VDDQC_HBM0_HBM2_HBM4_HBM6_EN
@@ -65,6 +65,8 @@ static steps_on_struct steps_on[] = {
 	{ RESET, 1, "MEDHA0_SYS_RST_PLD_L" }, //MEDHA0_SYS_RST_PLD_L
 	{ RESET, 0, "MEDHA1_SYS_RST_PLD_L" }, //MEDHA1_SYS_RST_PLD_L
 };
+
+#define MAX_STEPS (sizeof(steps_on) / sizeof(steps_on[0]))
 
 static bool iris_power_control(uint8_t onoff)
 {
@@ -91,20 +93,52 @@ void cmd_iris_power_cycle(const struct shell *shell, size_t argc, char **argv)
 		shell_warn(shell, "iris power cycle(on) fail!");
 }
 
+bool inverse_bit(uint8_t offset, uint8_t bit)
+{
+	uint8_t tmp_data = 0;
+	bool error_flag = false;
+	if (!plat_read_cpld(offset, &tmp_data, 1))
+		error_flag = true;
+	LOG_INF("read offset 0x%x = 0x%x", offset, tmp_data);
+	tmp_data ^= BIT(bit);
+	LOG_WRN("write offset 0x%x = 0x%x", offset, tmp_data);
+	if (!plat_write_cpld(offset, &tmp_data))
+		error_flag = true;
+	return error_flag;
+}
+
+int power_steps = 0;
 void cmd_iris_steps_on(const struct shell *shell, size_t argc, char **argv)
 {
-	for (int i = 0; i < sizeof(steps_on) / sizeof(steps_on[0]); i++) {
-		LOG_DBG("set %s on, offset = 0x%x, bit = %d", steps_on[i].name,
-			steps_on[i].cpld_offset, steps_on[i].bit);
-
-		if (!set_cpld_bit(steps_on[i].cpld_offset, steps_on[i].bit, 1)) {
-			// if error show name
-			shell_warn(shell, "set %s fail", steps_on[i].name);
-			return;
+	if (power_steps >= MAX_STEPS) {
+		shell_warn(shell, "no more steps");
+		return;
+	}
+	// first step need to inverse all enable bit
+	if (power_steps == 0) {
+		for (int i = 0; i < MAX_STEPS; i++) {
+			if (inverse_bit(steps_on[i].cpld_offset, steps_on[i].bit)) {
+				LOG_ERR("set %-35s fail", steps_on[i].name);
+			}
 		}
 
-		shell_print(shell, "set %-35s success", steps_on[i].name);
+		if (!iris_power_control(1)) {
+			shell_warn(shell, "iris power on fail!");
+			return;
+		}
 	}
+
+	LOG_DBG("set %s on, offset = 0x%x, bit = %d", steps_on[power_steps].name,
+		steps_on[power_steps].cpld_offset, steps_on[power_steps].bit);
+
+	// turn on the steps on bit
+	if (inverse_bit(steps_on[power_steps].cpld_offset, steps_on[power_steps].bit)) {
+		shell_print(shell, "set %-35s fail", steps_on[power_steps].name);
+		return;
+	} else
+		shell_print(shell, "set %-35s success", steps_on[power_steps].name);
+
+	power_steps += 1;
 }
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_iris_power_cmd,
