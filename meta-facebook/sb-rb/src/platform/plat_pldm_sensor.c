bdbf1ae3e5ada29ee94950821a1838a44928d3d6
Kevin Huang <KevinHuang4@quntatw.com>
2025-11-06 16:43:29 +0800

    update SENSOR_NUM_ASIC_P0V4_VDDQL_HBM0246_VOLT_V threshold

--- meta-facebook/sb-rb/src/platform/plat_pldm_sensor.c
+++ meta-facebook/sb-rb/src/platform/plat_pldm_sensor.c
@@ -5596,7 +5596,7 @@ pldm_sensor_info plat_pldm_sensor_vr_table[] = {
 			0x00000000, //uint32_t normal_min;
 			0, //uint32_t warning_high;
 			0, //uint32_t warning_low;
-			432, //uint32_t critical_high;
+			461, //uint32_t critical_high;
 			368, //uint32_t critical_low;
 			0, //uint32_t fatal_high;
 			0, //uint32_t fatal_low;
