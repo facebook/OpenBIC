4d008d9852d83ec123c8337ca536c5ce407ba588
Kevin Huang <KevinHuang4@quntatw.com>
2025-11-21 20:28:26 +0800

    fix step pwr

--- meta-facebook/sb-rb/src/shell/shell_iris_power.c
+++ meta-facebook/sb-rb/src/shell/shell_iris_power.c
@@ -11,6 +11,7 @@
 LOG_MODULE_REGISTER(shell_iris_power);
 
 typedef struct steps_on_struct {
+	uint8_t power_on_value;
 	uint8_t cpld_offset;
 	uint8_t bit;
 	uint8_t *name;
@@ -18,52 +19,52 @@ typedef struct steps_on_struct {
 
 static steps_on_struct steps_on[] = {
 	// follow pwr sequence order
-	{ VR_AND_CLK_EN_PIN_CTRL, 1, "FM_PLD_UBC_EN" }, //p12V
-	{ VR_4_EN, 6, "FM_P3V3_EN" }, //FM_P3V3_EN
-	{ VR_4_EN, 7, "FM_P5V_EN" }, //FM_P5V_EN
-	{ VR_4_EN, 5, "LDO_IN_1V2_EN_R" }, //LDO_IN_1V2_EN_R
-	{ VR_4_EN, 4, "FM_P1V80_EN" }, //FM_P1V80_EN
-	{ VR_3_EN, 0, "FM_AVDD_HCSL_EN" }, //FM_AVDD_HCSL_EN
-	{ VR_1_EN, 3, "FM_HAMSA_VDD_EN" }, //FM_HAMSA_VDD_EN
-	{ VR_1_EN, 7, "MEDHA1_VDD_EN" }, //MEDHA1_VDD_EN
-	{ VR_1_EN, 6, "FM_MEDHA0_VDD_EN" }, //FM_MEDHA0_VDD_EN
-	{ VR_1_EN, 5, "FM_OWL_E_VDD_EN" }, //FM_OWL_E_VDD_EN
-	{ VR_1_EN, 4, "FM_OWL_W_VDD_EN" }, //FM_OWL_W_VDD_EN
-	{ VR_1_EN, 1, "FM_MAX_M_VDD_EN" }, //FM_MAX_M_VDD_EN
-	{ VR_1_EN, 0, "FM_MAX_N_VDD_EN" }, //FM_MAX_N_VDD_EN
-	{ VR_1_EN, 2, "FM_MAX_S_VDD_EN" }, //FM_MAX_S_VDD_EN
-	{ VR_AND_CLK_EN, 3, "FM_OWL_E_TRVDD0P75_EN" }, //FM_OWL_E_TRVDD0P75_EN
-	{ VR_AND_CLK_EN, 2, "FM_OWL_W_TRVDD0P75_EN" }, //FM_OWL_W_TRVDD0P75_EN
-	{ VR_2_EN, 4, "FM_VDDPHY_HBM0_HBM2_HBM4_HBM6_EN" }, //FM_VDDPHY_HBM0_HBM2_HBM4_HBM6_EN
-	{ VR_2_EN, 0, "FM_VDDPHY_HBM1_HBM3_HBM5_HBM7_EN" }, //FM_VDDPHY_HBM1_HBM3_HBM5_HBM7_EN
-	{ VR_4_EN, 1, "FM_P1V5_PLL_VDDA_OWL_EN" }, //FM_P1V5_PLL_VDDA_OWL_EN
-	{ VR_4_EN, 0, "FM_P1V5_PLL_VDDA_SOC_EN" }, //FM_P1V5_PLL_VDDA_SOC_EN
-	{ VR_4_EN, 2, "FM_PLL_VDDA15_HBM0_HBM2_EN" }, //FM_PLL_VDDA15_HBM0_HBM2_EN
-	{ VR_4_EN, 5, "FM_PLL_VDDA15_HBM1_HBM3_EN" }, //FM_PLL_VDDA15_HBM1_HBM3_EN
-	{ VR_4_EN, 6, "FM_PLL_VDDA15_HBM4_HBM6_EN" }, //FM_PLL_VDDA15_HBM4_HBM6_EN
-	{ VR_4_EN, 4, "FM_PLL_VDDA15_HBM5_HBM7_EN" }, //FM_PLL_VDDA15_HBM5_HBM7_EN
-	{ VR_AND_CLK_EN, 6, "FM_AEGIS_CLK_100MHZ_EN_N" }, //FM_AEGIS_CLK_100MHZ_EN_N
-	{ VR_AND_CLK_EN, 7, "FM_AEGIS_CLK_48MHZ_EN" }, //FM_AEGIS_CLK_48MHZ_EN
-	{ VR_AND_CLK_EN_PIN_CTRL, 0, "FM_IRIS_CLK_312MHZ_EN_N" }, //FM_IRIS_CLK_312MHZ_EN_N
-	{ VR_2_EN, 5, "FM_VPP_HBM0_HBM2_HBM4_HBM6_EN" }, //FM_VPP_HBM0_HBM2_HBM4_HBM6_EN
-	{ VR_2_EN, 1, "FM_VPP_HBM1_HBM3_HBM5_HBM7_EN" }, //FM_VPP_HBM1_HBM3_HBM5_HBM7_EN
-	{ VR_2_EN, 6, "FM_VDDQC_HBM0_HBM2_HBM4_HBM6_EN" }, //FM_VDDQC_HBM0_HBM2_HBM4_HBM6_EN
-	{ VR_2_EN, 2, "FM_VDDQC_HBM1_HBM3_HBM5_HBM7_EN" }, //FM_VDDQC_HBM1_HBM3_HBM5_HBM7_EN
-	{ VR_2_EN, 7, "FM_VDDQL_HBM0_HBM2_HBM4_HBM6_EN" }, //FM_VDDQL_HBM0_HBM2_HBM4_HBM6_EN
-	{ VR_2_EN, 3, "FM_VDDQL_HBM1_HBM3_HBM5_HBM7_EN" }, //FM_VDDQL_HBM1_HBM3_HBM5_HBM7_EN
-	{ VR_AND_CLK_EN, 1, "FM_HAMSA_AVDD_PCIE_EN" }, //FM_HAMSA_AVDD_PCIE_EN
-	{ VR_AND_CLK_EN, 5, "FM_OWL_E_TRVDD0P9_EN" }, //FM_OWL_E_TRVDD0P9_EN
-	{ VR_AND_CLK_EN, 4, "FM_OWL_W_TRVDD0P9_EN" }, //FM_OWL_W_TRVDD0P9_EN
-	{ VR_3_EN, 3, "FM_PVDD0P9_EN" }, //FM_PVDD0P9_EN (OWL_E_PVDD0P9, OWL_W_PVDD0P9)
-	{ VR_3_EN, 2, "FM_P1V5_RVDD_EN" }, //FM_P1V5_RVDD_EN (OWL_E_RVDD1P5, OWL_W_RVDD1P5)
-	{ VR_AND_CLK_EN, 0, "FM_HAMSA_VDDHRXTX_PCIE_EN" }, //FM_HAMSA_VDDHRXTX_PCIE_EN
-	{ VR_3_EN, 2, "FM_PVDD1P5_EN" }, //FM_PVDD1P5_EN
-	{ RESET, 5, "HAMSA_POWER_ON_RESET_PLD_L" }, //HAMSA_POWER_ON_RESET_PLD_L
-	{ RESET, 4, "MEDHA0_POWER_ON_RESET_PLD_L" }, //MEDHA0_POWER_ON_RESET_PLD_L
-	{ RESET, 3, "MEDHA1_POWER_ON_RESET_PLD_L" }, //MEDHA1_POWER_ON_RESET_PLD_L
-	{ RESET, 2, "HAMSA_SYS_RST_PLD_L" }, //HAMSA_SYS_RST_PLD_L
-	{ RESET, 1, "MEDHA0_SYS_RST_PLD_L" }, //MEDHA0_SYS_RST_PLD_L
-	{ RESET, 0, "MEDHA1_SYS_RST_PLD_L" }, //MEDHA1_SYS_RST_PLD_L
+	{ 1, VR_AND_CLK_EN_PIN_CTRL, 1, "FM_PLD_UBC_EN" }, //p12V
+	{ 1, VR_4_EN, 6, "FM_P3V3_EN" }, //FM_P3V3_EN
+	{ 1, VR_4_EN, 7, "FM_P5V_EN" }, //FM_P5V_EN
+	{ 1, VR_4_EN, 5, "LDO_IN_1V2_EN_R" }, //LDO_IN_1V2_EN_R
+	{ 1, VR_4_EN, 4, "FM_P1V80_EN" }, //FM_P1V80_EN
+	{ 1, VR_3_EN, 0, "FM_AVDD_HCSL_EN" }, //FM_AVDD_HCSL_EN
+	{ 1, VR_1_EN, 3, "FM_HAMSA_VDD_EN" }, //FM_HAMSA_VDD_EN
+	{ 1, VR_1_EN, 7, "MEDHA1_VDD_EN" }, //MEDHA1_VDD_EN
+	{ 1, VR_1_EN, 6, "FM_MEDHA0_VDD_EN" }, //FM_MEDHA0_VDD_EN
+	{ 1, VR_1_EN, 5, "FM_OWL_E_VDD_EN" }, //FM_OWL_E_VDD_EN
+	{ 1, VR_1_EN, 4, "FM_OWL_W_VDD_EN" }, //FM_OWL_W_VDD_EN
+	{ 1, VR_1_EN, 1, "FM_MAX_M_VDD_EN" }, //FM_MAX_M_VDD_EN
+	{ 1, VR_1_EN, 0, "FM_MAX_N_VDD_EN" }, //FM_MAX_N_VDD_EN
+	{ 1, VR_1_EN, 2, "FM_MAX_S_VDD_EN" }, //FM_MAX_S_VDD_EN
+	{ 1, VR_AND_CLK_EN, 3, "FM_OWL_E_TRVDD0P75_EN" }, //FM_OWL_E_TRVDD0P75_EN
+	{ 1, VR_AND_CLK_EN, 2, "FM_OWL_W_TRVDD0P75_EN" }, //FM_OWL_W_TRVDD0P75_EN
+	{ 1, VR_2_EN, 4, "FM_VDDPHY_HBM0_HBM2_HBM4_HBM6_EN" }, //FM_VDDPHY_HBM0_HBM2_HBM4_HBM6_EN
+	{ 1, VR_2_EN, 0, "FM_VDDPHY_HBM1_HBM3_HBM5_HBM7_EN" }, //FM_VDDPHY_HBM1_HBM3_HBM5_HBM7_EN
+	{ 1, VR_4_EN, 1, "FM_P1V5_PLL_VDDA_OWL_EN" }, //FM_P1V5_PLL_VDDA_OWL_EN
+	{ 1, VR_4_EN, 0, "FM_P1V5_PLL_VDDA_SOC_EN" }, //FM_P1V5_PLL_VDDA_SOC_EN
+	{ 1, VR_3_EN, 7, "FM_PLL_VDDA15_HBM0_HBM2_EN" }, //FM_PLL_VDDA15_HBM0_HBM2_EN
+	{ 1, VR_3_EN, 5, "FM_PLL_VDDA15_HBM1_HBM3_EN" }, //FM_PLL_VDDA15_HBM1_HBM3_EN
+	{ 1, VR_3_EN, 6, "FM_PLL_VDDA15_HBM4_HBM6_EN" }, //FM_PLL_VDDA15_HBM4_HBM6_EN
+	{ 1, VR_3_EN, 4, "FM_PLL_VDDA15_HBM5_HBM7_EN" }, //FM_PLL_VDDA15_HBM5_HBM7_EN
+	{ 1, VR_AND_CLK_EN, 6, "FM_AEGIS_CLK_100MHZ_EN_N" }, //FM_AEGIS_CLK_100MHZ_EN_N
+	{ 1, VR_AND_CLK_EN, 7, "FM_AEGIS_CLK_48MHZ_EN" }, //FM_AEGIS_CLK_48MHZ_EN
+	{ 0, VR_AND_CLK_EN_PIN_CTRL, 0, "FM_IRIS_CLK_312MHZ_EN_N" }, //FM_IRIS_CLK_312MHZ_EN_N
+	{ 1, VR_2_EN, 5, "FM_VPP_HBM0_HBM2_HBM4_HBM6_EN" }, //FM_VPP_HBM0_HBM2_HBM4_HBM6_EN
+	{ 1, VR_2_EN, 1, "FM_VPP_HBM1_HBM3_HBM5_HBM7_EN" }, //FM_VPP_HBM1_HBM3_HBM5_HBM7_EN
+	{ 1, VR_2_EN, 6, "FM_VDDQC_HBM0_HBM2_HBM4_HBM6_EN" }, //FM_VDDQC_HBM0_HBM2_HBM4_HBM6_EN
+	{ 1, VR_2_EN, 2, "FM_VDDQC_HBM1_HBM3_HBM5_HBM7_EN" }, //FM_VDDQC_HBM1_HBM3_HBM5_HBM7_EN
+	{ 1, VR_2_EN, 7, "FM_VDDQL_HBM0_HBM2_HBM4_HBM6_EN" }, //FM_VDDQL_HBM0_HBM2_HBM4_HBM6_EN
+	{ 1, VR_2_EN, 3, "FM_VDDQL_HBM1_HBM3_HBM5_HBM7_EN" }, //FM_VDDQL_HBM1_HBM3_HBM5_HBM7_EN
+	{ 1, VR_AND_CLK_EN, 1, "FM_HAMSA_AVDD_PCIE_EN" }, //FM_HAMSA_AVDD_PCIE_EN
+	{ 1, VR_AND_CLK_EN, 5, "FM_OWL_E_TRVDD0P9_EN" }, //FM_OWL_E_TRVDD0P9_EN
+	{ 1, VR_AND_CLK_EN, 4, "FM_OWL_W_TRVDD0P9_EN" }, //FM_OWL_W_TRVDD0P9_EN
+	{ 1, VR_3_EN, 3, "FM_PVDD0P9_EN" }, //FM_PVDD0P9_EN (OWL_E_PVDD0P9, OWL_W_PVDD0P9)
+	{ 1, VR_3_EN, 1, "FM_P1V5_RVDD_EN" }, //FM_P1V5_RVDD_EN (OWL_E_RVDD1P5, OWL_W_RVDD1P5)
+	{ 1, VR_AND_CLK_EN, 0, "FM_HAMSA_VDDHRXTX_PCIE_EN" }, //FM_HAMSA_VDDHRXTX_PCIE_EN
+	{ 1, VR_3_EN, 2, "FM_PVDD1P5_EN" }, //FM_PVDD1P5_EN
+	{ 1, RESET, 5, "HAMSA_POWER_ON_RESET_PLD_L" }, //HAMSA_POWER_ON_RESET_PLD_L
+	{ 1, RESET, 4, "MEDHA0_POWER_ON_RESET_PLD_L" }, //MEDHA0_POWER_ON_RESET_PLD_L
+	{ 1, RESET, 3, "MEDHA1_POWER_ON_RESET_PLD_L" }, //MEDHA1_POWER_ON_RESET_PLD_L
+	{ 1, RESET, 2, "HAMSA_SYS_RST_PLD_L" }, //HAMSA_SYS_RST_PLD_L
+	{ 1, RESET, 1, "MEDHA0_SYS_RST_PLD_L" }, //MEDHA0_SYS_RST_PLD_L
+	{ 1, RESET, 0, "MEDHA1_SYS_RST_PLD_L" }, //MEDHA1_SYS_RST_PLD_L
 };
 
 #define MAX_STEPS (sizeof(steps_on) / sizeof(steps_on[0]))
@@ -93,20 +94,6 @@ void cmd_iris_power_cycle(const struct shell *shell, size_t argc, char **argv)
 		shell_warn(shell, "iris power cycle(on) fail!");
 }
 
-bool inverse_bit(uint8_t offset, uint8_t bit)
-{
-	uint8_t tmp_data = 0;
-	bool error_flag = false;
-	if (!plat_read_cpld(offset, &tmp_data, 1))
-		error_flag = true;
-	LOG_DBG("read offset 0x%x = 0x%x", offset, tmp_data);
-	tmp_data ^= BIT(bit);
-	LOG_DBG("write offset 0x%x = 0x%x", offset, tmp_data);
-	if (!plat_write_cpld(offset, &tmp_data))
-		error_flag = true;
-	return error_flag;
-}
-
 int power_steps = 0;
 void cmd_iris_steps_on(const struct shell *shell, size_t argc, char **argv)
 {
@@ -114,29 +101,16 @@ void cmd_iris_steps_on(const struct shell *shell, size_t argc, char **argv)
 		LOG_WRN("no more steps");
 		return;
 	}
-	// first step need to inverse all enable bit
-	if (power_steps == 0) {
-		for (int i = 0; i < MAX_STEPS; i++) {
-			if (inverse_bit(steps_on[i].cpld_offset, steps_on[i].bit)) {
-				LOG_ERR("set %-35s fail", steps_on[i].name);
-			}
-		}
 
-		if (!iris_power_control(1)) {
-			LOG_WRN("iris power on fail!");
-			return;
-		}
-	}
-
-	LOG_DBG("set %s on, offset = 0x%x, bit = %d", steps_on[power_steps].name,
-		steps_on[power_steps].cpld_offset, steps_on[power_steps].bit);
+	set_cpld_bit(VR_1STEP_FUNC_EN_REG, 0, 1);
 
 	// turn on the steps on bit
-	if (inverse_bit(steps_on[power_steps].cpld_offset, steps_on[power_steps].bit)) {
-		LOG_ERR("set %-35s fail", steps_on[power_steps].name);
-		return;
-	} else
-		LOG_INF("set %-35s success", steps_on[power_steps].name);
+	if (set_cpld_bit(steps_on[power_steps].cpld_offset, steps_on[power_steps].bit,
+			 steps_on[power_steps].power_on_value) == false) {
+		shell_print(shell, "set %-35s fail", steps_on[power_steps].name);
+	} else {
+		shell_print(shell, "set %-35s success", steps_on[power_steps].name);
+	}
 
 	power_steps += 1;
 }
