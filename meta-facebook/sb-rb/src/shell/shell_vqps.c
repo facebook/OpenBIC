8e5a12a8200abe342ce81654a638b7e872e6d002
Billy <Billy.Tang@quantatw.com>
2025-12-05 18:52:53 +0800

    sb: rb: update VQPS shell cmd for U200070 and EVT-specific bit mapping Summary: - Change VQPS shell command handling from U200052 to U200070 IO expander. - Update P1V8_OWL_EW_VQPS_EN bit mapping:   - EVT1B → bit 1   - EVT2  → bit 7 Test Plan: - Build code: Pass - Verified VQPS get/set works correctly on EVT1B and EVT2 boards.

--- meta-facebook/sb-rb/src/shell/shell_vqps.c
+++ meta-facebook/sb-rb/src/shell/shell_vqps.c
@@ -44,7 +44,8 @@ LOG_MODULE_REGISTER(vqps);
 #define MEDHA1_VQPS_W_EN_BIT 2
 #define MEDHA0_VQPS_E_EN_BIT 1
 #define MEDHA1_VQPS_E_EN_BIT 0
-#define P1V8_OWL_EW_VQPS_EN 1
+#define EVT1B_P1V8_OWL_EW_VQPS_EN 1
+#define EVT2_P1V8_OWL_EW_VQPS_EN 7
 
 #define VQPS_SET_BIT(orig, bit) ((uint8_t)((orig) | (1u << (bit))))
 #define VQPS_CLR_BIT(orig, bit) ((uint8_t)((orig) & ~(1u << (bit))))
@@ -61,7 +62,7 @@ static const cpld_pin_map_t vqps_list[] = {
 	{ "MEDHA0_VQPS_E_EN", MEDHA0_VQPS_E_EN_BIT, 0 },
 	{ "MEDHA1_VQPS_E_EN", MEDHA1_VQPS_E_EN_BIT, 0 },
 	/* EVB EVT1B only, via IO expander */
-	{ "P1V8_OWL_EW_VQPS_EN", P1V8_OWL_EW_VQPS_EN, 0 },
+	{ "P1V8_OWL_EW_VQPS_EN", 0, 0 },
 };
 
 static const cpld_pin_map_t *get_vqps_item(const char *name)
@@ -87,6 +88,7 @@ static int cmd_vqps_get(const struct shell *shell, size_t argc, char **argv)
 	uint8_t board_rev = get_board_rev_id();
 	uint8_t reg_status = 0;
 	uint8_t EVB_reg_status = 0;
+	uint8_t bit_val = 0;
 
 	if (!plat_read_cpld(VQPS_STATUS_CPLD_OFFSET, &reg_status, 1)) {
 		LOG_DBG("plat_read_cpld failed: offset=0x%02x", VQPS_STATUS_CPLD_OFFSET);
@@ -110,7 +112,7 @@ static int cmd_vqps_get(const struct shell *shell, size_t argc, char **argv)
 
 	for (int i = 0; i < 5; i++) {
 		const cpld_pin_map_t *item = &vqps_list[i];
-		uint8_t bit_val = (reg_status >> item->bit) & 0x1;
+		bit_val = (reg_status >> item->bit) & 0x1;
 		shell_print(shell, "%s : %d", item->name, bit_val);
 	}
 
@@ -127,26 +129,27 @@ static int cmd_vqps_get(const struct shell *shell, size_t argc, char **argv)
 						LOG_ERR("Failed to read IO expander (EVT1B)");
 						return -1;
 					}
+					bit_val = (EVB_reg_status >> EVT1B_P1V8_OWL_EW_VQPS_EN) & 0x1;
 
 				} else if (board_rev == REV_ID_EVT2) {
-					if (get_pca6554apw_ioe_value(U200052_IO_I2C_BUS,
-								     U200052_IO_ADDR, OUTPUT_PORT,
+					if (get_pca6554apw_ioe_value(U200070_IO_I2C_BUS,
+								     U200070_IO_ADDR, OUTPUT_PORT,
 								     &EVB_reg_status) != 0) {
 						LOG_ERR("EVT2: Failed to read PCA6554 OUTPUT_PORT");
 						return -1;
 					}
+					bit_val = (EVB_reg_status >> EVT2_P1V8_OWL_EW_VQPS_EN) & 0x1;
 				} else {
 					LOG_WRN("Board rev (0x%02X) does not support this function",
 						board_rev);
 					return -1;
 				}
 
-				uint8_t bit_val = (EVB_reg_status >> P1V8_OWL_EW_VQPS_EN) & 0x1;
 				shell_print(shell, "%s : %d", item->name, bit_val);
 				continue;
 			}
 
-			uint8_t bit_val = (EVB_reg_status >> item->bit) & 0x1;
+			bit_val = (EVB_reg_status >> item->bit) & 0x1;
 			shell_print(shell, "%s : %d", item->name, bit_val);
 		}
 	}
@@ -191,7 +194,7 @@ static int cmd_vqps_set(const struct shell *shell, size_t argc, char **argv)
 
 		if (board_rev == REV_ID_EVT1B) {
 			/* EVT1B: control via TCA6424A OUTPUT_PORT_2, bit = P1V8_OWL_EW_VQPS_EN */
-			if (!tca6424a_i2c_write_bit(TCA6424A_OUTPUT_PORT_2, P1V8_OWL_EW_VQPS_EN,
+			if (!tca6424a_i2c_write_bit(TCA6424A_OUTPUT_PORT_2, EVT1B_P1V8_OWL_EW_VQPS_EN,
 						    (uint8_t)set_val)) {
 				LOG_ERR("EVT1B: Failed to write IO expander for %s", name);
 				shell_error(shell, "write IO expander failed");
@@ -204,7 +207,7 @@ static int cmd_vqps_set(const struct shell *shell, size_t argc, char **argv)
 			/* EVT2: control via PCA6554 OUTPUT_PORT */
 			uint8_t cur = 0;
 
-			if (get_pca6554apw_ioe_value(U200052_IO_I2C_BUS, U200052_IO_ADDR,
+			if (get_pca6554apw_ioe_value(U200070_IO_I2C_BUS, U200070_IO_ADDR,
 						     OUTPUT_PORT, &cur) != 0) {
 				LOG_ERR("EVT2: Failed to read PCA6554 OUTPUT_PORT");
 				shell_error(shell, "read PCA6554 failed");
@@ -212,12 +215,12 @@ static int cmd_vqps_set(const struct shell *shell, size_t argc, char **argv)
 			}
 
 			if (set_val == 1) {
-				cur |= BIT(P1V8_OWL_EW_VQPS_EN);
+				cur |= BIT(EVT2_P1V8_OWL_EW_VQPS_EN);
 			} else {
-				cur &= ~BIT(P1V8_OWL_EW_VQPS_EN);
+				cur &= ~BIT(EVT2_P1V8_OWL_EW_VQPS_EN);
 			}
 
-			set_pca6554apw_ioe_value(U200052_IO_I2C_BUS, U200052_IO_ADDR, OUTPUT_PORT,
+			set_pca6554apw_ioe_value(U200070_IO_I2C_BUS, U200070_IO_ADDR, OUTPUT_PORT,
 						 cur);
 
 			shell_print(shell, "set %s to %ld done (EVB EVT2 IO exp)", name, set_val);
