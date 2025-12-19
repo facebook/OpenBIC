b35e9d96667a344139c31b50c07977b20b0af1a4
lisa <Lisa-Chang@quantatw.com>
2025-12-19 15:40:47 +0800

    minerva-ag: version commit for ob-minerva-ag-2025.51.02
    Summary:
    - version commit for ob-minerva-ag-2025.51.02
    
    Test Plan:
    - Build code: PASS

--- meta-facebook/minerva-ag/src/platform/plat_version.h
+++ meta-facebook/minerva-ag/src/platform/plat_version.h
@@ -40,8 +40,8 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x25
-#define BIC_FW_WEEK 0x48
-#define BIC_FW_VER 0x01
+#define BIC_FW_WEEK 0x51
+#define BIC_FW_VER 0x02
 #define BIC_FW_platform_0 0x61 // char: a
 #define BIC_FW_platform_1 0x67 // char: g
 #define BIC_FW_platform_2 0x00 // char: '\0'
