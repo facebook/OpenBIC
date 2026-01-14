eb2452ab6db9eba4fd56aaa0c126de84ce038c7f
lisa <Lisa-Chang@quantatw.com>
2026-01-14 11:14:07 -0800

    minerva-ag: Add UBC in vr_status cli command (#2622)
    Summary:
    - Add UBC in vr_status cli command
    
    Pull Request resolved: https://github.com/facebook/OpenBIC/pull/2622
    
    Test Plan: - Build code: PASS
    
    Reviewed By: jamesatha
    
    Differential Revision: D88331288
    
    fbshipit-source-id: fd29732ffb6670062353f5cb5b49f43af7c7ea9b

--- common/dev/bmr316.c
+++ common/dev/bmr316.c
@@ -27,6 +27,122 @@
 
 LOG_MODULE_REGISTER(bmr316);
 
+bool bmr316_i2c_read(uint8_t bus, uint8_t addr, uint8_t reg, uint8_t *data, uint8_t len)
+{
+	CHECK_NULL_ARG_WITH_RETURN(data, false);
+
+	memset(data, 0, len);
+
+	I2C_MSG i2c_msg = { 0 };
+	uint8_t retry = 5;
+	i2c_msg.bus = bus;
+	i2c_msg.target_addr = addr;
+	i2c_msg.tx_len = 1;
+	i2c_msg.rx_len = len;
+	i2c_msg.data[0] = reg;
+
+	if (i2c_master_read(&i2c_msg, retry)) {
+		LOG_ERR("Failed to read mp29816a, bus: %d, addr: 0x%x, reg: 0x%x", bus, addr, reg);
+		return false;
+	}
+
+	memcpy(data, i2c_msg.data, len);
+	return true;
+}
+
+bool bmr316_get_vr_status(sensor_cfg *cfg, uint8_t rail, uint8_t vr_status_rail,
+			  uint16_t *vr_status)
+{
+	CHECK_NULL_ARG_WITH_RETURN(cfg, false);
+	CHECK_NULL_ARG_WITH_RETURN(vr_status, false);
+
+	uint16_t val = 0;
+
+	switch (vr_status_rail) {
+	case PMBUS_STATUS_BYTE: {
+		uint8_t data[1] = { 0 };
+		if (!bmr316_i2c_read(cfg->port, cfg->target_addr, PMBUS_STATUS_BYTE, data,
+				     sizeof(data))) {
+			return false;
+		}
+		val = (uint16_t)data[0];
+	} break;
+	case PMBUS_STATUS_WORD: {
+		uint8_t data[2] = { 0 };
+		if (!bmr316_i2c_read(cfg->port, cfg->target_addr, PMBUS_STATUS_WORD, data,
+				     sizeof(data))) {
+			return false;
+		}
+		val = data[0] | (data[1] << 8);
+	} break;
+	case PMBUS_STATUS_VOUT: {
+		uint8_t data[1] = { 0 };
+		if (!bmr316_i2c_read(cfg->port, cfg->target_addr, PMBUS_STATUS_VOUT, data,
+				     sizeof(data))) {
+			return false;
+		}
+		val = (uint16_t)data[0];
+	} break;
+	case PMBUS_STATUS_IOUT: {
+		uint8_t data[1] = { 0 };
+		if (!bmr316_i2c_read(cfg->port, cfg->target_addr, PMBUS_STATUS_IOUT, data,
+				     sizeof(data))) {
+			return false;
+		}
+		val = (uint16_t)data[0];
+	} break;
+	case PMBUS_STATUS_INPUT: {
+		uint8_t data[1] = { 0 };
+		if (!bmr316_i2c_read(cfg->port, cfg->target_addr, PMBUS_STATUS_INPUT, data,
+				     sizeof(data))) {
+			return false;
+		}
+		val = (uint16_t)data[0];
+	} break;
+	case PMBUS_STATUS_TEMPERATURE: {
+		uint8_t data[1] = { 0 };
+		if (!bmr316_i2c_read(cfg->port, cfg->target_addr, PMBUS_STATUS_TEMPERATURE, data,
+				     sizeof(data))) {
+			return false;
+		}
+		val = (uint16_t)data[0];
+	} break;
+	case PMBUS_STATUS_CML: {
+		uint8_t data[1] = { 0 };
+		if (!bmr316_i2c_read(cfg->port, cfg->target_addr, PMBUS_STATUS_CML, data,
+				     sizeof(data))) {
+			return false;
+		}
+		val = (uint16_t)data[0];
+	} break;
+	default:
+		LOG_ERR("VR[0x%x] not support vr status:0x%x.", cfg->num, vr_status_rail);
+		return false;
+		break;
+	}
+	*vr_status = val;
+	return true;
+}
+
+bool bmr316_clear_vr_status(sensor_cfg *cfg, uint8_t rail)
+{
+	CHECK_NULL_ARG_WITH_RETURN(cfg, false);
+
+	I2C_MSG i2c_msg = { 0 };
+	uint8_t retry = 5;
+	i2c_msg.bus = cfg->port;
+	i2c_msg.target_addr = cfg->target_addr;
+	i2c_msg.tx_len = 1;
+	i2c_msg.data[0] = PMBUS_CLEAR_FAULTS;
+
+	if (i2c_master_write(&i2c_msg, retry)) {
+		LOG_ERR("VR[0x%x] clear fault failed.", cfg->num);
+		return false;
+	}
+
+	return true;
+}
+
 uint8_t bmr316_read(sensor_cfg *cfg, int *reading)
 {
 	CHECK_NULL_ARG_WITH_RETURN(cfg, SENSOR_UNSPECIFIED_ERROR);
