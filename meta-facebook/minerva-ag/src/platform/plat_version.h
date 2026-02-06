fbf368b98aab740e0189c857d10d0f4e5fc579d1
lisa <Lisa-Chang@quantatw.com>
2026-02-06 14:08:35 +0800

    minerva-ag: version commit for ob-minerva-ag-2026.06.01
    Summary:
    - version commit for ob-minerva-ag-2026.06.01
    
    Test Plan:
    - Build code: PASS

--- meta-facebook/minerva-ag/src/platform/plat_version.h
+++ meta-facebook/minerva-ag/src/platform/plat_version.h
@@ -39,8 +39,8 @@
 #define AUXILIARY_FW_REVISION 0x00000000
 
 #define BIC_FW_YEAR_MSB 0x20
-#define BIC_FW_YEAR_LSB 0x25
-#define BIC_FW_WEEK 0x53
+#define BIC_FW_YEAR_LSB 0x26
+#define BIC_FW_WEEK 0x06
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x61 // char: a
 #define BIC_FW_platform_1 0x67 // char: g
