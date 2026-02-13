d3e03b3ef68ad9ca16b3f9dd81d0f2c6d6528133
Billy <Billy.Tang@quantatw.com>
2026-02-13 09:34:56 +0800

    sb: sitv3: modify platform version for 2026.06.01 release Summary: - Version commit for 2026.06.01 Test Plan: - Build code: Pass

--- meta-facebook/sb-si/src/platform/plat_version.h
+++ meta-facebook/sb-si/src/platform/plat_version.h
@@ -37,7 +37,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x26
-#define BIC_FW_WEEK 0x05
+#define BIC_FW_WEEK 0x06
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x73 // char: s
 #define BIC_FW_platform_1 0x69 // char: i
