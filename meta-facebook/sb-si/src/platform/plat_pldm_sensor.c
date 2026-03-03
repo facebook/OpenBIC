04c1682d41174d5790217b0fbcb0ff3b7b9b6005
adamHSU <Adam.Hsu@.quantatw.com>
2026-03-03 14:53:47 +0800

    sb: si: fix sensor UCR value Summary: - Correct the UCR value which was incorrectly set during the initial configuration. Test Plan: - Build code: Pass

--- meta-facebook/sb-si/src/platform/plat_pldm_sensor.c
+++ meta-facebook/sb-si/src/platform/plat_pldm_sensor.c
@@ -488,7 +488,7 @@ pldm_sensor_info plat_pldm_sensor_vr_table[] = {
 			0x00000000, //uint32_t normal_min;
 			0x00000000, //uint32_t warning_high;
 			0x00000000, //uint32_t warning_low;
-			10350, //uint32_t critical_high;
+			103500, //uint32_t critical_high;
 			0x00000000, //uint32_t critical_low;
 			0x00000000, //uint32_t fatal_high;
 			0x00000000, //uint32_t fatal_low;
