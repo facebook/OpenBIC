64c291dda079bc5fc064daeebdc22fdc617a1961
Kevin Huang <KevinHuang4@quntatw.com>
2026-04-21 14:53:09 +0800

    sb: rb: fix rail error

--- meta-facebook/sb-rb/src/shell/shell_plat_voltage_peak.c
+++ meta-facebook/sb-rb/src/shell/shell_plat_voltage_peak.c
@@ -164,7 +164,8 @@ static int cmd_clear_voltage_peak(const struct shell *shell, size_t argc, char *
 
 static void voltage_rname_get(size_t idx, struct shell_static_entry *entry)
 {
-	if ((get_asic_board_id() == ASIC_BOARD_ID_EVB))
+	if (((get_asic_board_id() != ASIC_BOARD_ID_EVB)) &&
+		    (idx == VR_RAIL_E_P3V3_OSFP_VOLT_V))
 		idx++;
 
 	uint8_t *name = NULL;
