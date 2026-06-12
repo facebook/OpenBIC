15585a83cd23ee941462a45fa1db1033e0c5cc84
Billy <Billy.Tang@quantatw.com>
2026-06-12 09:30:42 +0800

    sb: el: update display name from IRIS to ARKE
    Summary:
    - Replace `IRIS` with `ARKE` in display output
    
    Test Plan:
    - Build code: Pass

--- meta-facebook/sb-el/src/platform/plat_arke_smbus.h
+++ meta-facebook/sb-el/src/platform/plat_arke_smbus.h
@@ -83,5 +83,5 @@
 // firmware block write
 #define FW_DATA_WRITE 138
 
-int iris_smbus_fast_boot(uint8_t *image_buff, uint32_t img_dest_addr, uint32_t img_size);
+int ARKE_smbus_fast_boot(uint8_t *image_buff, uint32_t img_dest_addr, uint32_t img_size);
 #endif
